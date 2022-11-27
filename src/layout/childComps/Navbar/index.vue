b66e090a8bc3ad5f2a54f52da1e2c7d6d87a5e75
lengyibai <1329670984@qq.com>
2022-11-27 13:00:21 +0800

    perf: 使用字体图标

--- src/layout/childComps/Navbar/index.vue
+++ src/layout/childComps/Navbar/index.vue
@@ -6,23 +6,9 @@
 
     <div class="right">
       <!-- 搜索 -->
-      <LibSvg
-        :svg="icon.search"
-        size="35px"
-        enterColor="var(--white)"
-        color="var(--theme-color-five)"
-        right="20px"
-        @click="$switchStore.$tip('开发中...', 'warning')"
-      />
+      <i class="iconfont wzry-search" @click="$switchStore.$tip('开发中...', 'warning')" />
       <!-- 设置 -->
-      <LibSvg
-        :svg="icon.setting"
-        size="35px"
-        enterColor="var(--white)"
-        color="var(--theme-color-five)"
-        right="20px"
-        @click="$switchStore.$tip('开发中...', 'warning')"
-      />
+      <i class="iconfont wzry-setting" @click="$switchStore.$tip('开发中...', 'warning')" />
 
       <!-- 用户操作 -->
       <UserMenu />
@@ -30,7 +16,6 @@
   </div>
 </template>
 <script setup lang="ts">
-import icon from "./img/icon";
 import switchStore from "@/store/globalSwitch";
 import PackUp from "./childComps/PackUp/index.vue"; //收起按钮
 import UserMenu from "./childComps/UserMenu/index.vue"; //用户卡片
