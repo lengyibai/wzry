47371604912ff0c428948105d85d0500cccc710c
lengyibai <1329670984@qq.com>
2022-12-01 01:18:04 +0800

    type: id类型更改为number & type: 删除无用的泛型

--- src/api/main/games/skin.ts
+++ src/api/main/games/skin.ts
@@ -2,16 +2,16 @@ import { get } from "@/api/helper/transfer";
 
 /** @description: 获取皮肤列表 */
 export const getSkin = () => {
-  return Promise.resolve(get<any, Hero.Data[]>({ name: "data_skin" }));
+  return Promise.resolve(get<Hero.Data[]>({ name: "data_skin" }));
 };
 
 /** @description: 获取指定英雄皮肤 */
-export const getHeroSkin = (id: string) => {
-  const skins = get<string, Hero.Skin>({ name: "data_skin", key: "hero", value: id }, false);
+export const getHeroSkin = (id: number) => {
+  const skins = get<Hero.Skin>({ name: "data_skin", key: "hero", value: id }, false);
   return Promise.resolve(skins);
 };
 
 /** @description: 获取皮肤类型 */
 export const getSkinType = () => {
-  return Promise.resolve(get<any, Hero.SkinType[]>({ name: "data_skintype" }));
+  return Promise.resolve(get<Hero.SkinType[]>({ name: "data_skintype" }));
 };
