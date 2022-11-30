7e105fc52b378622afc5a5ea5e5ea841077e1491
lengyibai <1329670984@qq.com>
2022-12-01 01:20:50 +0800

    feat: 增加仅带有label的支持插槽的表单

---
+++ src/components/business/Form/FormLabel/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="head-poster" :class="{ center: center }">
+    <div class="label" :style="{ width: labelWidth }">
+      <span class="text-gradient-one"><i class="star">*</i>{{ label }}： </span>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+interface Props {
+  label: string; //标题文字
+  labelWidth?: string; //标题宽度
+  center?: boolean; //是否居中
+}
+
+withDefaults(defineProps<Props>(), {
+  label: "标题",
+  labelWidth: "150px",
+  center: false,
+});
+</script>
+<style scoped lang="less">
+@import url("./index.less");
+</style>
