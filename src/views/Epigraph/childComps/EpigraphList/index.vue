806b46d44f404623eeb766ba807f228e7f32b8b8
lengyibai <1329670984@qq.com>
2022-12-03 02:11:58 +0800

    perf: 优化列表响应式布局

--- src/views/Epigraph/childComps/EpigraphList/index.vue
+++ src/views/Epigraph/childComps/EpigraphList/index.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="epigraph-list" :style="{ opacity: show ? 1 : 0 }">
-    <LibGridLayout gap="15px" :count="count">
-      <EpigraphCard v-for="(item, index) in epigraph_list" :data="item" :key="index" />
-    </LibGridLayout>
+  <div>
+    <transition name="hero-list">
+      <div class="epigraph-list" v-if="show">
+        <LibGridLayout ref="epigraphListRef" v-if="epigraph_list.length" gap="15px" :count="count" :eqhMultiple="0.5">
+          <transition-group name="hero" appear>
+            <div
+              v-for="(item, index) in epigraph_list"
+              :style="{
+                'transition-delay': 0.025 * index + 's',
+              }"
+              :key="item.id"
+            >
+              <EpigraphCard :data="item" />
+            </div>
+          </transition-group>
+        </LibGridLayout>
+      </div>
+    </transition>
   </div>
 </template>
 <script setup lang="ts">
-import { onBeforeUnmount, ref, watch } from "vue";
+import { onActivated, onBeforeUnmount, ref, watch, nextTick, onMounted } from "vue";
 import $bus from "@/utils/eventBus";
 import EpigraphCard from "./childComps/EpigraphCard/index.vue";
 
@@ -18,8 +32,9 @@ const props = withDefaults(defineProps<Props>(), {
   data: () => [],
 });
 
+const epigraphListRef = ref();
 const show = ref(false); //淡入显示列表
-const count = ref(5); //一行的个数
+const count = ref(4); //一行的个数
 const epigraph_list = ref<Epigraph.Data[]>([]); //铭文列表
 
 /* 每次修改更新列表 */
@@ -27,34 +42,43 @@ watch(
   () => props.data,
   (v: Epigraph.Data[]) => {
     show.value = false;
-    setTimeout(() => {
+    nextTick(() => {
       epigraph_list.value = v;
       show.value = true;
-    }, 300);
+    });
   },
   { deep: true, immediate: true }
 );
 
-/* 实时修改一行个数 */
-const change = [
-  [1550, 3],
-  [1250, 2],
-  [950, 1],
-];
-const initCount = () => {
-  const v = document.documentElement.clientWidth;
-  if (v > 1400) {
-    count.value = 4;
-  }
-  for (const [a, b] of change) {
-    if (v < a) {
-      count.value = b;
+/* 进入页面后更新高度 */
+onActivated(() => {
+  nextTick(() => {
+    epigraphListRef.value.updateHeight();
+  }).catch(() => {});
+});
+
+onMounted(() => {
+  /* 实时修改一行个数 */
+  const change = [
+    [1680, 3],
+    [1340, 2],
+    [1020, 1],
+  ];
+  const changeCount = () => {
+    const v = document.documentElement.clientWidth;
+    if (v > 1680) {
+      count.value = 4;
     }
-  }
-};
-initCount();
-$bus.on("resize", () => {
-  initCount();
+    for (const [a, b] of change) {
+      if (v < a) {
+        count.value = b;
+      }
+    }
+  };
+  changeCount();
+  $bus.on("resize", () => {
+    changeCount();
+  });
 });
 
 onBeforeUnmount(() => {
