7d7eaeff7e2431683a0c56176211fccf96cf73ed
lengyibai <1329670984@qq.com>
2022-12-03 02:00:02 +0800

    replace: 登录页视频背景链接替换

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -2,7 +2,7 @@
 const HTTPS1 = "https://wikicdn.dayz.plus/";
 
 /** @description: 登录页视频 */
-export const LOGINBG = HTTPS1 + "8b52f27abcee47a78a15fb233484c3bb.mp4";
+export const LOGINBG = HTTPS1 + "5bcf42bf39e2444d9c82b98ed8413bed.mp4";
 
 /** @description: 登录后背景视频 */
 export const BG = HTTPS1 + "9d35649f76384b78a798c227486d3be0.mp4";
