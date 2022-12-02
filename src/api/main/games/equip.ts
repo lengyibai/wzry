6525f6c32e1d59edc41da52c951d4718d3950b8a
lengyibai <1329670984@qq.com>
2022-12-03 01:58:49 +0800

    feat: 增加获取装备类型和效果

--- src/api/main/games/equip.ts
+++ src/api/main/games/equip.ts
@@ -4,3 +4,11 @@ import { get } from "@/api/helper/transfer";
 export const getEquip = () => {
   return Promise.resolve(get<Equip.Data[]>({ name: "data_equip" }));
 };
+/** @description: 获取装备类型列表 */
+export const getEquipType = () => {
+  return Promise.resolve(get<Hero.General[]>({ name: "data_equiptype" }));
+};
+/** @description: 获取装备效果列表 */
+export const getEquipEffect = () => {
+  return Promise.resolve(get<Hero.General[]>({ name: "data_equipeffect" }));
+};
