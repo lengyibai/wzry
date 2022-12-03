8cf69a0428d827bcafad3f2af4a7a96b35bab03f
lengyibai <1329670984@qq.com>
2022-12-04 00:52:49 +0800

    feat: 增加皮肤页模板

---
+++ src/views/Skin/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="skin">
+    <h1>皮肤</h1>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="less">
+.skin {
+  width: 100%;
+  height: 100%;
+  font-size: 26px;
+}
+</style>
