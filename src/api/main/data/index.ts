5930e025ad6d6d0d928f4162859429124853ebc6
lengyibai <1329670984@qq.com>
2022-12-05 01:03:21 +0800

    refactor: 移除get前缀避免冲突

--- src/api/main/data/index.ts
+++ src/api/main/data/index.ts
@@ -1,61 +1,64 @@
 import http from "@/api/index";
 
 /** @description: 获取用户列表 */
-export const getUser = () => http.Get<any[]>("/user.json");
+export const User = () => http.Get<any[]>("/user.json");
 
 /** @description: 获取英雄基础列表 */
-export const getHerobasic = () => http.Get<any[]>("/heroBasic.json");
+export const HeroBasic = () => http.Get<any[]>("/heroBasic.json");
+
+/** @description: 获取英雄图片列表 */
+export const HeroImg = () => http.Get<any[]>("/heroImg.json");
 
 /** @description: 获取英雄信息列表 */
-export const getHerodata = () => http.Get<any[]>("/heroData.json");
+export const Herodata = () => http.Get<any[]>("/heroData.json");
 
 /** @description: 获取英雄语音列表 */
-export const getVoice = (hero_name: string) => http.Get<any[]>(`/voices/${hero_name}.json`);
+export const Voice = (hero_name: string) => http.Get<any[]>(`/voices/${hero_name}.json`);
 
 /** @description: 获取技能类型列表 */
-export const getSkilltype = () => http.Get<any[]>("/skillType.json");
+export const Skilltype = () => http.Get<any[]>("/skillType.json");
 
 /** @description: 获取技能效果列表 */
-export const getSkilleffect = () => http.Get<any[]>("/skillEffect.json");
+export const Skilleffect = () => http.Get<any[]>("/skillEffect.json");
 
 /** @description: 获取皮肤列表 */
-export const getSkin = () => http.Get<any[]>("/skin.json");
+export const Skin = () => http.Get<any[]>("/skin.json");
 
 /** @description: 获取皮肤类型列表 */
-export const getSkintype = () => http.Get<any[]>("/skinType.json");
+export const Skintype = () => http.Get<any[]>("/skinType.json");
 
 /** @description: 获取装备列表 */
-export const getEquip = () => http.Get<any[]>("/equip.json");
+export const Equip = () => http.Get<any[]>("/equip.json");
 
 /** @description: 获取装备类型列表 */
-export const getEquiptype = () => http.Get<any[]>("/equipType.json");
+export const Equiptype = () => http.Get<any[]>("/equipType.json");
 
 /** @description: 获取装备效果列表 */
-export const getEquipeffect = () => http.Get<any[]>("/equipEffect.json");
+export const Equipeffect = () => http.Get<any[]>("/equipEffect.json");
 
 /** @description: 获取铭文列表 */
-export const getEpigraph = () => http.Get<any[]>("/epigraph.json");
+export const Epigraph = () => http.Get<any[]>("/epigraph.json");
 
 /** @description: 获取铭文类型列表 */
-export const getEpigraphtype = () => http.Get<any[]>("/epigraphType.json");
+export const Epigraphtype = () => http.Get<any[]>("/epigraphType.json");
 
 /** @description: 获取铭文效果列表 */
-export const getEpigrapheffect = () => http.Get<any[]>("/epigraphEffect.json");
+export const Epigrapheffect = () => http.Get<any[]>("/epigraphEffect.json");
 
 /** @description: 获取职业列表 */
-export const getProfessiontype = () => http.Get<any[]>("/professionType.json");
+export const Professiontype = () => http.Get<any[]>("/professionType.json");
 
 /** @description: 获取定位列表 */
-export const getLocationtype = () => http.Get<any[]>("/locationType.json");
+export const Locationtype = () => http.Get<any[]>("/locationType.json");
 
 /** @description: 获取特长列表 */
-export const getSpecialtytype = () => http.Get<any[]>("/specialtyType.json");
+export const Specialtytype = () => http.Get<any[]>("/specialtyType.json");
 
 /** @description: 获取时期列表 */
-export const getPeriodtype = () => http.Get<any[]>("/periodType.json");
+export const Periodtype = () => http.Get<any[]>("/periodType.json");
 
 /** @description: 获取阵营列表 */
-export const getCamptype = () => http.Get<any[]>("/campType.json");
+export const Camptype = () => http.Get<any[]>("/campType.json");
 
 /** @description: 获取关系列表 */
-export const getRelationtype = () => http.Get<any[]>("/relationType.json");
+export const Relationtype = () => http.Get<any[]>("/relationType.json");
