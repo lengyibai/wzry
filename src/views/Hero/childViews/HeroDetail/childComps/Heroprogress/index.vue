c0d3ba939406e73363c45ae313f09cc94e9c8cbe
lengyibai <1329670984@qq.com>
2022-12-06 00:15:50 +0800

    feat: 增加滚动进度

---
+++ src/views/Hero/childViews/HeroDetail/childComps/Heroprogress/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- 滚动进度 -->
+  <div class="hero-progress">
+    <div class="page-index" v-for="(item, i) in pageName" :key="i" :class="{ active: index === i + 1 }">
+      <div class="tab flex" @click="toggle(i + 1)" v-show="index !== i + 1">{{ item }}</div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+interface Props {
+  index: number;
+  pageName: string[];
+}
+withDefaults(defineProps<Props>(), {
+  index: 0,
+  pageName: () => [],
+});
+
+interface Emits {
+  (e: "toggle", v: number): void;
+}
+const emit = defineEmits<Emits>();
+
+const toggle = (index: number) => {
+  emit("toggle", index);
+};
+</script>
+<style scoped lang="less">
+@import url("./index.less");
+</style>
