b18e2b1cb2c302817763a454001ee54133a6ff92
lengyibai <1329670984@qq.com>
2022-12-05 23:48:30 +0800

    feat: 增加获取关系列表、获取指定英雄关系

---
+++ src/api/main/games/relationship.ts
@@ -0,0 +1,12 @@
+import { get } from "@/api/helper/transfer";
+
+/** @description: 获取关系列表 */
+export const getRelationship = () => {
+  return Promise.resolve(get<Hero.Relationship[]>({ name: "data_relationship" }));
+};
+
+/** @description: 获取指定英雄关系 */
+export const getHeroRelationship = (id: number) => {
+  const Relationships = get<Hero.Relationship>({ name: "data_relationship", key: "id", value: id });
+  return Promise.resolve(Relationships);
+};
