77e1340dd64e0b6f988c03d7a9db65f1da7fc400
lengyibai <1329670984@qq.com>
2022-12-05 12:12:52 +0800

    visual: 给每页设置视差背景

--- src/views/Hero/childViews/HeroDetail/index.vue
+++ src/views/Hero/childViews/HeroDetail/index.vue
@@ -3,16 +3,24 @@
     <LibMaskClose @close="hide" />
     <!--资料皮肤-->
     <HeroDetailParallaxBg :bg="hero_data.poster">
-      <HeroMaterialSkins />
+      <HeroInfo />
+    </HeroDetailParallaxBg>
+
+    <!--皮肤-->
+    <HeroDetailParallaxBg :bg="hero_data.poster" v-if="hero_data.skins.length">
+      <HeroSkin />
     </HeroDetailParallaxBg>
 
     <!--技能-->
-    <HeroDetailParallaxBg :bg="hero_data.poster" v-if="hero_data.skills">
+    <HeroDetailParallaxBg :bg="hero_data.skins[1].poster" v-if="hero_data.skills.length">
       <HeroSkill />
     </HeroDetailParallaxBg>
 
     <!--故事-->
-    <HeroDetailParallaxBg :bg="hero_data.poster" v-if="hero_data.gamestory">
+    <HeroDetailParallaxBg
+      :bg="hero_data.skins[2] ? hero_data.skins[2].poster : hero_data.poster"
+      v-if="hero_data.gamestory"
+    >
       <HeroStory />
     </HeroDetailParallaxBg>
   </div>
@@ -21,7 +29,8 @@
 import { useRouter } from "vue-router";
 import heroStore from "@/store/hero";
 import HeroDetailParallaxBg from "./childComps/HeroDetailParallaxBg/index.vue"; //滚动视差背景
-import HeroMaterialSkins from "./childComps/HeroMaterialSkins/index.vue"; //资料、皮肤页
+import HeroInfo from "./childComps/HeroInfo/index.vue"; //资料
+import HeroSkin from "./childComps/HeroSkin/index.vue"; //皮肤鉴赏
 import HeroSkill from "./childComps/HeroSkill/index.vue"; //技能页
 import HeroStory from "./childComps/HeroStory/index.vue"; //历史页
 
