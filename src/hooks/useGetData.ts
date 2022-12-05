f38a43f56db806476c21d7c53490f826c7f158b1
lengyibai <1329670984@qq.com>
2022-12-05 11:56:30 +0800

    rename: 简化名称去掉get前缀

--- src/hooks/useGetData.ts
+++ src/hooks/useGetData.ts
@@ -1,25 +1,26 @@
 import switchStore from "@/store/globalSwitch";
 import {
-  getUser,
-  getHerobasic,
-  getHerodata,
-  getSkilltype,
-  getSkilleffect,
-  getSkin,
-  getSkintype,
-  getEquip,
-  getEquiptype,
-  getEquipeffect,
-  getEpigraph,
-  getEpigraphtype,
-  getEpigrapheffect,
-  getProfessiontype,
-  getLocationtype,
-  getSpecialtytype,
-  getPeriodtype,
-  getCamptype,
-  getRelationtype,
-  getVoice,
+  User,
+  HeroBasic,
+  HeroImg,
+  Herodata,
+  Skilltype,
+  Skilleffect,
+  Skin,
+  Skintype,
+  Equip,
+  Equiptype,
+  Equipeffect,
+  Epigraph,
+  Epigraphtype,
+  Epigrapheffect,
+  Professiontype,
+  Locationtype,
+  Specialtytype,
+  Periodtype,
+  Camptype,
+  Relationtype,
+  Voice,
 } from "@/api/main/data";
 import { getHeroBasic } from "@/api/main/games/hero";
 
@@ -27,25 +28,26 @@ export default async () => {
   const $switchStore = switchStore();
 
   const requests: [string, () => Promise<any>, string][] = [
-    ["user", getUser, "用户"],
-    ["herobasic", getHerobasic, "英雄基础"],
-    ["herodata", getHerodata, "英雄信息"],
-    ["skilltype", getSkilltype, "技能类型"],
-    ["skilleffect", getSkilleffect, "技能效果"],
-    ["skin", getSkin, "皮肤"],
-    ["skintype", getSkintype, "皮肤类型"],
-    ["equip", getEquip, "装备"],
-    ["equiptype", getEquiptype, "装备类型"],
-    ["equipeffect", getEquipeffect, "装备效果"],
-    ["epigraph", getEpigraph, "铭文"],
-    ["epigraphtype", getEpigraphtype, "铭文类型"],
-    ["epigrapheffect", getEpigrapheffect, "铭文效果"],
-    ["professiontype", getProfessiontype, "职业"],
-    ["locationtype", getLocationtype, "定位"],
-    ["specialtytype", getSpecialtytype, "特长"],
-    ["periodtype", getPeriodtype, "时期"],
-    ["camptype", getCamptype, "阵营"],
-    ["relationtype", getRelationtype, "关系"],
+    ["user", User, "用户"],
+    ["herobasic", HeroBasic, "英雄基础"],
+    ["heroimg", HeroImg, "英雄图片"],
+    ["herodata", Herodata, "英雄信息"],
+    ["skilltype", Skilltype, "技能类型"],
+    ["skilleffect", Skilleffect, "技能效果"],
+    ["skin", Skin, "皮肤"],
+    ["skintype", Skintype, "皮肤类型"],
+    ["equip", Equip, "装备"],
+    ["equiptype", Equiptype, "装备类型"],
+    ["equipeffect", Equipeffect, "装备效果"],
+    ["epigraph", Epigraph, "铭文"],
+    ["epigraphtype", Epigraphtype, "铭文类型"],
+    ["epigrapheffect", Epigrapheffect, "铭文效果"],
+    ["professiontype", Professiontype, "职业"],
+    ["locationtype", Locationtype, "定位"],
+    ["specialtytype", Specialtytype, "特长"],
+    ["periodtype", Periodtype, "时期"],
+    ["camptype", Camptype, "阵营"],
+    ["relationtype", Relationtype, "关系"],
   ];
 
   const setData = (name: string, data: any) => {
@@ -65,7 +67,7 @@ export default async () => {
     const hero_list = await getHeroBasic();
     for (let i = 0; i < hero_list.length; i++) {
       if (!["梦奇", "盾山"].includes(hero_list[i].name)) {
-        setData(`voice_${hero_list[i].name}`, { data: await getVoice(hero_list[i].name) });
+        setData(`voice_${hero_list[i].name}`, { data: await Voice(hero_list[i].name) });
         $switchStore.$loading.show(`正在下载${hero_list[i].name}语音${voice_index++}/${hero_list.length - 2}`);
       }
     }
