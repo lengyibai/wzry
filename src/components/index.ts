2e520097b2e19c349b73ad0bcefe999215d0917f
lengyibai <1329670984@qq.com>
2022-12-06 15:29:34 +0800

    delete: 删除不必要

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAsyncComponent, App } from "vue";
-
-const components = import.meta.glob("@/components/**/*.vue");
-export default function install(app: App) {
-  Object.entries(components).forEach(([key, value]) => {
-    const name = key.replace(/\/index.vue/g, "");
-    app.component(name.slice(name.lastIndexOf("/") + 1), defineAsyncComponent(value as any));
-  });
-}
