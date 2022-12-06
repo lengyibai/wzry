a4e15fc2738f502a9f1eebeef93826eacf4a0c21
lengyibai <1329670984@qq.com>
2022-12-06 16:50:03 +0800

    移除不必要依赖

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,13 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import useRouter from "./router/index";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import directives from "@/utils/directives";
-
-import components from "@/components/index";
 
 import "@/styles/index.css";
 
 const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate);
 
 const app = createApp(App);
 
-app.use(pinia).use(useRouter).use(directives).use(components).mount("#app");
+app.use(pinia).use(useRouter).use(directives).mount("#app");
