7e92feefe0fc693f67eb59246bd3bd763cd9ab1e
lengyibai <1329670984@qq.com>
2022-12-07 15:55:50 +0800

    feat: 获取指定皮肤类型

--- src/api/main/games/skin.ts
+++ src/api/main/games/skin.ts
@@ -7,11 +7,15 @@ export const getSkin = () => {
 
 /** @description: 获取指定英雄皮肤 */
 export const getHeroSkin = (id: number) => {
-  const skins = get<Hero.Skin>({ name: "data_skin", key: "hero", value: id }, false);
-  return Promise.resolve(skins);
+  return Promise.resolve(get<Hero.Skin>({ name: "data_skin", key: "hero", value: id }, false));
 };
 
-/** @description: 获取皮肤类型 */
+/** @description: 获取皮肤类型列表 */
 export const getSkinType = () => {
   return Promise.resolve(get<Hero.SkinType[]>({ name: "data_skintype" }));
+};
+
+/** @description: 获取指定皮肤类型 */
+export const getAssignSkinType = (id: number) => {
+  return Promise.resolve(get<Hero.SkinType>({ name: "data_skintype", key: "id", value: id }));
 };
