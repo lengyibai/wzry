e42ef2b4680eeaeb4f00b5a006a0870c778d2be4
lengyibai <1329670984@qq.com>
2022-12-08 23:35:22 +0800

    feat: 增加添加英雄技能

--- src/api/main/games/skill.ts
+++ src/api/main/games/skill.ts
@@ -1,11 +1,11 @@
-import { get } from "@/api/helper/transfer";
-
-interface SkillType {
-  id: number;
-  skills: Hero.Skill[];
-}
+import { get, post } from "@/api/helper/transfer";
 
 /** @description: 获取指定英雄技能 */
 export const getHeroSkill = (id: number) => {
-  return Promise.resolve(get<SkillType>({ name: "data_skill", key: "id", value: id }).skills);
+  return Promise.resolve(get<Hero.SkillParams>({ name: "data_skill", key: "id", value: id }).skills);
+};
+
+/** @description: 添加英雄技能 */
+export const addHeroSkill = (data: Hero.SkillParams) => {
+  return Promise.resolve(post<Hero.SkillParams>("data_skill", data));
 };
