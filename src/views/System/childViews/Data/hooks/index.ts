d63eb652fa6d9d29eabab9fa1e7804b64fc0a32e
lengyibai <1329670984@qq.com>
2022-12-10 00:46:42 +0800

    feat: 完成本地数据管理

---
+++ src/views/System/childViews/Data/hooks/index.ts
@@ -0,0 +1,120 @@
+import { ref, toRaw } from "vue";
+import { $deepCopy } from "@/utils/index";
+import {
+  HeroBasic,
+  HeroImg,
+  Herodata,
+  Skill,
+  Skilltype,
+  Skilleffect,
+  Skin,
+  Skintype,
+  Relationtype,
+  Relationship,
+  Equip,
+  Equiptype,
+  Equipeffect,
+  Epigraph,
+  Epigraphtype,
+  Epigrapheffect,
+  Professiontype,
+  Locationtype,
+  Specialtytype,
+  Periodtype,
+  Camptype,
+} from "@/api/main/data";
+export default () => {
+  const keywords: [string, string][] = [
+    ["herobasic", "英雄基础"],
+    ["heroimg", "英雄图片"],
+    ["herodata", "英雄信息"],
+    ["skill", "技能列表"],
+    ["skilltype", "技能类型"],
+    ["skilleffect", "技能效果"],
+    ["skin", "皮肤"],
+    ["skintype", "皮肤类型"],
+    ["relationship", "关系"],
+    ["relationtype", "关系类型"],
+    ["equip", "装备"],
+    ["equiptype", "装备类型"],
+    ["equipeffect", "装备效果"],
+    ["epigraph", "铭文"],
+    ["epigraphtype", "铭文类型"],
+    ["epigrapheffect", "铭文效果"],
+    ["professiontype", "职业"],
+    ["locationtype", "定位"],
+    ["specialtytype", "特长"],
+    ["periodtype", "时期"],
+    ["camptype", "阵营"],
+  ];
+
+  const requests: Record<string, () => Promise<any>> = {
+    herobasic: HeroBasic,
+    heroimg: HeroImg,
+    herodata: Herodata,
+    skill: Skill,
+    skilltype: Skilltype,
+    skilleffect: Skilleffect,
+    skin: Skin,
+    skintype: Skintype,
+    relationship: Relationship,
+    relationtype: Relationtype,
+    equip: Equip,
+    equiptype: Equiptype,
+    equipeffect: Equipeffect,
+    epigraph: Epigraph,
+    epigraphtype: Epigraphtype,
+    epigrapheffect: Epigrapheffect,
+    professiontype: Professiontype,
+    locationtype: Locationtype,
+    specialtytype: Specialtytype,
+    periodtype: Periodtype,
+    camptype: Camptype,
+  };
+
+  const tableData = ref<any[]>([]);
+  let cache: any[] = [];
+
+  /* 获取本地数据 */
+  const getLocalData = (name: string, prefix = "data_") => {
+    return JSON.parse(localStorage.getItem(prefix + name) as string);
+  };
+
+  /* 设置状态 */
+  const setStatus = (data: any, v: any) => {
+    data.data = JSON.parse(localStorage.getItem("data_" + data.key) as string);
+    if (JSON.stringify(v) === JSON.stringify(data.data)) {
+      data.status = "最新";
+    } else if (data.data.length > v.length) {
+      data.status = "本地已更改";
+    } else {
+      data.status = "待更新";
+    }
+  };
+
+  /* 更新数据 */
+  const updateData = (key: string, data: any) => {
+    localStorage.setItem("data_" + key, JSON.stringify(data));
+  };
+
+  /* 加载数据 */
+  const load = async () => {
+    tableData.value = keywords.map((item) => {
+      const v = getLocalData(item[0]);
+      return {
+        name: item[1],
+        key: item[0],
+        data: v,
+        status: "正在检查...",
+      };
+    });
+    for (let i = 0; i < tableData.value.length; i++) {
+      const data = tableData.value[i];
+      const v = (await requests[data.key]()).data;
+      setStatus(data, v);
+    }
+    cache = $deepCopy<any[]>(tableData.value);
+  };
+
+  return { keywords, requests, load, setStatus, cache, tableData, updateData };
+};
--- src/views/System/childViews/Data/index.vue
+++ src/views/System/childViews/Data/index.vue
@@ -1,76 +1,87 @@
 <template>
   <div class="data">
-    <LibTable :data="tableData" :head="['数据类型', '数据量', '操作']" :sort="['数据量']" @sortChange="sortChange">
+    <LibTable
+      :data="tableData"
+      :head="['数据类型', '数量', '状态', '操作']"
+      :sort="['数据量']"
+      @sortChange="sortChange"
+    >
       <template v-slot:body="{ data }">
-        <TableColumn>{{ data.name }}</TableColumn>
-        <TableColumn>{{ data.length }}</TableColumn>
-        <TableColumn width="275px">
-          <button class="update cursor-pointer" @click="handleUpdate(data)">更新</button>
+        <TableColumn minWidth="175px">{{ data.name }}</TableColumn>
+        <TableColumn minWidth="100px">{{ data.data.length }}</TableColumn>
+        <TableColumn minWidth="200px">{{ data.status }}</TableColumn>
+        <TableColumn width="500px" minWidth="425px">
           <button class="export cursor-pointer" @click="handleExport(data)">导出</button>
+          <button
+            class="replace cursor-pointer"
+            v-if="!['最新', '待更新', '正在检查...'].includes(data.status)"
+            @click="handleReplace(data)"
+          >
+            重置
+          </button>
+          <button class="check cursor-pointer" @click="handleCheck(data)">检查更新</button>
+          <button class="update cursor-pointer" v-if="data.status === '待更新'" @click="handleUpdate(data)">
+            更新
+          </button>
         </TableColumn>
       </template>
     </LibTable>
+    <ConfirmClose
+      v-model="show_ConfirmClose"
+      @close="closeConfirmClose"
+      text="即将从远程下载当前数据进行覆盖"
+      title="确认重置"
+    />
   </div>
 </template>
 <script setup lang="ts" name="Data">
-import { $typeSort, $deepCopy, $savefiles } from "@/utils/index";
-import { onActivated, ref } from "vue";
-const keywords: [string, string][] = [
-  ["user", "用户"],
-  ["herobasic", "英雄基础"],
-  ["heroimg", "英雄图片"],
-  ["herodata", "英雄信息"],
-  ["skill", "技能列表"],
-  ["skilltype", "技能类型"],
-  ["skilleffect", "技能效果"],
-  ["skin", "皮肤"],
-  ["skintype", "皮肤类型"],
-  ["relationship", "关系"],
-  ["relationtype", "关系类型"],
-  ["equip", "装备"],
-  ["equiptype", "装备类型"],
-  ["equipeffect", "装备效果"],
-  ["epigraph", "铭文"],
-  ["epigraphtype", "铭文类型"],
-  ["epigrapheffect", "铭文效果"],
-  ["professiontype", "职业"],
-  ["locationtype", "定位"],
-  ["specialtytype", "特长"],
-  ["periodtype", "时期"],
-  ["camptype", "阵营"],
-];
+import { $typeSort, $savefiles } from "@/utils/index";
+import { ref } from "vue";
+import hooks from "./hooks/index";
 
-/* 获取本地数据 */
-const getLocalData = (name: string, prefix = "data_") => {
-  return JSON.parse(localStorage.getItem(prefix + name) as string);
-};
-
-const tableData = ref<any[]>([]);
-let cache: any[] = [];
-
-onActivated(() => {
-  tableData.value = keywords.map((item) => {
-    const v = getLocalData(item[0]);
-    return {
-      name: item[1],
-      key: item[0],
-      length: v.length,
-      data: v,
-    };
-  });
-  cache = $deepCopy<any[]>(tableData.value);
-});
+const { requests, cache, tableData, load, setStatus, updateData } = hooks();
+load();
 
 /* 导出 */
 const handleExport = (data: any) => {
-  $savefiles(JSON.stringify(data.data, null, 2), data.key + ".json");
+  $savefiles(JSON.stringify({ data: data.data }, null, 2), data.key + ".json");
 };
 
-/* 更新 */
-const handleUpdate = (data: any) => {
-  console.log(data);
+/* 检查更新 */
+const handleCheck = async (data: any) => {
+  data.status = "正在检查...";
+  const v = (await requests[data.key]()).data;
+  setTimeout(() => {
+    setStatus(data, v);
+  }, 1000);
 };
 
+/* 强制覆盖 */
+const show_ConfirmClose = ref(false);
+let replace_data: any = {};
+const handleReplace = (data: any) => {
+  replace_data = data;
+  show_ConfirmClose.value = true;
+};
+
+/* 确认覆盖 */
+const closeConfirmClose = async (status: boolean) => {
+  if (status) {
+    const v = (await requests[replace_data.key]()).data;
+    updateData(replace_data.key, v);
+    handleCheck(replace_data);
+  }
+};
+
+/* 更新指定 */
+const handleUpdate = async (data: any) => {
+  const v = (await requests[data.key]()).data;
+  updateData(data.key, v);
+  data.data = v;
+  handleCheck(data);
+};
+
+/* 排序触发 */
 const sortChange = (v: number[]) => {
   if (v[1] === 1 || v[1] === 2) {
     tableData.value = $typeSort(tableData.value, "length", v[1] === 1 ? true : false);
