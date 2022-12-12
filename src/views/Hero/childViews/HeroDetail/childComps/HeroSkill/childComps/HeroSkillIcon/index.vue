c263f5137f8e2a12104c9198efc7372ee4fb86db
lengyibai <1329670984@qq.com>
2022-12-12 22:11:17 +0800

    feat: 增加切换按钮功能

--- src/views/Hero/childViews/HeroDetail/childComps/HeroSkill/childComps/HeroSkillIcon/index.vue
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroSkill/childComps/HeroSkillIcon/index.vue
@@ -13,9 +13,18 @@
       <transition name="border-fade">
         <div class="border" v-show="currentIndex === index"></div>
       </transition>
-      <img :src="item.img" @click="selectSkill(index)" />
+      <img :src="item.img" @click="handleSelectSkill(index)" />
       <img :src="item.img" :class="{ active: currentIndex === index }" />
     </div>
+
+    <!-- 底部内容 -->
+    <i
+      v-if="hero_data.deputy.length"
+      class="toggle iconfont wzry-qiehuan cursor-pointer"
+      :class="{ 'hide-bottom': !show, deputy: status }"
+      @click="handleToggleSkill"
+      title="切换技能"
+    ></i>
   </div>
 </template>
 <script setup lang="ts">
@@ -25,29 +34,41 @@ import { heroDefault } from "@/defaultValue/defaults";
 import heroStore from "@/store/hero";
 import heroDetailStore from "@/store/heroDetail";
 
+interface Props {
+  status: boolean;
+}
+withDefaults(defineProps<Props>(), {
+  status: false,
+});
+
 const $heroStore = heroStore();
 const $heroDetailStore = heroDetailStore();
 
 const show = ref(false);
+const currentIndex = ref(0); //处于展示的技能索引
 const hero_data = ref<typeof heroDefault>($deepCopy(heroDefault)); //英雄数据
 hero_data.value = $heroStore.hero_info;
-const currentIndex = ref(0); //处于展示的技能索引
 
 $heroDetailStore.setScollFn((index) => {
   if (index === 3) {
     show.value = true;
   }
 });
-
 /* 点击需要展示的技能 */
 const emit = defineEmits<{
   (e: "select-skill", index: number): void;
+  (e: "toggle-skill"): void;
 }>();
 
 /* 点击技能后传递索引号 */
-const selectSkill = (index: number) => {
+const handleSelectSkill = (index: number) => {
   currentIndex.value = index;
   emit("select-skill", index);
+};
+
+/* 切换副技能 */
+const handleToggleSkill = () => {
+  emit("toggle-skill");
 };
 </script>
 <style scoped lang="less">
--- src/views/Hero/childViews/HeroDetail/childComps/HeroSkill/index.vue
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroSkill/index.vue
@@ -1,33 +1,50 @@
 <template>
   <div class="hero-skill">
     <!--技能图标-->
-    <HeroSkillIcon @select-skill="selectSkill" />
+    <HeroSkillIcon @select-skill="EmitSelectSkill" @toggle-skill="EmitToggleSkill" :status="deputy_status" />
 
     <!--主体内容-->
     <transition name="fade">
-      <HeroSkillContent :index="index" />
+      <HeroSkillContent :skill="activeSkill" />
     </transition>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import heroDetailStore from "@/store/heroDetail";
+import heroStore from "@/store/hero";
 import HeroSkillIcon from "./childComps/HeroSkillIcon/index.vue"; //技能图标
 import HeroSkillContent from "./childComps/HeroSkillContent/index.vue"; //主体内容
 
+const $heroStore = heroStore();
 const $heroDetailStore = heroDetailStore();
 
 const toggle = ref(true); //用于切换动画
 const index = ref(0); //当前展示技能的索引
+const hero_data = $heroStore.hero_info;
+const deputy_status = ref(false); //是否使用副技能
 
-/* 切换技能 */
-const selectSkill = (i: number) => {
+/* 处于展示的技能 */
+const activeSkill = computed(() => {
+  if (deputy_status.value) {
+    return hero_data.deputy[index.value];
+  }
+  return hero_data.skills[index.value];
+});
+
+/* 选择技能 */
+const EmitSelectSkill = (i: number) => {
   toggle.value = false;
-  $heroDetailStore.skillToggler();
+  $heroDetailStore.skillToggler(i);
   setTimeout(() => {
     toggle.value = true;
     index.value = i;
   }, 375);
+};
+
+/* 切换副技能 */
+const EmitToggleSkill = () => {
+  deputy_status.value = !deputy_status.value;
 };
 </script>
 <style scoped lang="less">
