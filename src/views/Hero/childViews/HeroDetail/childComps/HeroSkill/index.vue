26e17f9e63c3457c4a873cd0cfd4059ec0383214
lengyibai <1329670984@qq.com>
2022-12-14 22:48:49 +0800

    perf: 将设置展示技能的操作以及副技能相关写入技能图标组件内

--- src/views/Hero/childViews/HeroDetail/childComps/HeroSkill/index.vue
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroSkill/index.vue
@@ -1,50 +1,29 @@
 <template>
   <div class="hero-skill">
     <!--技能图标-->
-    <HeroSkillIcon @select-skill="EmitSelectSkill" @toggle-skill="EmitToggleSkill" :status="deputy_status" />
+    <HeroSkillIcon @select-skill="EmitSelectSkill" />
 
     <!--主体内容-->
     <transition name="fade">
-      <HeroSkillContent :skill="activeSkill" />
+      <HeroSkillContent :skill="hero_skills" v-if="hero_skills" />
     </transition>
   </div>
 </template>
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import heroDetailStore from "@/store/heroDetail";
-import heroStore from "@/store/hero";
+import { ref } from "vue";
 import HeroSkillIcon from "./childComps/HeroSkillIcon/index.vue"; //技能图标
 import HeroSkillContent from "./childComps/HeroSkillContent/index.vue"; //主体内容
 
-const $heroStore = heroStore();
-const $heroDetailStore = heroDetailStore();
-
 const toggle = ref(true); //用于切换动画
-const index = ref(0); //当前展示技能的索引
-const hero_data = $heroStore.hero_info;
-const deputy_status = ref(false); //是否使用副技能
-
-/* 处于展示的技能 */
-const activeSkill = computed(() => {
-  if (deputy_status.value) {
-    return hero_data.deputy[index.value];
-  }
-  return hero_data.skills[index.value];
-});
+const hero_skills = ref<Hero.Skill>();
 
 /* 选择技能 */
-const EmitSelectSkill = (i: number) => {
+const EmitSelectSkill = (skills: Hero.Skill) => {
   toggle.value = false;
-  $heroDetailStore.skillToggler(i);
   setTimeout(() => {
     toggle.value = true;
-    index.value = i;
+    hero_skills.value = skills;
   }, 375);
-};
-
-/* 切换副技能 */
-const EmitToggleSkill = () => {
-  deputy_status.value = !deputy_status.value;
 };
 </script>
 <style scoped lang="less">
