3b011d70518037ce232b3fe1f923078ba12eb534
lengyibai <1329670984@qq.com>
2022-12-14 22:47:47 +0800

    perf: 代码优化、重写切换副技能逻辑、集成选择技能的功能，发送事件参数给父元素

--- src/views/Hero/childViews/HeroDetail/childComps/HeroSkill/childComps/HeroSkillIcon/index.vue
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroSkill/childComps/HeroSkillIcon/index.vue
@@ -4,7 +4,7 @@
       class="icon cursor-pointer"
       :class="{ active: show }"
       ref="skillImg"
-      v-for="(item, index) in hero_data.skills"
+      v-for="(item, index) in active_skills"
       :style="{
         'transition-delay': 0.05 * index + 's',
       }"
@@ -19,56 +19,70 @@
 
     <!-- 底部内容 -->
     <i
-      v-if="hero_data.deputy.length"
+      v-if="hero_data.skills.length > 1"
       class="toggle iconfont wzry-qiehuan cursor-pointer"
-      :class="{ 'hide-bottom': !show, deputy: status }"
+      :class="{ 'hide-bottom': !show }"
       @click="handleToggleSkill"
       title="切换技能"
     ></i>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
-import { $deepCopy } from "@/utils";
-import { heroDefault } from "@/defaultValue/defaults";
+import { ref, toRef, computed } from "vue";
 import heroStore from "@/store/hero";
 import heroDetailStore from "@/store/heroDetail";
-
-interface Props {
-  status: boolean;
-}
-withDefaults(defineProps<Props>(), {
-  status: false,
-});
 
 const $heroStore = heroStore();
 const $heroDetailStore = heroDetailStore();
 
-const show = ref(false);
+const show = ref(false); //是否显示技能
 const currentIndex = ref(0); //处于展示的技能索引
-const hero_data = ref<typeof heroDefault>($deepCopy(heroDefault)); //英雄数据
-hero_data.value = $heroStore.hero_info;
+const hero_data = toRef($heroStore, "hero_info"); //设置英雄详情
+const deputy_index = ref(0); //主副技能索引
+const active_skills = ref<Hero.Skill[]>([]); //展示的技能组
+active_skills.value = hero_data.value.skills[deputy_index.value];
 
-$heroDetailStore.setScollFn((index) => {
-  if (index === 3) {
-    show.value = true;
-  }
+//处于展示的技能
+const calcActiveSkill = computed(() => {
+  return active_skills.value[currentIndex.value];
 });
+
+/* 设置需要滚动触发的函数 */
+$heroDetailStore.setScollFn((index) => {
+  show.value = index === 3;
+});
+
 /* 点击需要展示的技能 */
 const emit = defineEmits<{
-  (e: "select-skill", index: number): void;
-  (e: "toggle-skill"): void;
+  (e: "select-skill", skills: Hero.Skill): void;
 }>();
 
 /* 点击技能后传递索引号 */
 const handleSelectSkill = (index: number) => {
   currentIndex.value = index;
-  emit("select-skill", index);
+  $heroDetailStore.skillToggler(index);
+  emit("select-skill", calcActiveSkill.value);
 };
+handleSelectSkill(0);
 
 /* 切换副技能 */
 const handleToggleSkill = () => {
-  emit("toggle-skill");
+  // 判断当前切换是否为最后一个副技能
+  if (deputy_index.value === hero_data.value.skills.length - 1) {
+    deputy_index.value = 0;
+  } else {
+    deputy_index.value += 1;
+  }
+  // 设置处于展示的技能组
+  active_skills.value = hero_data.value.skills[deputy_index.value];
+  // 如果当前技能留空，则使用主技能
+  active_skills.value.forEach((item, index) => {
+    if (!item.name) {
+      active_skills.value[index] = hero_data.value.skills[0][index];
+    }
+  });
+  // 更新技能信息
+  handleSelectSkill(currentIndex.value);
 };
 </script>
 <style scoped lang="less">
