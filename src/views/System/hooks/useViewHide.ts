b5bd5e61b83d4e8c8694c8fbe0baac165f74e491
lengyibai <1329670984@qq.com>
2022-12-15 00:31:06 +0800

    rename: 事件命名

--- src/views/System/hooks/useViewHide.ts
+++ src/views/System/hooks/useViewHide.ts
@@ -1,8 +1,8 @@
 import { ref } from "vue";
 
 export default <T>(emit: (event: "update:modelValue", ...args: any[]) => void, key: string) => {
-  const show = ref(false);
-  const timer = ref<Interval>();
+  const show = ref(false); //是否显示页面
+  const timer = ref<Interval>(); //自动保存计时器
   const finish = ref(false); //是否发布成功
   const status = ref(0); //发布状态
   const hero_id = ref<number | undefined>(); //英雄id
@@ -27,20 +27,28 @@ export default <T>(emit: (event: "update:modelValue", ...args: any[]) => void, k
     );
   }, 1000);
 
-  /* 点击关闭显示确认关闭弹窗 */
-  const cancel = () => {
+  /* 取消发布 */
+  const EmitCancelRelease = () => {
     show_ConfirmClose.value = true;
   };
 
-  const close = (status = false) => {
+  /* 关闭后操作 */
+  const close = () => {
     clearInterval(timer.value);
     show.value = false;
     setTimeout(() => {
-      if (!status) {
-        localStorage.removeItem(key);
-      }
       emit("update:modelValue", false);
     }, 500);
+  };
+
+  /* 关闭前保存 */
+  const EmitConfirmSave = () => {
+    close();
+  };
+  /* 关闭后删除 */
+  const EmitConfirmRemove = () => {
+    localStorage.removeItem(key);
+    close();
   };
   return {
     show,
@@ -50,7 +58,8 @@ export default <T>(emit: (event: "update:modelValue", ...args: any[]) => void, k
     timer,
     form_data,
     hero_id,
-    cancel,
-    close,
+    EmitCancelRelease,
+    EmitConfirmRemove,
+    EmitConfirmSave,
   };
 };
