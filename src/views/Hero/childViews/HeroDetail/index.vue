a3e1cffc44b3e4d05bff754c5022ce28e3a89539
lengyibai <1329670984@qq.com>
2022-12-15 00:12:22 +0800

    perf: 优化事件命名、关闭详情页后加载英雄列表、

--- src/views/Hero/childViews/HeroDetail/index.vue
+++ src/views/Hero/childViews/HeroDetail/index.vue
@@ -1,36 +1,32 @@
 <template>
   <div class="hero-detail">
     <!-- 顶部关闭 -->
-    <LibMaskClose @close="hide" />
-    <LibFullScroll v-model="scroll_index" @end="scrollEnd">
+    <LibMaskClose @close="EmitHide" />
+    <LibFullScroll v-model="scroll_index" @end="EmitScrollEnd">
       <!--资料皮肤-->
       <HeroParallax class="scroll-item" :bg="hero_data.poster">
         <HeroInfo />
       </HeroParallax>
 
       <!--皮肤-->
-      <HeroParallax class="scroll-item" :bg="hero_data.poster" v-if="hero_data.skins.length">
+      <HeroParallax class="scroll-item" :bg="hero_data.poster" v-if="skin_num">
         <HeroSkin />
       </HeroParallax>
 
       <!--技能-->
-      <HeroParallax
-        class="scroll-item"
-        :bg="hero_data.skins[hero_data.skins.length - 1].poster"
-        v-if="hero_data.skills.length"
-      >
+      <HeroParallax class="scroll-item" :bg="hero_data.skins[skin_num - 1].poster" v-if="skill_num">
         <HeroSkill />
       </HeroParallax>
     </LibFullScroll>
 
     <!-- 滚动进度 -->
     <transition name="progress">
-      <Heroprogress v-show="show_progress" :index="scroll_index" :pageName="page_name" @toggle="toggle" />
+      <Heroprogress v-show="show_progress" :index="scroll_index" :pageName="page_name" @toggle="EmitToggle" />
     </transition>
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed } from "vue";
 import { useRouter } from "vue-router";
 import heroStore from "@/store/hero";
 import heroDetailStore from "@/store/heroDetail";
@@ -43,7 +39,6 @@ import HeroSkill from "./childComps/HeroSkill/index.vue"; //技能页
 interface Emits {
   (e: "update:modelValue", v: boolean): void;
 }
-
 const emit = defineEmits<Emits>();
 
 const $router = useRouter();
@@ -53,29 +48,44 @@ const $heroDetailStore = heroDetailStore();
 const scroll_index = ref(1); //滚动索引
 const show_progress = ref(false); //显示滚动索引组件
 const hero_data = $heroStore.hero_info; //英雄信息
-const page_name = ["英雄资料", "皮肤鉴赏", "技能信息"];
+const page_name = ["英雄资料", "皮肤鉴赏", "技能信息"]; //滚动索引标题
+
+//技能数量
+const skill_num = computed(() => {
+  return hero_data.skills[0].length;
+});
+
+//皮肤数量
+const skin_num = computed(() => {
+  return hero_data.skins.length;
+});
 
 /* 点击滚动索引 */
-const toggle = (index: number) => {
+const EmitToggle = (index: number) => {
   scroll_index.value = index;
 };
-
 /* 滚动结束触发 */
-const scrollEnd = (index: number) => {
+const EmitScrollEnd = (index: number) => {
   $heroDetailStore.setIndex(index);
 };
-
 /* 隐藏自身 */
-const hide = () => {
+const EmitHide = () => {
   emit("update:modelValue", false);
   $router.replace("/hero");
   $heroDetailStore.setSkinVoice("盾山"); //置空语音
+
+  if ($heroStore.profession === "") {
+    setTimeout(() => {
+      $heroStore.getHeroList(hero_data.profession[0]);
+    }, 1500);
+  }
 };
 
 onMounted(() => {
+  //延迟显示滚动索引
   setTimeout(() => {
     show_progress.value = true;
-  }, 2000);
+  }, 1500);
 });
 </script>
 <style scoped lang="less">
