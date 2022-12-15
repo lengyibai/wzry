a471e9036c6d1e6bde9aa78cf86185400d01ffb3
lengyibai <1329670984@qq.com>
2022-12-15 22:31:27 +0800

    remove: 移除element ui组件

--- src/components/ErrorMessage/404.vue
+++ src/components/ErrorMessage/404.vue
@@ -4,16 +4,11 @@
     <div class="not-detail">
       <h2>404</h2>
       <h4>抱歉，您访问的页面不存在~🤷‍♂️🤷‍♀️</h4>
-      <el-button type="primary" @click="router.push(HOME_URL)">返回首页</el-button>
     </div>
   </div>
 </template>
 
-<script setup lang="ts" name="404">
-import { useRouter } from "vue-router";
-import { HOME_URL } from "@/config/config";
-const router = useRouter();
-</script>
+<script setup lang="ts" name="404"></script>
 
 <style scoped lang="less">
 @import "./index.less";
--- src/components/ErrorMessage/500.vue
+++ src/components/ErrorMessage/500.vue
@@ -4,16 +4,11 @@
     <div class="not-detail">
       <h2>500</h2>
       <h4>抱歉，您的网络不见了~🤦‍♂️🤦‍♀️</h4>
-      <el-button type="primary" @click="router.push(HOME_URL)">返回首页</el-button>
     </div>
   </div>
 </template>
 
-<script setup lang="ts" name="500">
-import { useRouter } from "vue-router";
-import { HOME_URL } from "@/config/config";
-const router = useRouter();
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="less">
 @import "./index.less";
