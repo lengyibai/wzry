00244deb7c648095ddf24466e03de777a8819221
lengyibai <1329670984@qq.com>
2022-12-16 14:54:29 +0800

    fix: 修复技能查找失败返回为空造成报错

--- src/api/main/games/skill.ts
+++ src/api/main/games/skill.ts
@@ -3,7 +3,7 @@ import { get, post } from "@/api/helper/transfer";
 /** @description: 获取指定英雄技能 */
 export const getHeroSkill = (id: number) => {
   const data = get<Hero.SkillParams>({ name: "data_skill", key: "id", value: id });
-  return Promise.resolve(data.skills || []);
+  return Promise.resolve(data?.skills || []);
 };
 
 /** @description: 添加英雄技能 */
