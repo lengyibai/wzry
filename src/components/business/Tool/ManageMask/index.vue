ecc7e8a6f47b70028fec37da91549270fbcb39bf
lengyibai <1329670984@qq.com>
2022-12-16 14:56:31 +0800

    feat: 增加系统管理打开增删改页面的蒙版组件

---
+++ src/components/business/Tool/ManageMask/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <transition name="fade">
+    <div class="manage-mask">
+      <transition name="fade">
+        <div class="content" v-if="show" :style="styles">
+          <slot></slot>
+        </div>
+      </transition>
+    </div>
+  </transition>
+</template>
+<script setup lang="ts">
+import { CSSProperties } from "vue";
+
+interface Props {
+  show: boolean;
+  styles?: CSSProperties;
+}
+withDefaults(defineProps<Props>(), {
+  show: false,
+});
+</script>
+<style scoped lang="less">
+@import url("./index.less");
+</style>
