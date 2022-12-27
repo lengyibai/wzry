9864d545379a0e9261ae14999eaea8e8b691044e
lengyibai <1329670984@qq.com>
2022-12-28 01:13:44 +0800

    feat: 增加滚动加载

--- src/components/common/LibGridLayout/index.vue
+++ src/components/common/LibGridLayout/index.vue
@@ -2,13 +2,14 @@
   <div
     class="LibGridLayout"
     ref="LibGridLayout"
+    @scroll.passive="scroll"
     :style="{ gridTemplateColumns: 'repeat(' + count + ', 1fr)', gridGap: gap }"
   >
     <slot></slot>
   </div>
 </template>
-<script setup>
-import { onBeforeMount, onMounted, ref, toRefs } from "vue";
+<script setup lang="ts">
+import { onBeforeMount, onMounted, ref, toRefs, nextTick } from "vue";
 
 const props = defineProps({
   count: {
@@ -27,7 +28,7 @@ const props = defineProps({
 
 const { eqhMultiple } = toRefs(props);
 
-let childrens = [];
+let childrens: HTMLElement[] = [];
 const LibGridLayout = ref();
 const fn = () => {
   requestAnimationFrame(() => {
@@ -37,17 +38,32 @@ const fn = () => {
   });
 };
 const updateHeight = () => {
-  childrens.forEach((item) => {
-    item.style.height = `${item.scrollWidth * eqhMultiple.value}px`;
+  childrens = [...LibGridLayout.value.children];
+  nextTick(() => {
+    childrens.forEach((item) => {
+      item.style.height = `${item.scrollWidth * eqhMultiple.value}px`;
+    });
+    window.addEventListener("resize", fn);
   });
-  window.addEventListener("resize", fn);
+};
+
+interface Emits {
+  (e: "load-more"): void;
+}
+const emit = defineEmits<Emits>();
+let lock = false;
+const scroll = (e: Event) => {
+  const el = e.target as HTMLElement;
+
+  if (el.scrollHeight < el.scrollTop + el.clientHeight * 1.5 && !lock) {
+    emit("load-more");
+  }
 };
 
 onMounted(() => {
-  childrens = [...LibGridLayout.value.children];
-  let a = null;
+  let a: any = null;
   (function fn() {
-    if (eqhMultiple.value > 0 && childrens?.length) {
+    if (eqhMultiple.value > 0 && childrens?.length === 0) {
       updateHeight();
     }
     a = requestAnimationFrame(fn);
@@ -55,7 +71,7 @@ onMounted(() => {
   // 3秒后还未获取到插槽元素，则取消获取
   setTimeout(() => {
     cancelAnimationFrame(a);
-  }, 3000);
+  }, 5000);
 });
 
 onBeforeMount(() => {
