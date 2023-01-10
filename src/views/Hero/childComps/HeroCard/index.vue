25a17fbb154cb8ff45835587c12eeb7a5d20dc4e
lengyibai <1329670984@qq.com>
2023-01-11 01:07:46 +0800

    feat: 增加身高和皮肤数量显示

--- src/views/Hero/childComps/HeroCard/index.vue
+++ src/views/Hero/childComps/HeroCard/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { heroDefault } from "@/defaultValue/defaults";
+import heroStore from "@/store/hero";
 
 interface Props {
   data: typeof heroDefault; //英雄数据
@@ -15,8 +16,23 @@ withDefaults(defineProps<Props>(), {
 
 const emit = defineEmits<Emits>();
 
+const $heroStore = heroStore();
+
 const show = ref(false); //显示查看详情选项
 const lineActive = ref(false); //悬浮文字底部线条
+
+//用于身高和皮肤数量排序显示相应数字
+const num_type = computed(() => {
+  return $heroStore.misc_sort;
+});
+//是否显示右上角数字
+const show_num = computed(() => ["身高", "皮肤数量"].includes(num_type.value));
+const num = (data: Hero.Data) =>
+  num_type.value === "身高"
+    ? data.height + "cm"
+    : num_type.value === "皮肤数量"
+    ? data.skins!.length + "款"
+    : "";
 
 /* 查看详情 */
 const handleViewClick = () => {
@@ -34,6 +50,7 @@ const handleViewClick = () => {
     @mouseleave="show = false"
   >
     <span class="id">No.{{ data.id }}</span>
+    <span class="num" v-if="show_num">{{ num(data) }}</span>
     <transition name="fade">
       <div class="select-mask" v-if="show">
         <img :src="data.headImg" class="head" />
