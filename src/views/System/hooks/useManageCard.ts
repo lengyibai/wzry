97d457d82ec08d823890c689c2aa727cbea1761a
lengyibai <1329670984@qq.com>
2023-01-11 20:21:16 +0800

    delete: 删除铭文管理

--- src/views/System/hooks/useManageCard.ts
+++ src/views/System/hooks/useManageCard.ts
@@ -17,17 +17,9 @@ const data: Info = {
   list: {
     Hero: "英雄",
     Skin: "皮肤",
-    Voice: "语音",
     Skill: "技能",
     Equip: "装备",
-    Epigraph: "铭文",
-    /* { name: "英雄关系", key: "" },
-    { name: "关系类型", key: "" },
-    { name: "技能类型", key: "" },
-    { name: "技能效果", key: "" },
-    { name: "装备类型", key: "" },
-    { name: "铭文类型", key: "" },
-    { name: "铭文效果", key: "" }, */
+    // Voice: "语音",
   },
 };
 
