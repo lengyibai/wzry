0842d846dfecb4133520e8178a9aebf94f4f0839
lengyibai <1329670984@qq.com>
2023-01-11 20:29:25 +0800

    fix: 修复语音列表获取元素报错

--- src/views/Hero/childViews/HeroDetail/childComps/HeroSkin/childComps/HeroVoice/index.vue
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroSkin/childComps/HeroVoice/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, nextTick } from "vue";
+import { ref, nextTick, onMounted } from "vue";
 import heroDetailStore from "@/store/heroDetail";
 
 const $heroDetailStore = heroDetailStore();
@@ -12,27 +12,29 @@ const time = ref(0); //当前播放时长
 const current_index = ref(-1); //当前播放索引
 const voices = ref<Hero.Voice[]>([]); //英雄数据
 
-$heroDetailStore.setSkinToggleFn(async (hero_name, skin_name) => {
-  await $heroDetailStore.setSkinVoice(hero_name, skin_name);
-  if (voice_length === $heroDetailStore.voice.length) return;
-  voice_length = $heroDetailStore.voice.length;
-  nextTick(() => {
-    voiceRef.value.forEach((item: HTMLElement, index: number) => {
-      /* 决定是从左还是从右入场 */
-      if (index % 2) {
-        item.style.transform = "translateX(-100%) translateY(500%) scale(0)";
-      } else {
-        item.style.transform = "translateX(100%) translateY(500%) scale(0)";
-      }
-      setTimeout(() => {
-        voices.value = $heroDetailStore.voice;
-        item.style.transitionDelay = `${index / 15}s`; //入场间隔
-        item.style.transform = "translateX(0%) translateY(0%) scale(1)";
-        //动画结束后初始化
+onMounted(() => {
+  $heroDetailStore.setSkinToggleFn(async (hero_name, skin_name) => {
+    await $heroDetailStore.setSkinVoice(hero_name, skin_name);
+    if (voice_length === $heroDetailStore.voice.length) return;
+    voice_length = $heroDetailStore.voice.length;
+    nextTick(() => {
+      voiceRef.value.forEach((item: HTMLElement, index: number) => {
+        /* 决定是从左还是从右入场 */
+        if (index % 2) {
+          item.style.transform = "translateX(-100%) translateY(500%) scale(0)";
+        } else {
+          item.style.transform = "translateX(100%) translateY(500%) scale(0)";
+        }
         setTimeout(() => {
-          item.style.transitionDelay = "0s";
+          voices.value = $heroDetailStore.voice;
+          item.style.transitionDelay = `${index / 15}s`; //入场间隔
+          item.style.transform = "translateX(0%) translateY(0%) scale(1)";
+          //动画结束后初始化
+          setTimeout(() => {
+            item.style.transitionDelay = "0s";
+          }, 500);
         }, 500);
-      }, 500);
+      });
     });
   });
 });
