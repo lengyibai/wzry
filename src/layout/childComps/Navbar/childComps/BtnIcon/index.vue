e3c67075f18ddb93fb4687855b054a82f228a396
lengyibai <1329670984@qq.com>
2023-01-12 19:44:17 +0800

    refactor: 导航栏按钮组件抽离

---
+++ src/layout/childComps/Navbar/childComps/BtnIcon/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import switchStore from "@/store/globalSwitch";
+
+const $switchStore = switchStore();
+</script>
+
+<template>
+  <div class="btn-icon">
+    <i
+      class="iconfont wzry-setting cursor-pointer"
+      @click="$switchStore.$tip('开发中...', 'warning')"
+      title="设置"
+    />
+    <a href="https://github.com/lengyibai/wzry" target="_blank"
+      ><i class="iconfont wzry-mark-github" title="Github"
+    /></a>
+  </div>
+</template>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
--- src/layout/childComps/Navbar/index.vue
+++ src/layout/childComps/Navbar/index.vue
@@ -4,27 +4,18 @@
     <PackUp />
 
     <div class="right">
-      <!-- 用户操作 -->
+      <!-- 用户菜单 -->
       <UserMenu />
 
-      <!-- 设置 -->
-      <i
-        class="iconfont wzry-setting cursor-pointer"
-        @click="$switchStore.$tip('开发中...', 'warning')"
-        title="设置"
-      />
-      <a href="https://github.com/lengyibai/wzry" target="_blank"
-        ><i class="iconfont wzry-mark-github" title="Github"
-      /></a>
+      <!-- 图标按钮 -->
+      <BtnIcon />
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import switchStore from "@/store/globalSwitch";
 import PackUp from "./childComps/PackUp/index.vue"; //折叠侧边栏按钮
 import UserMenu from "./childComps/UserMenu/index.vue"; //用户卡片
-
-const $switchStore = switchStore();
+import BtnIcon from "./childComps/BtnIcon/index.vue"; //图标按钮
 </script>
 <style lang="less" scoped>
 @import url("./index.less");
