240c77437ae381763381deae1027f73d23f05ece
lengyibai <1329670984@qq.com>
2023-01-13 12:25:52 +0800

    feat: 增加独立的选中按钮

---
+++ src/components/business/Parts/K-Check/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="K-Check cursor-pointer" @click="toggle">
+    <img :src="icon" />
+    <span :class="{ active: modelValue }">开启</span>
+  </div>
+</template>
+<script setup lang="ts">
+import { computed } from "vue";
+
+interface Props {
+  modelValue: boolean | string; //选中状态
+}
+interface Emits {
+  (e: "update:modelValue", v: boolean): void;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: false,
+});
+
+const emit = defineEmits<Emits>();
+
+const icon = computed(
+  () =>
+    `https://lengyibai.gitee.io/wzry-material/image/${
+      props.modelValue ? "select_true" : "select_false"
+    }.png`
+);
+
+const toggle = () => {
+  emit("update:modelValue", !props.modelValue);
+};
+</script>
+<style scoped lang="less">
+@import url("./index.less");
+</style>
