40c6330a28943235438354dcd6a27d7805e77333
lengyibai <1329670984@qq.com>
2023-01-13 22:21:01 +0800

    feat: 登录页右上角增加工具栏

---
+++ src/views/Base/Login/childComps/ToolBar/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+interface Emits {
+  (e: "clicks", v: string): void;
+}
+const emit = defineEmits<Emits>();
+
+const muted = ref(false);
+
+const icon = computed(() => {
+  return muted.value ? "wzry-jingyin" : "wzry-laba";
+});
+
+/* 点击某个按钮 */
+const handleShowNotice = (v: string) => {
+  emit("clicks", v);
+  if (v === "sound") {
+    muted.value = !muted.value;
+  }
+};
+</script>
+
+<template>
+  <div class="tool-bar">
+    <div class="line"></div>
+    <div
+      class="box cursor-pointer"
+      @click="handleShowNotice('sound')"
+      :style="{ opacity: muted ? 0.75 : 1 }"
+    >
+      <i class="iconfont" :class="icon"></i>
+    </div>
+    <div class="line"></div>
+    <div class="box cursor-pointer" @click="handleShowNotice('notice')">
+      <i class="iconfont wzry-gonggao"></i>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
