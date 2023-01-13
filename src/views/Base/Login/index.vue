e77a3c75dc7d6e90d82f8b9df18cb1369350d719
lengyibai <1329670984@qq.com>
2023-01-13 22:21:59 +0800

    feat: 右上角增加工具栏

--- src/views/Base/Login/index.vue
+++ src/views/Base/Login/index.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { LOGINBG } from "@/config/assets";
-import Login from "./childComp/Login/index.vue"; //登录盒子
-import LoginUpdate from "./childComp/LoginUpdate/index.vue"; //公告
+import Login from "./childComps/Login/index.vue"; //登录盒子
+import Notice from "./childComps/Notice/index.vue"; //公告
+import ToolBar from "./childComps/ToolBar/index.vue"; //工具栏
 
-const show_KDialog = ref(true); //是否显示公告
+const show_notice = ref(true); //是否显示公告
+const muted = ref(false); //是否静音
+
+const EmitToolType = (v: string) => {
+  if (v === "sound") {
+    muted.value = !muted.value;
+  } else if (v === "notice") {
+    show_notice.value = true;
+  }
+};
 </script>
 
 <template>
@@ -16,9 +26,10 @@ const show_KDialog = ref(true); //是否显示公告
       />
     </div>
 
+    <ToolBar @clicks="EmitToolType" />
     <Login />
-    <LibBgVideo :video="LOGINBG" />
-    <LoginUpdate v-model="show_KDialog" />
+    <LibBgVideo :video="LOGINBG" :muted="muted" />
+    <Notice v-model="show_notice" />
   </div>
 </template>
 
