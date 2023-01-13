96cb954ceae2cfeeb4c30b794155963d796242b6
lengyibai <1329670984@qq.com>
2023-01-13 12:33:52 +0800

    feat: 增加主题、动画速率、点击音效、背景音乐、视频背景、小贴士、列表分页展示配置项

--- src/store/setting.ts
+++ src/store/setting.ts
@@ -2,7 +2,27 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 const switchStore = defineStore("setting", () => {
-  return {};
+  const config = ref({
+    tip: true,
+    videoBg: false,
+    audio: true,
+    audioVolume: 50,
+    music: true,
+    musicVolume: 50,
+    lazy: true,
+    theme: 0,
+    speed: 1,
+  });
+
+  const data = localStorage.getItem("config");
+  if (data) config.value = { ...config.value, ...JSON.parse(data) };
+
+  /** @description: 保存设置 */
+  const saveConfig = (v: any) => {
+    config.value = v;
+    localStorage.setItem("config", JSON.stringify(v));
+  };
+  return { config, saveConfig };
 });
 
 export default switchStore;
