12f3a54457509455a6bb066753cdfd9d96e45d44
lengyibai <1329670984@qq.com>
2023-01-13 12:28:47 +0800

    feat: 增加多选按钮

---
+++ src/components/business/Parts/K-Select/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+interface Props {
+  modelValue: number;
+  option: string[];
+}
+interface Emits {
+  (e: "update:modelValue", v: number): void;
+}
+
+withDefaults(defineProps<Props>(), {
+  modelValue: 0,
+  option: () => [],
+});
+const emit = defineEmits<Emits>();
+
+/* 选择 */
+const handleSelect = (index: number) => {
+  emit("update:modelValue", index);
+};
+</script>
+
+<template>
+  <div class="k-select">
+    <div
+      class="option cursor-pointer"
+      :class="{ active: modelValue === index }"
+      v-for="(item, index) in option"
+      @click="handleSelect(index)"
+      :key="index"
+    >
+      {{ item }}
+    </div>
+    <div
+      class="active-bg"
+      :style="{ transform: 'translateX(calc(' + modelValue + ' * 100%))' }"
+    ></div>
+  </div>
+</template>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
