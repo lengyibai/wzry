1ba73861769e5e4d0f30262be033915179001eb2
lengyibai <1329670984@qq.com>
2023-01-13 12:30:03 +0800

    feat: 增加问号提示框

---
+++ src/layout/childComps/Navbar/childComps/BtnIcon/childComps/DescSet/index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface Props {
+  desc: string;
+}
+withDefaults(defineProps<Props>(), {
+  desc: "",
+});
+
+const show_desc = ref(false); // 显示tip
+</script>
+
+<template>
+  <div class="desc-set">
+    <transition name="desc-set">
+      <div
+        class="desc-tip"
+        v-show="show_desc"
+        @mouseenter="show_desc = true"
+        @mouseleave="show_desc = false"
+      >
+        {{ desc }}
+      </div>
+    </transition>
+    <div
+      class="question-icon cursor-pointer"
+      @mouseenter="show_desc = true"
+      @mouseleave="show_desc = false"
+    >
+      <span>?</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
