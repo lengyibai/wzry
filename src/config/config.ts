b5ee9f5cd9f0e1ce66577c959c4adfd8a050c86e
lengyibai <1329670984@qq.com>
2023-01-13 22:17:08 +0800

    remove: 删除全局帐号

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,10 +3,6 @@
 /** @description: 登录页地址（默认） */
 export const LOGIN_URL = "/login";
 
-/** @description: 登录账号 */
-export const ACCOUNT = 1329670984;
-// export const ACCOUNT = 3080572703;
-
 /** @description: 本地版本 */
 export const LOCAL_VERSION = "23.01.11.01";
 
