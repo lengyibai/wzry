3835d5966820b5ccdddc72b6b031423b1a3bb7b7
lengyibai <1329670984@qq.com>
2023-01-14 11:47:08 +0800

    remove: 删除多余代码

--- src/hooks/useAutoLogin.ts
+++ src/hooks/useAutoLogin.ts
@@ -1,21 +1,15 @@
 import authStore from "@/store/auth";
 import routesStore from "@/store/routes";
-import switchStore from "@/store/globalSwitch";
 
 const addRouter = () => {
   const user = localStorage.getItem("user");
 
   const $authStore = authStore();
   const $routesStore = routesStore();
-  const $switchStore = switchStore();
   if (user) {
     const userInfo = JSON.parse(user) as User;
     $authStore.setUserInfo(userInfo);
     $routesStore.addRoutes(userInfo.role!);
-    $authStore.watchStatus();
-  } else {
-    $switchStore.$tip("身份校验失败，请重新登录", "error");
-    $authStore.clearToken();
   }
 };
 
