930a6981f2a9a3653b9ae834b3cf27ff63af0d44
lengyibai <1329670984@qq.com>
2023-01-16 01:23:46 +0800

    feat: 音乐及音量、音效及音量、视频背景、重置配置生效

--- src/layout/childComps/Navbar/childComps/BtnIcon/index.vue
+++ src/layout/childComps/Navbar/childComps/BtnIcon/index.vue
@@ -4,6 +4,7 @@ import settingStore from "@/store/setting";
 import musicStore from "@/store/music";
 import clickAudio from "@/store/clickAudio";
 import switchStore from "@/store/globalSwitch";
+import { $debounce } from "@/utils";
 import DescSet from "./childComps/DescSet/index.vue"; //悬浮问号显示tip
 
 const $settingStore = settingStore();
@@ -11,26 +12,60 @@ const $musicStore = musicStore();
 const $switchStore = switchStore();
 const $clickAudio = clickAudio();
 
+// 默认配置
+const default_config = {
+  tip: true,
+  videoBg: true,
+  audio: true,
+  audioVolume: 50,
+  music: true,
+  musicVolume: 50,
+  lazy: true,
+  theme: 0,
+  speed: 1,
+  loginSound: true,
+};
+
 const show_setting = ref(false); //是否显示设置弹窗
+const show_confirm_reset = ref(false); //是否显示确认重置弹窗
 const config = ref<SettingConfig>($settingStore.config);
 
-config.value = JSON.parse(JSON.stringify($settingStore.config));
-
-/* 本地配置生效 */
+/* 本地配置立即生效 */
 const setTakeEffect = () => {
   $musicStore.setVolume(config.value.musicVolume);
   $clickAudio.setVolume(config.value.audioVolume);
 };
 setTakeEffect();
 
-/* 背景音乐音量调节 */
+/* 开启音乐 */
+const EmitMusic = (v: boolean) => {
+  EmitSaveConfig();
+  if (!v) $musicStore.pause();
+};
+/* 音乐音量调节 */
 const EmitMusicVolume = (v: number) => {
   $musicStore.setVolume(v);
+  $debounce(() => {
+    EmitSaveConfig();
+  }, 1000);
 };
 
-/* 点击音效音量调节 */
+/* 开启音效 */
+const EmitAudio = (v: boolean) => {
+  $clickAudio.setAudio(v);
+  EmitSaveConfig();
+};
+/* 音效音量调节 */
 const EmitAudioVolume = (v: number) => {
   $clickAudio.setVolume(v);
+  $debounce(() => {
+    EmitSaveConfig();
+  }, 1000);
+};
+
+/* 视频背景 */
+const EmitVideoBg = () => {
+  EmitSaveConfig();
 };
 
 /* 显示设置弹窗 */
@@ -42,7 +77,14 @@ const handleShowSetting = () => {
 /* 保存配置 */
 const EmitSaveConfig = () => {
   $settingStore.saveConfig(config.value!);
-  $switchStore.$msg("本地配置已更新");
+};
+
+/* 重置配置 */
+const EmitResetConfig = () => {
+  $settingStore.saveConfig(default_config);
+  config.value = default_config;
+  setTakeEffect();
+  $switchStore.$msg("已重置所有配置项");
 };
 </script>
 
@@ -60,7 +102,6 @@ const EmitSaveConfig = () => {
     <transition name="fade">
       <K-Dialog
         v-if="show_setting"
-        @close="EmitSaveConfig"
         title="仅展示，功能尚未开发"
         width="920px"
         v-model="show_setting"
@@ -80,28 +121,28 @@ const EmitSaveConfig = () => {
           />
         </div>
 
-        <!-- 点击音效音量 -->
+        <!-- 音效 -->
         <div class="option">
-          <div class="label">点击音效音量</div>
+          <div class="label">音效</div>
           <K-Range
             @update:model-value="EmitAudioVolume"
             v-model="config.audioVolume"
             :text="config.audioVolume + '%'"
             :disabled="!config.audio"
           />
-          <K-Check v-model="config.audio" />
+          <K-Check v-model="config.audio" @change="EmitAudio" />
         </div>
 
-        <!-- 背景音乐音量 -->
+        <!-- 音乐 -->
         <div class="option">
-          <div class="label">背景音乐音量</div>
+          <div class="label">音乐</div>
           <K-Range
             v-model="config.musicVolume"
             @update:model-value="EmitMusicVolume"
             :text="config.musicVolume + '%'"
             :disabled="!config.music"
           />
-          <K-Check v-model="config.music" />
+          <K-Check v-model="config.music" @change="EmitMusic" />
         </div>
 
         <!-- 视频背景 -->
@@ -112,7 +153,7 @@ const EmitSaveConfig = () => {
               desc="默认为图片背景是为了解决手机端部分浏览器使用视频背景会全屏遮挡的问题（PC端默认为视频背景）"
             />
           </div>
-          <K-Check v-model="config.videoBg" />
+          <K-Check v-model="config.videoBg" @change="EmitVideoBg" />
         </div>
 
         <!-- 小贴士 -->
@@ -136,8 +177,23 @@ const EmitSaveConfig = () => {
           </div>
           <K-Check v-model="config.lazy" />
         </div>
+
+        <!-- 重置配置 -->
+        <K-Button
+          v-if="show_setting"
+          type="error"
+          @click="show_confirm_reset = true"
+          >重置配置</K-Button
+        >
       </K-Dialog>
     </transition>
+
+    <!-- 确认重置 -->
+    <ConfirmClose
+      v-model="show_confirm_reset"
+      @confirm="EmitResetConfig"
+      text="确定重置所有配置项？"
+    />
   </div>
 </template>
 
