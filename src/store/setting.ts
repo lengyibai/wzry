c23c67b45c64435cf3826b625460a3d6a7ba7871
lengyibai <1329670984@qq.com>
2023-01-16 02:10:36 +0800

    feat: 增加对移动端的判断，决定是否禁用视频背景

--- src/store/setting.ts
+++ src/store/setting.ts
@@ -1,14 +1,15 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { $isPhone } from "@/utils";
 
 const switchStore = defineStore("setting", () => {
   const config = ref<SettingConfig>({
     tip: true,
-    videoBg: false,
+    videoBg: true,
     audio: true,
     audioVolume: 50,
     music: true,
-    musicVolume: 0.5,
+    musicVolume: 50,
     lazy: true,
     theme: 0,
     speed: 1,
@@ -18,11 +19,16 @@ const switchStore = defineStore("setting", () => {
   const data = localStorage.getItem("config");
   if (data) config.value = { ...config.value, ...JSON.parse(data) };
 
+  if ($isPhone) {
+    config.value.videoBg = false;
+  }
+
   /** @description: 保存设置 */
   const saveConfig = (v: SettingConfig) => {
     config.value = { ...config.value, ...v };
     localStorage.setItem("config", JSON.stringify(config.value));
   };
+
   return { config, saveConfig };
 });
 
