badf19e52183383c3a8236d7c0df316d68aacb00
lengyibai <1329670984@qq.com>
2023-01-17 01:46:01 +0800

    feat: 动画独立使用

--- src/App.vue
+++ src/App.vue
@@ -50,4 +50,33 @@ const { LOCAL_VERSION, REMOTE_VERSION } = useVersion();
     }
   }
 }
+
+/* 圆形路由跳转 */
+.round-clip-enter-active {
+  animation: round-clip-in 0.75s;
+}
+
+.round-clip-leave-active {
+  animation: round-clip-out 1.25s;
+}
+
+@keyframes round-clip-in {
+  0% {
+    clip-path: circle(0% at 50% 50%);
+  }
+
+  100% {
+    clip-path: circle(100% at 50% 50%);
+  }
+}
+
+@keyframes round-clip-out {
+  0% {
+    clip-path: circle(100% at 50% 50%);
+  }
+
+  100% {
+    clip-path: circle(0% at 50% 50%);
+  }
+}
 </style>
