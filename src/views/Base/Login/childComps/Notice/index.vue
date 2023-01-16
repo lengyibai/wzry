6ec32d8dc6aa32d9b65f20f2cfab04c7b1f9d867
lengyibai <1329670984@qq.com>
2023-01-16 14:56:17 +0800

    remove: 移除多余的transition标签

--- src/views/Base/Login/childComps/Notice/index.vue
+++ src/views/Base/Login/childComps/Notice/index.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <transition name="fade">
-    <K-Dialog v-bind="$attrs" width="960px" header="王者荣耀后台管理系统公告">
-      <div class="content">
-        <p class="dear">亲爱的用户：</p>
-        <p style="text-indent: 2em">欢迎加入QQ群：1062077391</p>
-      </div>
-    </K-Dialog>
-  </transition>
+  <K-Dialog v-bind="$attrs" width="960px" header="王者荣耀后台管理系统公告">
+    <div class="content">
+      <p class="dear">亲爱的用户：</p>
+      <p style="text-indent: 2em">欢迎加入QQ群：1062077391</p>
+    </div>
+  </K-Dialog>
 </template>
 
 <style scoped lang="less">
