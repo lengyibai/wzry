666f8f6c9658de3aea99dcd89516a3412e3b934c
lengyibai <1329670984@qq.com>
2023-01-18 19:29:51 +0800

    remove: 删除多余变量isActive

--- src/layout/childComps/Navbar/childComps/PackUp/index.vue
+++ src/layout/childComps/Navbar/childComps/PackUp/index.vue
@@ -6,18 +6,14 @@
   />
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
-
 import switchStore from "@/store/switch";
 import otherStore from "@/store/other";
 
 const $otherStore = otherStore();
 const $switchStore = switchStore();
 
-const isActive = ref(true);
-
 const toggleClick = () => {
-  $switchStore.$clickAudio(`收起侧边栏${isActive.value}`);
+  $switchStore.$clickAudio("收起侧边栏");
   $otherStore.setCollapse();
 };
 </script>
