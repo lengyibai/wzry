2c700272d8d02907a7ed9b9c7d8b9145bbeb4209
lengyibai <1329670984@qq.com>
2023-01-23 21:43:23 +0800

    perf: tips和noTips分离

--- src/store/tip.ts
+++ src/store/tip.ts
@@ -2,18 +2,19 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 const tipStore = defineStore("tip", () => {
-  const tips = ref<Record<string, { text: string; noTip: boolean }>>({
-    "2rb7": {
-      text: "Tip开启成功",
-      noTip: false,
-    },
+  const tips = ref<Record<string, string>>({
+    "2rb7": "Tip开启成功",
+  });
+
+  const noTips = ref<Record<string, boolean>>({
+    "2rb7": false,
   });
 
   /** @description: 设置指定tip禁止再次提示 */
   const setNoTip = (name: string) => {
-    tips.value[name].noTip = true;
+    noTips.value[name] = true;
   };
-  return { tips, setNoTip };
+  return { tips, noTips, setNoTip };
 });
 
 export default tipStore;
