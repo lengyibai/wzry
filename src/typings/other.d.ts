c14cb6431229fdbe253a407d320938fbf3278d39
lengyibai <1329670984@qq.com>
2023-01-24 02:36:12 +0800

    style: 增加注释

--- src/typings/other.d.ts
+++ src/typings/other.d.ts
@@ -1,29 +1,25 @@
 /** @description: 弹窗位置 */
-declare type TipType =
-  | "left-top"
-  | "right-top"
-  | "left-bottom"
-  | "right-bottom";
+type TipType = "left-top" | "right-top" | "left-bottom" | "right-bottom";
 
 /** @description: 消息类型 */
-declare type MsgType = "info" | "warning" | "error";
-declare interface MsgText {
-  id: number;
-  text: string;
-  type: string;
+type MsgType = "info" | "warning" | "error";
+interface MsgText {
+  id: number; //唯一值
+  text: string; //提示内容
+  type: MsgType; //提示类型
 }
 
 /** @description: 设置配置项 */
-declare interface SettingConfig {
-  tip: boolean;
-  videoBg: boolean;
-  audio: boolean;
-  audioVolume: number;
-  music: boolean;
-  musicVolume: number;
-  speed: 0 | 1 | 2;
-  particle: boolean;
-  muted: boolean;
+interface SettingConfig {
+  tip: boolean; //小贴士
+  videoBg: boolean; //使用视频背景
+  audio: boolean; //音效
+  audioVolume: number; //音效音量
+  music: boolean; //音乐
+  musicVolume: number; //音乐音量
+  speed: 0 | 1 | 2; //动画速率
+  particle: boolean; //粒子特效
+  muted: boolean; //登录页视频静音
 }
 
 /** @description: 开关 */
@@ -31,12 +27,12 @@ declare namespace Switch {
   type ClickAudio = (name?: string) => void;
   type Msg = (text: string, type?: MsgType) => void;
   type Tip = (obj: {
-    title?: string;
-    text: string;
-    align?: TipType;
-    btn?: boolean;
-    btnText?: string[];
-    btnFn?: () => void;
+    title?: string; //左上角标题
+    text: string; //提示内容
+    align?: TipType; //在页面上的位置
+    btn?: boolean; //显示按钮
+    btnText?: string[]; //按钮上的文字
+    btnFn?: () => void; //点击确认后执行的函数
   }) => void;
   interface Loading {
     show: (text?: string) => void;
