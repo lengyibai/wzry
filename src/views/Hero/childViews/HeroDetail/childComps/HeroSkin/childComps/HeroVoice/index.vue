0251cbacf8f755ca29e0404e9d93ed461dd7b9b6
lengyibai <1329670984@qq.com>
2023-01-25 00:35:20 +0800

    fix: 解决语音列表无法获取dom元素报错问题

--- src/views/Hero/childViews/HeroDetail/childComps/HeroSkin/childComps/HeroVoice/index.vue
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroSkin/childComps/HeroVoice/index.vue
@@ -19,24 +19,29 @@ onMounted(() => {
     await $heroDetailStore.setSkinVoice(hero_name, skin_name);
     if (voice_length === $heroDetailStore.voice.length) return;
     voice_length = $heroDetailStore.voice.length;
+
     nextTick(() => {
-      voiceRef.value.forEach((item: HTMLElement, index: number) => {
-        /* 决定是从左还是从右入场 */
-        if (index % 2) {
-          item.style.transform = "translateX(-100%) translateY(500%) scale(0)";
-        } else {
-          item.style.transform = "translateX(100%) translateY(500%) scale(0)";
-        }
-        setTimeout(() => {
-          voices.value = $heroDetailStore.voice;
-          item.style.transitionDelay = `${index / 15}s`; //入场间隔
-          item.style.transform = "translateX(0%) translateY(0%) scale(1)";
-          //动画结束后初始化
+      voiceRef.value &&
+        voiceRef.value.forEach((item: HTMLElement, index: number) => {
+          /* 决定是从左还是从右入场 */
+          if (index % 2) {
+            item.style.transform =
+              "translateX(-100%) translateY(500%) scale(0)";
+          } else {
+            item.style.transform = "translateX(100%) translateY(500%) scale(0)";
+          }
+
           setTimeout(() => {
-            item.style.transitionDelay = "0s";
+            voices.value = $heroDetailStore.voice;
+            item.style.transitionDelay = `${index / 15}s`; //入场间隔
+            item.style.transform = "translateX(0%) translateY(0%) scale(1)";
+
+            //动画结束后初始化
+            setTimeout(() => {
+              item.style.transitionDelay = "0s";
+            }, 500);
           }, 500);
-        }, 500);
-      });
+        });
     });
   });
 });
--- src/views/Skin/childComps/SkinVoice/index.vue
+++ src/views/Skin/childComps/SkinVoice/index.vue
@@ -54,30 +54,30 @@ onMounted(() => {
   //设置需要切换皮肤触发的函数
   $heroDetailStore.setSkinToggleFn(async (hero_name, skin_name) => {
     await $heroDetailStore.setSkinVoice(hero_name, skin_name);
-
     if (voice_length === $heroDetailStore.voice.length) return;
-
     voice_length = $heroDetailStore.voice.length;
 
     nextTick(() => {
-      voiceRef.value.forEach((item: HTMLElement, index: number) => {
-        //决定是从左还是从右入场
-        if (index % 2) {
-          item.style.transform = "translateX(-100%) translateY(500%) scale(0)";
-        } else {
-          item.style.transform = "translateX(100%) translateY(500%) scale(0)";
-        }
+      voiceRef.value &&
+        voiceRef.value.forEach((item: HTMLElement, index: number) => {
+          //决定是从左还是从右入场
+          if (index % 2) {
+            item.style.transform =
+              "translateX(-100%) translateY(500%) scale(0)";
+          } else {
+            item.style.transform = "translateX(100%) translateY(500%) scale(0)";
+          }
 
-        setTimeout(() => {
-          item.style.transitionDelay = `${index / 15}s`; //入场间隔
-          item.style.transform = "translateX(0%) translateY(0%) scale(1)";
-
-          //动画结束后初始化
           setTimeout(() => {
-            item.style.transitionDelay = "0s";
+            item.style.transitionDelay = `${index / 15}s`; //入场间隔
+            item.style.transform = "translateX(0%) translateY(0%) scale(1)";
+
+            //动画结束后初始化
+            setTimeout(() => {
+              item.style.transitionDelay = "0s";
+            }, 500);
           }, 500);
-        }, 500);
-      });
+        });
     });
   });
 });
