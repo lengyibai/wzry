9021630839a60e7376080a3fa63aa95dd45d6f79
lengyibai <1329670984@qq.com>
2023-01-24 19:59:13 +0800

    feat: 关闭公告后触发新手引导

--- src/views/Base/Login/index.vue
+++ src/views/Base/Login/index.vue
@@ -7,11 +7,14 @@ import ToolBar from "./childComps/ToolBar/index.vue"; //工具栏
 import DownLoad from "./childComps/DownLoad/index.vue"; //下载数据
 
 import { LOGINBG } from "@/config/assets";
+import switchStore from "@/store/switch";
 import settingStore from "@/store/setting";
 
 const $settingStore = settingStore();
+const $switchStore = switchStore();
 
 const IMGBED = window.IMGBED; //全局图床链接
+let tip_status = false; //用于tip新手引导
 
 const show_notice = ref(true); //显示公告
 const finish = ref(false); //数据下载完成
@@ -19,12 +22,21 @@ const finish = ref(false); //数据下载完成
 //启用视频背景
 const enable_video_bg = computed(() => $settingStore.config.videoBg);
 
+$switchStore.$loading.close();
+
 /**
  * @description: 点击右上角工具栏
  * @param {string} v 点击工具栏的静音或公告按钮类型
  */
 const EmitToolType = (v: string) => {
   v === "notice" && (show_notice.value = true);
+};
+
+/** @description: 关闭公告触发 */
+const EmitCloseNotice = () => {
+  if (tip_status) return;
+  $switchStore.$tip({ text: "9f5m" });
+  tip_status = true;
 };
 </script>
 
@@ -54,7 +66,11 @@ const EmitToolType = (v: string) => {
 
     <!-- 公告 -->
     <transition v-if="finish" name="fade">
-      <Notice v-if="show_notice" v-model="show_notice" />
+      <Notice
+        v-if="show_notice"
+        v-model="show_notice"
+        @update:model-value="EmitCloseNotice"
+      />
     </transition>
 
     <!-- 下载进度 -->
