619228808b1cc9a3dbb0c520d3a49b4b2f47de84
lengyibai <1329670984@qq.com>
2023-01-26 20:23:09 +0800

    feat: 增加@vitejs/plugin-legacy

--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,26 @@ import vue from "@vitejs/plugin-vue";
 import { visualizer } from "rollup-plugin-visualizer"; //build 视图分析依赖文件
 import Components from "unplugin-vue-components/vite"; //组件自动注册
 import VueSetupExtend from "vite-plugin-vue-setup-extend"; //设置name
+import legacyPlugin from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   base: "./",
   plugins: [
     vue(),
+    VueSetupExtend(),
     Components({
       dts: "src/typings/components.d.ts", // 生成在src路径下名为auto-import.d.ts的声明文件
       dirs: ["src/components"], // 配置需要默认导入的自定义组件文件夹，该文件夹下的所有组件都会自动 import
     }),
-    VueSetupExtend(),
-    visualizer({
-      emitFile: true,
-      filename: "visualizer.html", //分析图生成的文件名
-      open: true, //如果存在本地服务端口，将在打包后自动展示
+    legacyPlugin({
+      targets: ["last 2 versions", "safari >=7", "chrome >= 30"], // 需要兼容的目标列表，可以设置多个
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"], // 面向IE11时需要此插件
     }),
+    // visualizer({
+    //   emitFile: true,
+    //   filename: "visualizer.html", //分析图生成的文件名
+    //   open: true, //如果存在本地服务端口，将在打包后自动展示
+    // }),
   ],
   css: {
     // css预处理器
@@ -56,7 +61,7 @@ export default defineConfig({
     outDir: "dist",
     minify: "terser",
     chunkSizeWarningLimit: 1500,
-    assetsInlineLimit: 0,
+    cssTarget: "chrome61",
     rollupOptions: {
       output: {
         chunkFileNames: "assets/js/[name]-[hash].js",
