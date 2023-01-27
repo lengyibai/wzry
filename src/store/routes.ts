791fa81bbaf179d1de3ceee839ea436d862aaf24
lengyibai <1329670984@qq.com>
2023-01-27 22:35:16 +0800

    rename: asyncRoutesName=>routeNames

--- src/store/routes.ts
+++ src/store/routes.ts
@@ -9,7 +9,7 @@ import sheelToRoute from "@/router/helper/sheelToRoute";
 
 const routeStore = defineStore("route", () => {
   const routes = ref<RouteRecordRaw[]>([]); //当前路由列表，针对侧边栏，解决通过vue-router提供的方法获取的路由列表与侧边栏要求的格式不匹配
-  const asyncRoutesName = ref<RouteRecordName[]>([]); //添加的路由name组，用于匹配并删除路由
+  const routeNames = ref<RouteRecordName[]>([]); //添加的路由name组，用于匹配并删除路由
 
   /** @description: 添加路由及存储路由侧边栏 */
   const addRoutes = (role: 0 | 1 = 1) => {
@@ -18,7 +18,7 @@ const routeStore = defineStore("route", () => {
     //根据用户权限设置路由表
     const initDynamicRouter: RouterSheel[] = role === 0 ? admin : user;
     //获取路由 name 组
-    asyncRoutesName.value = initDynamicRouter.map((item) => {
+    routeNames.value = initDynamicRouter.map((item) => {
       return item.name as RouteRecordName;
     });
     //将路由表转换成真实路由
@@ -32,7 +32,7 @@ const routeStore = defineStore("route", () => {
 
   /** @description: 通过路由 name 删除路由及路由侧边栏 */
   const removeRoutes = () => {
-    asyncRoutesName.value.forEach((item) => {
+    routeNames.value.forEach((item) => {
       router.removeRoute(item);
       //删除侧边栏
       routes.value.forEach((route, index) => {
@@ -45,7 +45,7 @@ const routeStore = defineStore("route", () => {
 
   return {
     routes,
-    asyncRoutesName,
+    routeNames,
     addRoutes,
     removeRoutes,
   };
