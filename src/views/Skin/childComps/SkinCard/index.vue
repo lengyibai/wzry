dadf727c3263f94bd69f246cfe144625377897ae
lengyibai <1329670984@qq.com>
2023-01-27 23:05:21 +0800

    fix: 修复悬浮文字无鼠标pointer样式

--- src/views/Skin/childComps/SkinCard/index.vue
+++ src/views/Skin/childComps/SkinCard/index.vue
@@ -41,9 +41,11 @@ const handle = (v: string) => {
 
     <!-- 悬浮工具 -->
     <div class="tool">
-      <span @click="handle('poster')"> <i class="iconfont wzry-fangda cursor-pointer" />大图</span>
-      <span @click="handle('voice')">
-        <i class="iconfont wzry-bofangyuyin cursor-pointer" />语音</span
+      <span class="cursor-pointer" @click="handle('poster')">
+        <i class="iconfont wzry-fangda" />大图</span
+      >
+      <span class="cursor-pointer" @click="handle('voice')">
+        <i class="iconfont wzry-bofangyuyin" />语音</span
       >
     </div>
   </div>
