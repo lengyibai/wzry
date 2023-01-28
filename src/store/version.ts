13d2dd7586a19b74346a337e8e8644af6b593233
lengyibai <1329670984@qq.com>
2023-01-28 22:28:38 +0800

    feat: 增加每10秒接收消息

--- src/store/version.ts
+++ src/store/version.ts
@@ -8,6 +8,7 @@ import useUpdateData from "@/hooks/useUpdateData";
 const versionStore = defineStore("version", () => {
   const $switchStore = switchStore();
 
+  const timer = ref<Interval>(); //实时更新计时器
   const local_version = ref(""); //本地版本
   const remote_version = ref(""); //远程版本
   const local_file = ref(""); //本地文件版本
@@ -36,47 +37,57 @@ const versionStore = defineStore("version", () => {
     local_file.value = v;
   };
 
-  //获取数据版本、文件版本、文件更新日志
-  Version().then((res) => {
-    const { main, file, log } = res.data;
-    remote_version.value = main;
-    file_version.value = file;
-    update_log.value.file = log;
+  /** @description: 获取数据版本、文件版本、文件更新日志 */
+  const watchVersion = () => {
+    Version().then((res) => {
+      const { main, file, log } = res.data;
+      remote_version.value = main;
+      file_version.value = file;
+      update_log.value.file = log;
 
-    //如果无本地版本，则直接更新，否则比对
-    if (!local_version.value) {
-      updateVersion(main);
-    } else {
-      const local = Number(local_version.value.replaceAll(".", ""));
-      const remote = Number(main.replaceAll(".", ""));
-      const test = remote - local;
+      //如果无本地版本，则直接更新，否则比对
+      if (!local_version.value) {
+        updateVersion(main);
+      } else {
+        const local = Number(local_version.value.replaceAll(".", ""));
+        const remote = Number(main.replaceAll(".", ""));
+        const test = remote - local;
 
-      // 如果为旧版，则自动更新并更新本地版本并返回更新改动
-      if (test > 0) {
-        data_status.value = true;
-        $switchStore.$msg("正在更新版本，更新后可查看数据改动，请稍等...");
-        useUpdateData().then((res) => {
-          update_log.value = { ...update_log.value, ...res };
+        // 如果为旧版，则自动更新并更新本地版本并返回更新改动
+        if (test > 0) {
+          clearInterval(timer.value);
+          data_status.value = true;
+          $switchStore.$msg("正在更新版本，更新后可查看数据改动，请稍等...");
+          useUpdateData().then((res) => {
+            update_log.value = { ...update_log.value, ...res };
+            show_update.value = true;
+          });
+        }
+      }
+
+      //如果无本地文件版本，则直接更新，否则比对
+      if (!local_file.value) {
+        updateFileVersion(file);
+      } else {
+        const local = Number(local_file.value.replaceAll(".", ""));
+        const remote = Number(file.replaceAll(".", ""));
+        const test = remote - local;
+
+        // 如果为旧版，则自动更新并更新本地版本
+        if (test > 0 && !data_status.value) {
+          clearInterval(timer.value);
           show_update.value = true;
-        });
+          file_status.value = true;
+        }
       }
-    }
+    });
+  };
 
-    //如果无本地文件版本，则直接更新，否则比对
-    if (!local_file.value) {
-      updateFileVersion(file);
-    } else {
-      const local = Number(local_file.value.replaceAll(".", ""));
-      const remote = Number(file.replaceAll(".", ""));
-      const test = remote - local;
-
-      // 如果为旧版，则自动更新并更新本地版本
-      if (test > 0 && !data_status.value) {
-        show_update.value = true;
-        file_status.value = true;
-      }
-    }
-  });
+  /* 实时接收更新通知 */
+  watchVersion();
+  timer.value = setInterval(() => {
+    watchVersion();
+  }, 10000);
 
   /** @description: 控制弹窗显示 */
   const setShowLog = (v: boolean) => {
