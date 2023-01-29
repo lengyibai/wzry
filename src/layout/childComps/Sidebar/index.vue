bf88d132b0c10facdefa9ae516524154d4e6f77b
lengyibai <1329670984@qq.com>
2023-01-29 20:06:59 +0800

    refactor: 抽离路由格式化为侧边栏可用的方法

--- src/layout/childComps/Sidebar/index.vue
+++ src/layout/childComps/Sidebar/index.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import { RouteRecordRaw } from "vue-router";
-
 import sideItem from "./childComp/SideItem.vue"; //子菜单
 
-import { Route } from "@/router/interface";
+import formatSidebarRoutes from "@/router/helper/formatSidebarRoutes";
 import routerStore from "@/store/routes";
 import collapseStore from "@/store/collapse";
 
@@ -12,43 +10,7 @@ const $routerStore = routerStore();
 
 const options = $routerStore.routes; //路由数据
 
-const formatRoute = (a: RouteRecordRaw[]) => {
-  const routes: Route[] = [];
-  const fn = (b: Route[] | null | undefined, zIndex: number) => {
-    const route: Route[] = [];
-    if (b && b[0].path) {
-      b.forEach((item) => {
-        let index = zIndex;
-        route.push({
-          path: item.path,
-          title: item.meta && item.meta.title,
-          children: fn(item.children, ++index),
-          meta: item.meta,
-          zIndex: index,
-        });
-      });
-      return route;
-    }
-
-    return null;
-  };
-
-  a.forEach((item) => {
-    if (!item.meta) return;
-    if (item?.meta?.hidden) return;
-    routes.push({
-      path: item.path,
-      title: item.meta.title as string,
-      meta: item.meta,
-      children: fn(item.children as Route[], 1),
-      zIndex: 1,
-    });
-  });
-
-  return routes;
-};
-
-const routes = formatRoute(options); //格式化后的路由数据
+const routes = formatSidebarRoutes(options); //格式化后的路由数据
 </script>
 
 <template>
