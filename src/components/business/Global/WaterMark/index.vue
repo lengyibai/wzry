82273b0375df98775ebf8829d4dcb4feb1c221c0
lengyibai <1329670984@qq.com>
2023-01-31 01:37:16 +0800

    feat: 增加帧率显示

--- src/components/business/Global/WaterMark/index.vue
+++ src/components/business/Global/WaterMark/index.vue
@@ -4,9 +4,11 @@ import { computed } from "vue";
 import { $browserV } from "@/utils";
 import collapseStore from "@/store/collapse";
 import versionStore from "@/store/version";
+import phoneStore from "@/store/phone";
 
 const $collapseStore = collapseStore();
 const $versionStore = versionStore();
+const $phoneStore = phoneStore();
 
 /* 是否为旧版 */
 const old = computed(() => $versionStore.local_version !== $versionStore.remote_version);
@@ -14,6 +16,7 @@ const old_file = computed(() => $versionStore.local_file !== $versionStore.file_
 
 /* 浏览器版本提示 */
 const low = $browserV.browser === "chrome" ? $browserV.version < 90 : $browserV.version < 15;
+
 const version = `${$browserV.version} ${
   low ? "(版本较低，部分效果可能无法显示，建议更换浏览器)" : ""
 }`;
@@ -22,6 +25,8 @@ const version = `${$browserV.version} ${
 <template>
   <transition name="fade">
     <div v-show="!$collapseStore.collapse" class="water-mark">
+      <p>帧率：{{ $phoneStore.fps }}</p>
+      <p>设备尺寸：{{ $phoneStore.width }}*{{ $phoneStore.height }}</p>
       <p :class="{ low: low }">浏览器{{ $browserV.browser }}内核版本：{{ version }}</p>
       <p :class="{ old: old }">当前数据版本：{{ $versionStore.local_version }}</p>
       <p :class="{ new: old }">最新数据版本：{{ $versionStore.remote_version }}</p>
--- src/store/phone.ts
+++ src/store/phone.ts
@@ -5,6 +5,23 @@ const phoneStore = defineStore("phone", () => {
   const vertical = ref(false); //是否为移动端
   const width = ref(0); // 宽度
   const height = ref(0); // 宽度
+  const fps = ref(0);
+
+  /* 获取设备帧率 */
+  let start = 0;
+  let count = 0;
+  const getFPS = () => {
+    count++;
+    const current = performance.now();
+    if (start === undefined) start = current;
+    if (current - start >= 500) {
+      fps.value = count;
+      start = current;
+      count = 0;
+    }
+    window.requestAnimationFrame(getFPS);
+  };
+  getFPS();
 
   /* 判断手机是否竖屏 */
   const tip = () => {
@@ -18,7 +35,7 @@ const phoneStore = defineStore("phone", () => {
     tip();
   });
 
-  return { vertical, width, height };
+  return { vertical, width, height, fps };
 });
 
 export default phoneStore;
