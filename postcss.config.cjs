909e69e3b09e11c8aa271f67432137245a8fd2c5
lengyibai <1329670984@qq.com>
2023-01-31 19:50:22 +0800

    ci: 优化兼容

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,14 @@
-//postcss.config.js
 module.exports = {
   plugins: {
     autoprefixer: {
-      overrideBrowserslist: ["Android 4.1", "iOS 7.1", "Chrome > 31", "ff > 31", "ie >= 8", "last 10 versions"],
+      overrideBrowserslist: [
+        "Android 4.1",
+        "iOS 7.1",
+        "Chrome > 31",
+        "ff > 31",
+        "ie >= 8",
+        "last 2 versions",
+      ],
     },
   },
 };
