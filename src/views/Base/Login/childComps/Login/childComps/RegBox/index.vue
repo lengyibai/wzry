156e3ea3eae4a0279caa9c559a3f731b45fd66f4
lengyibai <1329670984@qq.com>
2023-02-01 14:17:19 +0800

    feat: 增加表单验证

--- src/views/Base/Login/childComps/Login/childComps/RegBox/index.vue
+++ src/views/Base/Login/childComps/Login/childComps/RegBox/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { ref, reactive } from "vue";
 
 import IntoBtn from "../IntoBtn/index.vue"; //按钮
 
@@ -7,7 +7,6 @@ import RoleSelect from "./childComps/RoleSelect/index.vue"; //权限选择
 
 import { register } from "@/api/main/user";
 import { userDefaultInfo } from "@/default";
-import { $existEmpty } from "@/utils";
 import switchStore from "@/store/switch";
 
 interface Emits {
@@ -17,15 +16,16 @@ const emit = defineEmits<Emits>();
 
 const $switchStore = switchStore();
 
-// 表单数据
-const form = reactive<User>({ ...userDefaultInfo });
+const form = reactive<User>({ ...userDefaultInfo }); // 表单数据
+
+const form_verify = ref<boolean[]>([false, false, false]); //表单验证
 
 /* 注册 */
 const handleReg = () => {
   $switchStore.$clickAudio("36jn");
-  // 非空判断
-  if ($existEmpty(form)) {
-    $switchStore.$msg("请完整填写", "error");
+
+  if (form_verify.value.some((item) => !item)) {
+    $switchStore.$msg("请正确填写", "error");
     return;
   }
 
@@ -46,19 +46,44 @@ const handleReg = () => {
     <!-- 昵称 -->
     <div class="box">
       <i class="iconfont wzry-nickname" />
-      <K-Input v-model="form.nickname" placeholder="请输入昵称" />
+      <K-Input
+        v-model="form.nickname"
+        v-model:empty="form_verify[0]"
+        padding-left="45px"
+        placeholder="请输入昵称"
+        required
+        :min="2"
+        :max="6"
+      />
     </div>
 
     <!-- 帐号 -->
     <div class="box">
       <i class="iconfont wzry-user" />
-      <K-Input v-model="form.id" placeholder="请输入帐号" />
+      <K-Input
+        v-model.number="form.id"
+        v-model:empty="form_verify[1]"
+        padding-left="45px"
+        placeholder="请输入帐号"
+        required
+        number
+        :min="6"
+        :max="12"
+      />
     </div>
 
     <!-- 密码 -->
     <div class="box">
       <i class="iconfont wzry-password" />
-      <K-Input v-model="form.password" placeholder="请输入密码" />
+      <K-Input
+        v-model="form.password"
+        v-model:empty="form_verify[2]"
+        padding-left="45px"
+        placeholder="请输入密码"
+        required
+        :min="6"
+        :max="18"
+      />
     </div>
 
     <!-- 权限选择 -->
