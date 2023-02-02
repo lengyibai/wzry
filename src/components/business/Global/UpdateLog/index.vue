61ba51dc8e005f5fc82eba40fec18a12a2fbf937
lengyibai <1329670984@qq.com>
2023-02-02 14:57:04 +0800

    visual: 无更新显示暂无

--- src/components/business/Global/UpdateLog/index.vue
+++ src/components/business/Global/UpdateLog/index.vue
@@ -28,19 +28,22 @@ const handleReset = () => {
   >
     <div class="main">
       <!-- 页面更新 -->
-      <h1 v-if="update_log.file">页面更新</h1>
-      <div class="content" v-html="update_log.file"></div>
+      <h1>页面更新</h1>
+      <div v-if="update_log.file" class="content" v-html="update_log.file"></div>
+      <div v-else class="content"><p>暂无</p></div>
 
       <!-- 基础数据更新 -->
-      <h1 v-if="update_log.data">基础数据更新</h1>
-      <div v-if="update_log.data" class="content">
-        <p v-html="update_log.data"></p>
+      <h1>基础数据更新</h1>
+      <div class="content">
+        <p v-if="update_log.data" v-html="update_log.data"></p>
+        <p v-else>暂无</p>
       </div>
 
       <!-- 语音更新 -->
-      <h1 v-if="update_log.voice">语音更新</h1>
-      <div v-if="update_log.voice" class="content">
-        <p v-html="update_log.voice"></p>
+      <h1>语音更新</h1>
+      <div class="content">
+        <p v-if="update_log.voice" v-html="update_log.voice"></p>
+        <p v-else>暂无</p>
       </div>
     </div>
 
