84a4c80bc1bd76685778f4fba709227e463f155a
lengyibai <1329670984@qq.com>
2023-02-03 14:39:10 +0800

    refactor: 使用字体图标代替取消按钮组件

--- src/components/business/Tool/ReleaseConfirm/index.vue
+++ src/components/business/Tool/ReleaseConfirm/index.vue
@@ -1,35 +1,25 @@
-<template>
-  <div class="release-confirm">
-    <!-- 取消发布 -->
-    <LibCancelBtn class="lib-cancel-btn" v-bind="$attrs" title="取消" />
-
-    <!-- 发布按钮 -->
-    <LibCommitBtn v-model="commit_status" class="lib-commit-btn" v-bind="$attrs" title="发布" />
-
-    <!-- 确认关闭 -->
-    <transition name="fade">
-      <ConfirmClose v-if="show_ConfirmClose" v-model="show_ConfirmClose" v-bind="$attrs" />
-    </transition>
-  </div>
-</template>
 <script setup lang="ts">
 import { ref, watch } from "vue";
 
 interface Props {
   status: number; //提交状态
   finish: boolean; //完成提交
-  showConfirmclose: boolean; // 显示确认关闭弹窗
 }
 const props = defineProps<Props>();
 
 interface Emits {
-  (e: "update:showConfirmclose", v: boolean): void;
   (e: "update:status", v: number): void;
+  (e: "close"): void;
 }
 const emit = defineEmits<Emits>();
 
 const commit_status = ref(0); //提交状态
 const show_ConfirmClose = ref(false); //是否显示确认关闭弹窗
+
+/* 关闭页面 */
+const handleClose = () => {
+  show_ConfirmClose.value = true;
+};
 
 watch(
   () => props.status,
@@ -41,15 +31,23 @@ watch(
 watch(commit_status, (v) => {
   emit("update:status", v);
 });
-
-watch(
-  () => [props.showConfirmclose, show_ConfirmClose.value],
-  (v) => {
-    show_ConfirmClose.value = v[0];
-    emit("update:showConfirmclose", v[1]);
-  }
-);
 </script>
+
+<template>
+  <div class="release-confirm">
+    <!-- 取消发布 -->
+    <i class="iconfont wzry-guanbi" title="取消" @click="handleClose"></i>
+
+    <!-- 发布按钮 -->
+    <LibCommitBtn v-model="commit_status" class="lib-commit-btn" v-bind="$attrs" title="发布" />
+
+    <!-- 确认关闭 -->
+    <transition name="fade">
+      <ConfirmClose v-if="show_ConfirmClose" v-model="show_ConfirmClose" v-bind="$attrs" />
+    </transition>
+  </div>
+</template>
+
 <style scoped lang="less">
 @import url("./index.less");
 </style>
