5d4191bfb95e0995a98667a1e485880ad5fa9aa0
lengyibai <1329670984@qq.com>
2023-02-03 18:10:47 +0800

    refactor: 发射全局事件参数改变

--- src/views/Skin/childComps/SkinToolbar/index.vue
+++ src/views/Skin/childComps/SkinToolbar/index.vue
@@ -89,9 +89,10 @@ const handleSelectStatus = (i: number) => {
   current_index.value = i;
 };
 
-$bus.on("mouseup", (el) => {
+$bus.on("mouseup", (e) => {
+  const el = (e as MouseEvent).target as HTMLElement;
   //如果点击的不是下拉菜单，则隐藏
-  if (!(el as HTMLElement).className.includes("select-filter")) {
+  if (!el.className.includes("select-filter")) {
     select_status.fill(false);
     current_index.value = -1;
   }
