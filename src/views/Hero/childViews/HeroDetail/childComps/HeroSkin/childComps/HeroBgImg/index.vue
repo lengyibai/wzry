a520423ec1abea0a89e60d22b704680d81ccf6f9
lengyibai <1329670984@qq.com>
2023-02-07 18:19:50 +0800

    feat: 皮肤切换支持多种动画

--- src/views/Hero/childViews/HeroDetail/childComps/HeroSkin/childComps/HeroBgImg/index.vue
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroSkin/childComps/HeroBgImg/index.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
+import { ref, watch } from "vue";
+
 interface Props {
   bgImg: string[]; //背景图组
   toggle: boolean; //用于切换
 }
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const index = ref(0);
+const animates = ["xmove", "ymove", "square", "clip", "spin", "tv", "fold", "deploy"];
+
+watch(
+  () => props.toggle,
+  () => {
+    if (animates.length - 1 === index.value) {
+      index.value = 0;
+    }
+    index.value++;
+  }
+);
 </script>
 
 <template>
   <div class="hero-bg-img">
-    <transition-group name="clip">
+    <transition-group :name="animates[index]">
       <img v-if="bgImg[0]" v-show="toggle" key="a" class="bg" :src="bgImg[0]" alt="" />
       <img v-show="!toggle" key="b" class="bg" :src="bgImg[1]" alt="" />
     </transition-group>
