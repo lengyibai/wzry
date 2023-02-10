2aa8a87a13fdbce36a96b90348773f2080a97c94
lengyibai <1329670984@qq.com>
2023-02-10 13:22:16 +0800

    visual: 优化400页面样式

--- src/views/Base/Error/400.vue
+++ src/views/Base/Error/400.vue
@@ -1,9 +1,22 @@
+<script setup lang="ts">
+import deviceStore from "@/store/device";
+
+const $deviceStore = deviceStore();
+</script>
+
 <template>
   <div class="main incompatible">
     <div class="text">
       <div class="a">400</div>
       <div class="b">浏览器兼容问题</div>
-      <div class="c">你的浏览器版本过低，为了不影响你的浏览体验，建议你升级浏览器或更换浏览器后再访问</div>
+      <div class="c">
+        <span class="green-text">Chrome 内核 >= 90</span><br />
+        <span class="green-text">Firefox 内核 >= 90</span><br />
+        <span class="green-text">Safari 内核 >= 15</span><br />
+        你的 <span class="red-text">{{ $deviceStore.browser_name }} 内核为 {{ $deviceStore.browser_version }}</span
+        >，不满足访问条件<br />
+        请尝试更换或升级浏览器后再访问
+      </div>
     </div>
   </div>
 </template>
