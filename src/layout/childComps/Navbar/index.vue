3bea07d9a1485821213a57ff756de45c6fef0ce8
lengyibai <1329670984@qq.com>
2023-02-21 16:52:18 +0800

    visual: 竖屏隐藏顶部时间

--- src/layout/childComps/Navbar/index.vue
+++ src/layout/childComps/Navbar/index.vue
@@ -1,19 +1,12 @@
 <script setup lang="ts">
-import { computed } from "vue";
-
 import PackUp from "./childComps/PackUp/index.vue"; //折叠侧边栏按钮
 import PassTime from "./childComps/PassTime/index.vue"; //中间过去时
 import UserMenu from "./childComps/UserMenu/index.vue"; //用户卡片
 import BtnIcon from "./childComps/BtnIcon/index.vue"; //图标按钮
 
-import collapseStore from "@/store/collapse";
 import deviceStore from "@/store/device";
 
-const $collapseStore = collapseStore();
 const $deviceStore = deviceStore();
-
-//侧边栏展开且设备竖屏
-const show_time = computed(() => $collapseStore.collapse || !$deviceStore.vertical);
 </script>
 
 <template>
@@ -22,7 +15,7 @@ const show_time = computed(() => $collapseStore.collapse || !$deviceStore.vertic
     <!-- 折叠按钮 -->
     <PackUp />
 
-    <div v-show="show_time" class="center">
+    <div v-show="!$deviceStore.vertical" class="center">
       <!-- 已过去时间 -->
       <PassTime />
     </div>
