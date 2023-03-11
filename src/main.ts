1c29e69b93aaa52efd1083fdc85bc2f0ad63effb
kunbei <1329670984@qq.com>
2023-03-11 14:48:49 +0800

    perf: 使用flexible完成html字体大小平滑过渡

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,26 @@ import { setupRouter } from "@/router";
 import setupStore from "@/store";
 import setupDirective from "@/directives";
 import useAutoLogin from "@/hooks/useAutoLogin";
+import { flexible, setFontsize } from "@/utils/flexible";
 
 import "@/styles/index.less";
 
 new VConsole();
+flexible.trigger([1400, 960], (v: number) => {
+  setFontsize([16, 14], v);
+});
+
+flexible.trigger([960, 720], (v: number) => {
+  setFontsize([14, 12], v);
+});
+
+flexible.trigger([720, 480], (v: number) => {
+  setFontsize([12, 10], v);
+});
+
+flexible.trigger([480, 0], () => {
+  document.documentElement.style.fontSize = "10px";
+});
 
 const app = createApp(App);
 
