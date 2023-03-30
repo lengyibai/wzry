200457c38c1cda7c4754cd9f5dffdaca462c9a73
kunbei <kunbei@gmail.com>
2023-03-30 09:47:11 +0800

    annota: 笔记备注

--- src/utils/modules/eventBus.ts
+++ src/utils/modules/eventBus.ts
@@ -1,4 +1,16 @@
 import mitt from "mitt";
 
+// type EventData = {
+//   "load-msg"?: "auto" | "smooth";
+//   "play-voice": undefined;
+//   role: string;
+// };
+
+// type MittEventMap = {
+//   [key in keyof EventData]: EventData[key];
+// };
+
+// const emitter = mitt<MittEventMap>();
+
 const emitter = mitt();
 export default emitter;
