5e5247996bb2125b78b06fdd5276560e14e8acea
lengyibai <1329670984@qq.com>
2023-04-12 23:39:27 +0800

    misc: 关闭i18n警告

--- vite.config.ts
+++ vite.config.ts
@@ -52,6 +52,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js",
     },
   },
   build: {
