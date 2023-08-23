4d041af8a5fdbadea7b527a6c6fa2a9b5a869da9
kunbei <kunbei@gmail.com>
2023-08-23 18:46:06 +0800

    perf: 延迟100ms显示loading

--- src/config/modules/loading.ts
+++ src/config/modules/loading.ts
@@ -4,23 +4,27 @@ import { Util } from "@/utils";
 
 let loadingTimeout: NodeJS.Timeout | undefined;
 let needLoadingRequestCount = 0;
+let timer: NodeJS.Timeout | undefined;
 
 /** @description 开启loading */
 const show = async (text: string) => {
-  if (loadingTimeout) clearTimeout(loadingTimeout);
+  timer = setTimeout(() => {
+    if (loadingTimeout) clearTimeout(loadingTimeout);
 
-  if (needLoadingRequestCount === 0) {
-    Util.$Bus.emit("loading", {
-      show: true,
-      text,
-    });
-  }
+    if (needLoadingRequestCount === 0) {
+      Util.$Bus.emit("loading", {
+        show: true,
+        text,
+      });
+    }
 
-  needLoadingRequestCount++;
+    needLoadingRequestCount++;
+  }, 100);
 };
 
 /** @description 关闭loading */
 const close = async () => {
+  clearTimeout(timer);
   needLoadingRequestCount--;
 
   if (needLoadingRequestCount === 0) {
