7dbf7736125bd439913ee25864b07a08d7ce6098
kunbei <kunbei@gmail.com>
2023-09-03 00:50:23 +0800

    remove: 删除无用代码

--- src/views/Skin/childComps/SkinVoice/index.vue
+++ src/views/Skin/childComps/SkinVoice/index.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import { ref, nextTick, onMounted } from "vue";
-
-import { HeroDetailStore } from "@/store";
+import { ref } from "vue";
 
 interface Props {
   /** 语音列表 */
@@ -9,12 +7,7 @@ interface Props {
 }
 defineProps<Props>();
 
-const $heroDetailStore = HeroDetailStore();
-
 const voiceRef = ref();
-
-/** 当前语音数量 */
-let voice_length = 1;
 
 /** 播放链接 */
 const play_link = ref("");
@@ -38,39 +31,6 @@ const onEnded = () => {
   current_index.value = -1;
   play_link.value = "";
 };
-
-onMounted(() => {
-  //设置需要切换皮肤触发的函数
-  $heroDetailStore.setSkinToggleFn(async (hero_name, skin_name) => {
-    await $heroDetailStore.setSkinVoice(hero_name, skin_name);
-    if (voice_length === $heroDetailStore.skin_voice.length) return;
-    voice_length = $heroDetailStore.skin_voice.length;
-
-    nextTick(() => {
-      voiceRef.value &&
-        voiceRef.value.forEach((item: HTMLElement, index: number) => {
-          //决定是从左还是从右入场
-          if (index % 2) {
-            item.style.transform = "translateX(-100%) translateY(500%) scale(0)";
-          } else {
-            item.style.transform = "translateX(100%) translateY(500%) scale(0)";
-          }
-
-          //播放第一个语音
-          setTimeout(() => {
-            //入场间隔
-            item.style.transitionDelay = `${index / 15}s`;
-            item.style.transform = "translateX(0%) translateY(0%) scale(1)";
-
-            //动画结束后初始化
-            setTimeout(() => {
-              item.style.transitionDelay = "0s";
-            }, 500);
-          }, 500);
-        });
-    });
-  });
-});
 </script>
 
 <template>
