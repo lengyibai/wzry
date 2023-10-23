97d03bac7f605b7bda05b24b38b6fbf987d53303
lengyibai <1329670984@qq.com>
2023-10-24 00:10:28 +0800

    type: 图床类型删除

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -2,11 +2,3 @@ module "*.vue" {
   import type { DefineComponent } from "vue";
   export default DefineComponent<{}, {}, any>;
 }
-
-interface Window {
-  /** 图床链接 */
-  IMGBED: string;
-}
-
-/** 图床链接 */
-declare const IMGBED: string;
