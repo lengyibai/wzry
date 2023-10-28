b4af4f703c5f17a7eefefbc1c09f491a3bb74d23
lengyibai <1329670984@qq.com>
2023-10-29 02:47:05 +0800

    feat: 增加获取fps hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useAutoLogin } from "./modules/useAutoLogin";
 export { useGetData } from "./modules/useGetData";
 export { useUpdateData } from "./modules/useUpdateData";
+export { useGetFps } from "./modules/useGetFps";
