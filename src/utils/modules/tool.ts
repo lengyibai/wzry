ec2af56a6e70055f121bd13634bc44a9b3004d7b
kunbei <kunbei@gmail.com>
2023-11-04 22:38:22 +0800

    feat: 增加FLIP查看图片

--- src/utils/modules/tool.ts
+++ src/utils/modules/tool.ts
@@ -453,6 +453,7 @@ export class ScaleImage {
       max-height: 75%;
       transform: scale(${this.scale});
       transition: all 0.25s;
+      background-color: #1a1a1a;
     `;
     this.img.src = this.src;
     this.overlay.appendChild(this.img);
@@ -574,6 +575,227 @@ export class ScaleImage {
       this.scale /= 2;
       this.scale = Math.max(this.scale, 0.25);
       this.img.style.transform = `scale(${this.scale}) translate(${this.x}px, ${this.y}px)`;
+    };
+    this.zoomInButton.addEventListener("mouseup", fn1);
+    this.zoomInButton.addEventListener("touchend", fn1);
+    this.zoomOutButton.addEventListener("mouseup", fn2);
+    this.zoomOutButton.addEventListener("touchend", fn2);
+  }
+}
+
+/** @description FLIP查看图片 */
+
+/** @description 查看图片 */
+export class ScaleFLIPImage {
+  src: string;
+  scale: number;
+  x: number;
+  y: number;
+  parent!: HTMLElement;
+  overlay!: HTMLElement;
+  zoomInButton!: HTMLElement;
+  zoomOutButton!: HTMLElement;
+  closeButton!: HTMLElement;
+  img!: HTMLImageElement;
+  isDragging!: boolean;
+  startX!: number;
+  startY!: number;
+
+  constructor(e: Event, src: string) {
+    this.parent = e.target as HTMLElement;
+    this.src = src;
+    this.scale = 1;
+    this.x = 0;
+    this.y = 0;
+
+    this.createOverlay();
+    this.creatButton();
+    this.createImage();
+  }
+
+  /* 创建容器 */
+  createOverlay() {
+    this.overlay = document.createElement("div");
+    this.overlay.style.cssText = `
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.75);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      transition: all 0.25s;
+      z-index: var(--z-index-view-img);
+      opacity: 0;
+    `;
+    document.body.appendChild(this.overlay);
+
+    setTimeout(() => {
+      this.overlay.style.opacity = "1";
+    });
+  }
+
+  /* 创建图片 */
+  createImage() {
+    setTimeout(() => {
+      this.img = document.createElement("img");
+      this.img.style.maxWidth = this.parent.offsetWidth + "px";
+      this.img.style.maxHeight = this.parent.offsetHeight + "px";
+      this.img.style.position = "absolute";
+      this.img.style.transition = "0.5s";
+      this.img.style.opacity = "0";
+      this.img.style.transformOrigin = "left top";
+      this.img.style.left = this.parent.getBoundingClientRect().left + "px";
+      this.img.style.top = this.parent.getBoundingClientRect().top + "px";
+
+      this.img.src = this.src;
+      this.overlay.appendChild(this.img);
+      this.bindEvents();
+    }, 1);
+
+    setTimeout(() => {
+      this.img.style.maxWidth = "75%";
+      this.img.style.maxHeight = "75%";
+      this.img.style.left = `50%`;
+      this.img.style.top = `50%`;
+      this.img.style.opacity = "1";
+      this.img.style.transformOrigin = "center center";
+      this.img.style.transform = `translate(-50%,-50%) scale(${this.scale})`;
+      this.img.style.backgroundColor = "#1a1a1a";
+      setTimeout(() => {
+        this.img.style.transition = "initial";
+        this.img.style.top = "initial";
+        this.img.style.left = "initial";
+        this.img.style.transform = `scale(${this.scale})`;
+        setTimeout(() => {
+          this.img.style.transition = "0.25s";
+        }, 16.7);
+      }, 500);
+    }, 100);
+  }
+
+  /* 创建按钮 */
+  creatButton() {
+    const button_style = `
+    position: absolute;
+    font-size: 3.125rem;
+    color: #fff;
+    z-index: 1;
+    `;
+
+    //关闭按钮
+    this.closeButton = document.createElement("button");
+    this.closeButton.style.cssText = button_style;
+    this.closeButton.className += "global_cursor-pointer iconfont wzry-guanbi";
+    this.closeButton.style.top = "0";
+    this.closeButton.style.right = "0";
+    this.closeButton.style.transform = "translate(-100%, 100%) scale(0.75)";
+
+    //放大按钮
+    this.zoomInButton = document.createElement("button");
+    this.zoomInButton.style.cssText = button_style;
+    this.zoomInButton.className += "global_cursor-pointer iconfont wzry-fangda";
+    this.zoomInButton.style.bottom = "0";
+    this.zoomInButton.style.left = "50%";
+    this.zoomInButton.style.transform = "translate(-150%, -100%)";
+
+    //缩小按钮
+    this.zoomOutButton = document.createElement("button");
+    this.zoomOutButton.style.cssText = button_style;
+    this.zoomOutButton.className += "global_cursor-pointer iconfont wzry-suoxiao";
+    this.zoomOutButton.style.bottom = "0";
+    this.zoomOutButton.style.left = "50%";
+    this.zoomOutButton.style.transform = "translate(50%, -100%)";
+
+    this.overlay.appendChild(this.closeButton);
+    this.overlay.appendChild(this.zoomInButton);
+    this.overlay.appendChild(this.zoomOutButton);
+  }
+
+  /* 绑定事件 */
+  bindEvents() {
+    this.overlay.addEventListener("wheel", (event) => {
+      if (event.deltaY > 0) {
+        this.scale /= 1.25;
+        if (this.scale < 0.25) this.scale = 0.25;
+      } else {
+        this.scale *= 1.25;
+        if (this.scale > 20) this.scale = 20;
+      }
+      this.img.style.transform = `scale(${this.scale}) translate(${this.x}px, ${this.y}px)`;
+    });
+
+    this.img.addEventListener("mousedown", (event) => {
+      event.preventDefault();
+    });
+
+    /* 拖动 */
+    document.addEventListener("mousedown", (event) => {
+      this.startX = event.clientX;
+      this.startY = event.clientY;
+      this.isDragging = true;
+      this.img.style.transition = "all 0s";
+    });
+    document.addEventListener("mouseup", () => {
+      this.isDragging = false;
+      this.img.style.transition = "all 0.25s";
+    });
+    document.addEventListener("mousemove", (event) => {
+      if (!this.isDragging) return;
+      const deltaX = event.clientX - this.startX;
+      const deltaY = event.clientY - this.startY;
+      this.startX = event.clientX;
+      this.startY = event.clientY;
+      this.x += deltaX / this.scale;
+      this.y += deltaY / this.scale;
+      this.img.style.transform = `scale(${this.scale}) translate(${this.x}px, ${this.y}px)`;
+    });
+
+    /* 兼容移动端 */
+    document.addEventListener("touchstart", (event) => {
+      this.startX = event.changedTouches[0].clientX;
+      this.startY = event.changedTouches[0].clientY;
+      this.isDragging = true;
+      this.img.style.transition = "all 0s";
+    });
+    document.addEventListener("touchend", () => {
+      this.isDragging = false;
+      this.img.style.transition = "all 0.25s";
+    });
+    document.addEventListener("touchmove", (event) => {
+      if (!this.isDragging) return;
+      const deltaX = event.changedTouches[0].clientX - this.startX;
+      const deltaY = event.changedTouches[0].clientY - this.startY;
+      this.startX = event.changedTouches[0].clientX;
+      this.startY = event.changedTouches[0].clientY;
+      this.x += deltaX / this.scale;
+      this.y += deltaY / this.scale;
+      this.img.style.transform = `scale(${this.scale}) translate(${this.x}px, ${this.y}px)`;
+    });
+
+    this.closeButton.addEventListener("click", () => {
+      this.overlay.style.opacity = "0";
+      setTimeout(() => {
+        this.overlay.remove();
+      }, 250);
+    });
+
+    /* 缩放 */
+    const fn1 = () => {
+      setTimeout(() => {
+        this.scale *= 2;
+        this.scale = Math.min(this.scale, 20);
+        this.img.style.transform = `scale(${this.scale}) translate(${this.x}px, ${this.y}px)`;
+      });
+    };
+    const fn2 = () => {
+      setTimeout(() => {
+        this.scale /= 2;
+        this.scale = Math.max(this.scale, 0.25);
+        this.img.style.transform = `scale(${this.scale}) translate(${this.x}px, ${this.y}px)`;
+      });
     };
     this.zoomInButton.addEventListener("mouseup", fn1);
     this.zoomInButton.addEventListener("touchend", fn1);
