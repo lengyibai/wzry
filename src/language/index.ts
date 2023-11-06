f800e88c622cf92cfad63148d52b00a3ea87358e
kunbei <kunbei@gmail.com>
2023-11-06 18:59:59 +0800

    feat: 开发模式下增加快捷键切换语言

--- src/language/index.ts
+++ src/language/index.ts
@@ -1,4 +1,5 @@
 import { createI18n } from "vue-i18n";
+import { App } from "vue";
 
 import zh from "./modules/zh";
 import tc from "./modules/tc";
@@ -22,16 +23,35 @@ const i18n = createI18n({
   messages,
 });
 
-const t = i18n.global.t;
+const { setLocaleMessage, locale, t } = i18n.global;
 
 /** @description 设置语言 */
 const setLanguage = (index: 0 | 1 | 2) => {
   const langs = ["zh", "tc", "en"];
   const lang = langs[index];
-  i18n.global.setLocaleMessage(lang, messages[lang]);
-  i18n.global.locale.value = lang;
+  setLocaleMessage(lang, messages[lang]);
+  locale.value = lang;
   localStorage.setItem(CONFIG.LOCAL_KEY.LANGUAGE, lang);
 };
 
-export { i18n, setLanguage, t };
+/** @description 挂载 */
+const setupLanguage = (app: App) => {
+  app.use(i18n);
+};
+
+//开发模式下按Ctrl + Q切换语言
+if (import.meta.env.DEV) {
+  document.addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.key === "q") {
+      const obj: Record<string, 0 | 1 | 2> = {
+        zh: 1,
+        tc: 2,
+        en: 0,
+      };
+      setLanguage(obj[locale.value]);
+    }
+  });
+}
+
+export { setupLanguage, setLanguage, t };
 export type { I18nTypeEnum };
