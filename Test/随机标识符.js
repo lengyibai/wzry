7bca332c1861fccaae4e6bf5728407a6aa9d9df4
kunbei <kunbei@gmail.com>
2023-11-10 22:53:49 +0800

    test: 测试文件变动

--- Test/随机标识符.js
+++ Test/随机标识符.js
@@ -1,59 +1,31 @@
-const g = (a) => {
-  let arr = [];
-  while (arr.length < 10) {
-    const c = "abcdefghijklmnopqrstuvwxyz0123456789";
-    const i =
-      Math.floor(Math.random() * 10) +
-      [...Array(3)].map(() => c[(Math.random() * c.length) | 0]).join``;
-    if (
-      !arr.includes(i) &&
-      !a.includes(i) &&
-      (i.match(/[a-z]/g) || []).length >= 2 &&
-      (i.match(/[0-9]/g) || []).length >= 2
-    ) {
-      arr.push(i);
+const generateRandomStrings = (length, count, excludedStrings) => {
+  const result = [];
+  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const excludedSet = new Set(excludedStrings);
+
+  for (let i = 0; i < count; i++) {
+    let randomString = "";
+    let lettersCount = Math.ceil(length / 2); // 字母数量为生成长度的一半，向上取整
+    let numbersCount = length - lettersCount; // 数字数量为生成长度减去字母数量
+
+    while (randomString.length < length) {
+      const randomChar =
+        characters[Math.floor(Math.random() * characters.length)];
+
+      if (!excludedSet.has(randomChar)) {
+        if (/[a-z]/.test(randomChar) && lettersCount > 0) {
+          randomString += randomChar;
+          lettersCount--;
+        } else if (/[0-9]/.test(randomChar) && numbersCount > 0) {
+          randomString += randomChar;
+          numbersCount--;
+        }
+      }
     }
+    result.push(randomString);
   }
-  return arr;
+
+  return result;
 };
-//eslint-disable-next-line no-console
-console.log(
-  g([
-    "bq69",
-    "p60v",
-    "45iy",
-    "6xc6",
-    "e6b4",
-    "36jn",
-    "h2w0",
-    "3dn3",
-    "u4c5",
-    "h7t9",
-    "3k4s",
-    "vw31",
-    "d5e2",
-    "4d8m",
-    "p53r",
-    "5zv8",
-    "e84n",
-    "n74s",
-    "le78",
-    "0o5c",
-    "9u8z",
-    "n4r4",
-    "rt25",
-    "16vy",
-    "2rb7",
-    "9f5m",
-    "05su",
-    "9oy5",
-    "58mz",
-    "1w7o",
-    "0vk2",
-    "1zs6",
-    "3vi5",
-    "2l5m",
-    "9ms5",
-    "2rb7",
-  ]),
-);
+
+console.log(generateRandomStrings(4, 10, ["a", "b", "c"]));
