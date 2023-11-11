6ed31ac4eb09ac0a973d8d1e798b1c87caa50cd9
lengyibai <1329670984@qq.com>
2023-11-12 00:55:56 +0800

    msic: 兼容低版本浏览器

--- public/js/flexible.js
+++ public/js/flexible.js
@@ -1,24 +1,26 @@
-const min_font_size = 10;
-const max_font_size = 16;
-const min_device_width = 320;
-const max_device_width = 1920;
-const html = document.documentElement;
+var flexible_min_font_size = 10;
+var flexible_max_font_size = 16;
+var flexible_min_device_width = 320;
+var flexible_max_device_width = 1920;
+var flexible_html = document.documentElement;
 
-function onResizeFn() {
-  let font_size;
-  const width = html.clientWidth;
+function flexible_onResizeFn() {
+  var font_size;
+  var width = flexible_html.clientWidth;
 
-  if (width < min_device_width) {
-    font_size = min_font_size;
-  } else if (width > max_device_width) {
-    font_size = max_font_size;
+  if (width < flexible_min_device_width) {
+    font_size = flexible_min_font_size;
+  } else if (width > flexible_max_device_width) {
+    font_size = flexible_max_font_size;
   } else {
-    const progress = (width - min_device_width) / (max_device_width - min_device_width);
-    font_size = min_font_size + progress * (max_font_size - min_font_size);
+    var progress =
+      (width - flexible_min_device_width) / (flexible_max_device_width - flexible_min_device_width);
+    font_size =
+      flexible_min_font_size + progress * (flexible_max_font_size - flexible_min_font_size);
   }
 
-  html.style.fontSize = `${font_size}px`;
+  flexible_html.style.fontSize = font_size + "px";
 }
 
-onResizeFn();
-window.addEventListener("resize", onResizeFn);
+flexible_onResizeFn();
+window.onresize = flexible_onResizeFn;
