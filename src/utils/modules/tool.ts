af110e25566eb5cb59b7578a08c7fe830905a266
kunbei <kunbei@gmail.com>
2023-11-17 15:57:34 +0800

    feat: 增加乱序函数

--- src/utils/modules/tool.ts
+++ src/utils/modules/tool.ts
@@ -1002,3 +1002,12 @@ export const preloadImages = async (images: string[]) => {
     });
   }
 };
+
+/** @description 数组乱序 */
+export const shuffleArray = <T>(arr: T[]): T[] => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
