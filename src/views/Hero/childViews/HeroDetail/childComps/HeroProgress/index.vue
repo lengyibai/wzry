f6c3f882624ced67e16b6596b76c8e35241990ee
kunbei <kunbei@gmail.com>
2023-11-20 19:19:54 +0800

    rename: Heroprogress=>HeroProgress

---
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroProgress/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { AudioStore } from "@/store";
+
+interface Props {
+  /** 滚动索引 */
+  index: number;
+}
+
+defineProps<Props>();
+const $emit = defineEmits<{
+  toggle: [v: number];
+}>();
+
+const $audioStore = AudioStore();
+
+/** 滚动索引标题 */
+const page_name = ["英雄资料", "技能信息", "皮肤语音"];
+
+/* 设置进度 */
+const handleToggle = (index: number) => {
+  $emit("toggle", index);
+  $audioStore.play("n4r4");
+};
+</script>
+
+<template>
+  <!-- 滚动进度 -->
+  <div class="hero-progress">
+    <div
+      v-for="(item, i) in page_name"
+      :key="i"
+      class="page-index"
+      :class="{ active: index === i + 1 }"
+      @click="handleToggle(i + 1)"
+    >
+      <div v-show="index !== i + 1" class="tab">
+        {{ item }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
