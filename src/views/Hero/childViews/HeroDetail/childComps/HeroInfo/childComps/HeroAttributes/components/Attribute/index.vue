3d145e6fdd016295ad4cd853508d99ef611d9346
kunbei <kunbei@gmail.com>
2023-12-06 22:21:56 +0800

    visual: 竖着显示英雄属性

---
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroInfo/childComps/HeroAttributes/components/Attribute/index.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+interface Props {
+  /** 属性名 */
+  attr: keyof Hero.SkillKey;
+  /** 长度 */
+  length: number;
+}
+
+defineProps<Props>();
+
+/** 精灵图坐标 */
+const y: Record<string, string> = {
+  survival: "0 0px",
+  attack: "-3.75rem 0rem",
+  effect: "-7.5rem 0rem",
+  difficulty: "-11.25rem 0rem",
+};
+
+/** 背景色 */
+const bgc: Record<string, string> = {
+  survival: "#1081d2",
+  attack: "#d3b95b",
+  effect: "#55c06b",
+  difficulty: "#d3422b",
+};
+
+/** 描述 */
+const text: Hero.SkillKey = {
+  survival: "生存能力",
+  attack: "攻击伤害",
+  effect: "技能效果",
+  difficulty: "上手难度",
+};
+</script>
+
+<template>
+  <div class="attribute">
+    <span class="label">{{ text[attr] }}</span>
+    <div class="bar">
+      <div
+        class="bar-inner"
+        :style="{
+          backgroundColor: bgc[attr],
+          height: length + '%',
+        }"
+      ></div>
+    </div>
+    <div class="ico" :style="{ backgroundPosition: y[attr] }"></div>
+  </div>
+</template>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
---
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroInfo/childComps/HeroAttributes/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+
+import Attribute from "./components/Attribute/index.vue";
+
+import { HeroDetailStore } from "@/store";
+
+const { hero_info } = storeToRefs(HeroDetailStore());
+</script>
+
+<template>
+  <div class="hero-attributes">
+    <Attribute attr="survival" :length="hero_info.survival" />
+    <Attribute attr="attack" :length="hero_info.attack" />
+    <Attribute attr="effect" :length="hero_info.effect" />
+    <Attribute attr="difficulty" :length="hero_info.difficulty" />
+  </div>
+</template>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
