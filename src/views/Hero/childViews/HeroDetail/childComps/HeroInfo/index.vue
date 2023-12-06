3650bdff1ee1b2b04016268b90e6d5f2da1180c0
kunbei <kunbei@gmail.com>
2023-12-06 22:25:53 +0800

    visual: 暂时注释入场动画

--- src/views/Hero/childViews/HeroDetail/childComps/HeroInfo/index.vue
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroInfo/index.vue
@@ -4,45 +4,41 @@ import { storeToRefs } from "pinia";
 
 import HeroTitle from "./childComps/HeroTitle/index.vue";
 import HeroBasic from "./childComps/HeroBasic/index.vue";
-import HeroRelationship from "./childComps/HeroRelationship/index.vue";
-import HeroAttribute from "./childComps/HeroAttribute/index.vue";
+import HeroRelationships from "./childComps/HeroRelationships/index.vue";
+import HeroAttributes from "./childComps/HeroAttributes/index.vue";
+import HeroRelationDesc from "./childComps/HeroRelationDesc/index.vue";
 
 import { HeroDetailStore } from "@/store";
 import { $tip, $tool } from "@/utils";
 import { KScrollTip } from "@/components/business";
 
-const { hero_info } = storeToRefs(HeroDetailStore());
-
-const relationshipRef = ref<InstanceType<typeof HeroRelationship>>();
+const relationshipRef = ref<InstanceType<typeof HeroRelationships>>();
 const downRef = ref<InstanceType<typeof KScrollTip>>();
 
 /** 控制页面元素显示 */
-const into = ref(false);
+const into = ref(true);
 
 onMounted(() => {
   //设置按顺序出场的动画
-  setTimeout(async () => {
-    into.value = true;
-
-    await nextTick();
-
-    setTimeout(() => {
-      if (!relationshipRef.value?.el) return;
-      const focusRelationship = new $tool.FocusElement(relationshipRef.value.el);
-
-      //显示完滚动提示后显示关系提示
-      $tip({
-        text: "05su",
-        align: "right-bottom",
-        createFn: () => {
-          focusRelationship.focus();
-        },
-        btnFn: () => {
-          focusRelationship.blur();
-        },
-      });
-    }, 1000);
-  }, 1000);
+  // setTimeout(async () => {
+  //   into.value = true;
+  //   await nextTick();
+  //   setTimeout(() => {
+  //     if (!relationshipRef.value?.el) return;
+  //     const focusRelationship = new $tool.FocusElement(relationshipRef.value.el);
+  //     //显示完滚动提示后显示关系提示
+  //     $tip({
+  //       text: "05su",
+  //       align: "right-bottom",
+  //       createFn: () => {
+  //         focusRelationship.focus();
+  //       },
+  //       btnFn: () => {
+  //         focusRelationship.blur();
+  //       },
+  //     });
+  //   }, 1000);
+  // }, 1000);
 });
 </script>
 
@@ -51,27 +47,26 @@ onMounted(() => {
     <!-- 英雄标题 -->
     <HeroTitle :class="{ show: !into }" />
 
+    <!-- 中间 -->
     <div class="basic-info__center">
-      <!-- 左侧基础数据 -->
-      <HeroBasic />
+      <div class="center-left">
+        <!-- 左侧基础数据 -->
+        <HeroBasic />
 
-      <transition name="relationship">
-        <HeroRelationship v-if="into" ref="relationshipRef" />
-      </transition>
+        <!-- 关系信息 -->
+        <HeroRelationDesc />
+      </div>
+
+      <!-- 关系图谱 -->
+      <HeroRelationships ref="relationshipRef" />
+
+      <!-- 属性列表 -->
+      <HeroAttributes />
     </div>
 
-    <!--  -->
-    <div class="basic-info__bottom">
-      <!-- 属性 -->
-      <transition name="attribute">
-        <div v-if="into" class="hero-attribute">
-          <HeroAttribute attr="survival" :length="hero_info.survival" />
-          <HeroAttribute attr="attack" :length="hero_info.attack" />
-          <HeroAttribute attr="effect" :length="hero_info.effect" />
-          <HeroAttribute attr="difficulty" :length="hero_info.difficulty" />
-        </div>
-      </transition>
-    </div>
+    <!-- 底部 -->
+    <div class="basic-info__bottom"></div>
+
     <!-- 可滚动提示 -->
     <KScrollTip ref="downRef" />
   </div>
