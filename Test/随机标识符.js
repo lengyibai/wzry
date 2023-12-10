c2aa96b19670616a5f508d8fb7d19acda2f7548d
lengyibai <1329670984@qq.com>
2023-12-11 02:03:48 +0800

    test: 测试文件更新

--- Test/随机标识符.js
+++ Test/随机标识符.js
@@ -27,4 +27,33 @@ const generateRandomStrings = (length, count, excludedStrings) => {
   return result;
 };
 
-console.log(generateRandomStrings(4, 10, ["a", "b", "c"]));
+console.log(
+  generateRandomStrings(4, 1, [
+    "bq69",
+    "p60v",
+    "45iy",
+    "6xc6",
+    "e6b4",
+    "36jn",
+    "h2w0",
+    "3dn3",
+    "u4c5",
+    "h7t9",
+    "3k4s",
+    "vw31",
+    "d5e2",
+    "4d8m",
+    "p53r",
+    "5zv8",
+    "e84n",
+    "n74s",
+    "le78",
+    "range",
+    "0o5c",
+    "9u8z",
+    "n4r4",
+    "rt25",
+    "16vy",
+    "kj62",
+  ]),
+);
