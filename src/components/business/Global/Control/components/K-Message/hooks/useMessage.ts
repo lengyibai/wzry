858f44a5c87f8cb9a46ae840c0bf7379b8a3b748
kunbei <kunbei@gmail.com>
2023-12-10 12:24:29 +0800

    perf: 优化分类整合导出

--- src/components/business/Global/Control/components/K-Message/hooks/useMessage.ts
+++ src/components/business/Global/Control/components/K-Message/hooks/useMessage.ts
@@ -6,44 +6,48 @@ import { AudioStore } from "@/store";
 export default () => {
   const $audioStore = AudioStore();
 
-  /** 消息队列 */
-  const messages = reactive<MsgText[]>([]);
+  const ExposeData = {
+    /** 消息队列 */
+    messages: reactive<MsgText[]>([]),
+  };
+  const { messages } = ExposeData;
 
-  const msg = (text = "未设置提示", type: MsgType = "info") => {
-    /** 获取文字长度 */
-    const text_length = text.split("").length / 3;
-    /** 通过文字长度，设置显示时长 */
-    const time = text_length > 3 ? text_length : text_length + 1;
+  const ExposeMethods = {
+    msg(text = "未设置提示", type: MsgType = "info") {
+      /** 获取文字长度 */
+      const text_length = text.split("").length / 3;
+      /** 通过文字长度，设置显示时长 */
+      const time = text_length > 3 ? text_length : text_length + 1;
 
-    //延迟提醒，避免与点击操作同时播放
-    setTimeout(() => {
-      const msgName = {
-        info: "n74s",
-        warning: "16vy",
-        error: "vw31",
-      };
-
-      //播放指定名称的音效
-      $audioStore.play(msgName[type]);
-
-      //创建消息内容
-      const msgText: MsgText = {
-        id: dayjs().unix(),
-        text,
-        type,
-      };
-
-      //将消息内容添加进消息队列
-      messages.unshift(msgText);
+      //延迟提醒，避免与点击操作同时播放
       setTimeout(() => {
-        messages.pop();
-      }, time * 1000);
-    });
+        const msgName = {
+          info: "n74s",
+          warning: "16vy",
+          error: "vw31",
+        };
+
+        //播放指定名称的音效
+        $audioStore.play(msgName[type]);
+
+        //创建消息内容
+        const msgText: MsgText = {
+          id: dayjs().unix(),
+          text,
+          type,
+        };
+
+        //将消息内容添加进消息队列
+        messages.unshift(msgText);
+        setTimeout(() => {
+          messages.pop();
+        }, time * 1000);
+      });
+    },
   };
 
   return {
-    /** 消息队列 */
-    messages,
-    msg,
+    ...ExposeData,
+    ...ExposeMethods,
   };
 };
--- src/layout/childComps/BarrageMain/hooks/useBarrages.ts
+++ src/layout/childComps/BarrageMain/hooks/useBarrages.ts
@@ -11,74 +11,79 @@ const useBarrages = () => {
   const audioPlay = new $tool.AudioPlayer({ end() {} });
   let barragesMove: $tool.BarragesMove;
 
-  /** 是否显示信息卡片 */
-  const show_card = ref(false);
-  /** 当前点击的弹幕信息 */
-  const barrage_info = ref<{
-    /** 语音文字 */
-    voiceText: string;
-    /** 英雄名 */
-    heroName: string;
-    /** 英雄头像 */
-    headImg: string;
-    /** 皮肤名 */
-    skinName: string;
-    /** 皮肤海报 */
-    skinBlurPoster: string;
-    /** 皮肤海报 */
-    skinPoster: string;
-    /** 海报大图 */
-    skinBigPoster: string;
-  }>();
+  const ExposeData = {
+    /** 是否显示信息卡片 */
+    show_card: ref(false),
+    /** 当前点击的弹幕信息 */
+    barrage_info: ref<{
+      /** 语音文字 */
+      voiceText: string;
+      /** 英雄名 */
+      heroName: string;
+      /** 英雄头像 */
+      headImg: string;
+      /** 皮肤名 */
+      skinName: string;
+      /** 皮肤海报 */
+      skinBlurPoster: string;
+      /** 皮肤海报 */
+      skinPoster: string;
+      /** 海报大图 */
+      skinBigPoster: string;
+    }>(),
+  };
+  const { show_card, barrage_info } = ExposeData;
 
-  /** @description 初始化 */
-  const init = (data: Barrage[], parent: HTMLElement, card: HTMLElement) => {
-    barragesMove?.destruction();
+  const ExposeMethods = {
+    /** @description 初始化 */
+    init(data: Barrage[], parent: HTMLElement, card: HTMLElement) {
+      barragesMove?.destruction();
 
-    barragesMove = new $tool.BarragesMove(parent, data, {
-      async click(v, e) {
-        const { skinName, heroId, link, text: voiceText } = v;
-        barrage_info.value = undefined;
-        show_card.value = true;
+      barragesMove = new $tool.BarragesMove(parent, data, {
+        async click(v, e) {
+          const { skinName, heroId, link, text: voiceText } = v;
+          barrage_info.value = undefined;
+          show_card.value = true;
 
-        //播放语音
-        audioPlay.play(link);
+          //播放语音
+          audioPlay.play(link);
 
-        //获取英雄和皮肤信息
-        const hero = await API_HERO.getHeroDetail(heroId);
-        const skin = (await API_SKIN.getHeroSkin(heroId, skinName)) || {};
-        barrage_info.value = {
-          voiceText,
-          heroName: hero.name,
-          headImg: hero.headImg,
-          skinName,
-          skinBlurPoster: skin.posterBlur || hero.posterBlur,
-          skinPoster: skin.poster || hero.poster,
-          skinBigPoster: skin.posterBig || hero.posterBig,
-        };
+          //获取英雄和皮肤信息
+          const hero = await API_HERO.getHeroDetail(heroId);
+          const skin = (await API_SKIN.getHeroSkin(heroId, skinName)) || {};
+          barrage_info.value = {
+            voiceText,
+            heroName: hero.name,
+            headImg: hero.headImg,
+            skinName,
+            skinBlurPoster: skin.posterBlur || hero.posterBlur,
+            skinPoster: skin.poster || hero.poster,
+            skinBigPoster: skin.posterBig || hero.posterBig,
+          };
 
-        new Image().src = barrage_info.value.skinBlurPoster;
+          new Image().src = barrage_info.value.skinBlurPoster;
 
-        //设置信息弹窗坐标
-        if (!card) return;
+          //设置信息弹窗坐标
+          if (!card) return;
 
-        card.style.left = `calc(${e.clientX}px - ${card.offsetWidth / 2}px)`;
-        card.style.top = `calc(${e.clientY}px + 1.5rem)`;
+          card.style.left = `calc(${e.clientX}px - ${card.offsetWidth / 2}px)`;
+          card.style.top = `calc(${e.clientY}px + 1.5rem)`;
 
-        setTimeout(() => {
-          if (card.offsetLeft < 0) {
-            card.style.left = "0";
-          }
+          setTimeout(() => {
+            if (card.offsetLeft < 0) {
+              card.style.left = "0";
+            }
 
-          if (card.offsetLeft + card.offsetWidth > window.innerWidth) {
-            card.style.left = `${window.innerWidth - card.offsetWidth}px`;
-          }
-        }, 250);
-      },
-      finish: () => {
-        $barrageStore.getBarrages();
-      },
-    });
+            if (card.offsetLeft + card.offsetWidth > window.innerWidth) {
+              card.style.left = `${window.innerWidth - card.offsetWidth}px`;
+            }
+          }, 250);
+        },
+        finish: () => {
+          $barrageStore.getBarrages();
+        },
+      });
+    },
   };
 
   onScopeDispose(() => {
@@ -86,11 +91,8 @@ const useBarrages = () => {
   });
 
   return {
-    /** 当前点击的弹幕信息 */
-    barrage_info,
-    /** 是否显示信息卡片 */
-    show_card,
-    init,
+    ...ExposeData,
+    ...ExposeMethods,
   };
 };
 
