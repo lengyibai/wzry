f5670f3656b77ce83031ee26458f5d15855e04c8
lengyibai <1329670984@qq.com>
2023-12-17 23:57:23 +0800

    feat: 增加获取铭文类型和效果列表

--- src/api/modules/games/epigraph.ts
+++ src/api/modules/games/epigraph.ts
@@ -8,3 +8,19 @@ export const getEpigraphList = () => {
   });
   return Promise.resolve(epigraph_list);
 };
+
+/** @description 获取铭文类型列表 */
+export const getEpigraphType = () => {
+  const epigraph_type = get<General[]>({
+    name: CONFIG.LOCAL_KEY.EPIGRAPH_TYPE,
+  });
+  return Promise.resolve(epigraph_type);
+};
+
+/** @description 获取铭文效果列表 */
+export const getEpigraphEffect = () => {
+  const epigraph_effect = get<General[]>({
+    name: CONFIG.LOCAL_KEY.EPIGRAPH_EFFECT,
+  });
+  return Promise.resolve(epigraph_effect);
+};
