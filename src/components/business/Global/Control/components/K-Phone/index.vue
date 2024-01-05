3e49e0648f77af72579ee82f9cc492b46f296e5c
kunbei <kunbei@gmail.com>
2024-01-05 17:31:40 +0800

    feat: 增加限制手机查看的组件

---
+++ src/components/business/Global/Control/components/K-Phone/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { $tool } from "@/utils";
+</script>
+
+<template>
+  <teleport to="body">
+    <div v-if="$tool.isPhone" class="k-phone">版本迭代阶段<br />暂不支持手机查看</div>
+  </teleport>
+</template>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
