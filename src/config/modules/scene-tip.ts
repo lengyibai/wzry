22bdf4d647ab131150da3522e1b0fd28070af530
kunbei <kunbei@gmail.com>
2024-01-06 02:06:06 +0800

    type: 设置类型

--- src/config/modules/scene-tip.ts
+++ src/config/modules/scene-tip.ts
@@ -1,10 +1,10 @@
 /** @description 小贴士配置 */
-const SCENE_TIP = {
+const SCENE_TIP: Record<keyof Global.Tip.Key, string> = {
   d7o5: "底部工具栏可切换音乐！",
-  f1y0: "我又回来啦！",
   kr53: "顶部的弹幕可以点击查看语音来源！",
   ma67: "点击英雄关系可切换英雄信息！",
   mu63: "不要忽略右上角的工具栏！",
+  f1y0: "",
 
   /**
    * PC端专属
@@ -15,6 +15,6 @@ const SCENE_TIP = {
    * 移动端专属
    */
   lp57: "手指按住边缘头像，拖动到中心位置并松开(点击头像也可以)。",
-} as const;
+};
 
 export { SCENE_TIP };
