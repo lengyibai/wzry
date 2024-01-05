6554e6d46559f23b102b1b0a5caf9bc6bba50f30
kunbei <kunbei@gmail.com>
2024-01-06 03:15:46 +0800

    perf: 双击可隐藏蒙版

--- src/components/business/Global/Control/components/K-Phone/index.vue
+++ src/components/business/Global/Control/components/K-Phone/index.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 import { $tool } from "@/utils";
+const show = ref(true);
 </script>
 
 <template>
   <teleport to="body">
-    <div v-if="$tool.isPhone" class="k-phone">版本迭代阶段<br />暂不支持手机查看</div>
+    <div v-if="$tool.isPhone && show" class="k-phone" @dblclick="show = false">
+      版本迭代阶段<br />暂不支持手机查看
+    </div>
   </teleport>
 </template>
 
