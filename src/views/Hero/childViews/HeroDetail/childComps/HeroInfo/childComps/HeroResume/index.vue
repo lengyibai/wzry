8ffb7a67549401995f4b19b7697ae83dd86bd83d
kunbei <kunbei@gmail.com>
2024-01-07 01:34:42 +0800

    feat: 增加英雄简述

---
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroInfo/childComps/HeroResume/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { HeroDetailStore } from "@/store";
+import { vTypewriterMultiple } from "@/directives";
+
+const $heroDetail = HeroDetailStore();
+
+const hero = computed(() => $heroDetail.hero_info);
+</script>
+
+<template>
+  <div
+    v-typewriter-multiple="{
+      speed: 25,
+      content: hero.resume,
+    }"
+    class="hero-resume"
+  ></div>
+</template>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
