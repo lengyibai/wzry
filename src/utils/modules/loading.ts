15506cb6edb35658e4d8d30e72e710fab05c5bde
kunbei <kunbei@gmail.com>
2024-01-07 18:22:04 +0800

    annota: 注释优化

--- src/utils/modules/loading.ts
+++ src/utils/modules/loading.ts
@@ -1,37 +1,43 @@
 import { $bus } from "./eventBus";
 
-let loadingTimeout: NodeJS.Timeout | undefined;
-let needLoadingRequestCount = 0;
-let timer: NodeJS.Timeout | undefined;
+let loadingCount = 0;
+let closeTimer: NodeJS.Timeout | undefined;
+let openTimer: NodeJS.Timeout | undefined;
 
 /** @description 开启loading */
-const show = async (text: string) => {
-  timer = setTimeout(() => {
-    if (loadingTimeout) clearTimeout(loadingTimeout);
+const show = (text: string) => {
+  //延迟显示loading，避免出现加载速度太快loading一闪而过
+  openTimer = setTimeout(() => {
+    //如果loading在延迟关闭之前再次被调用，则清除关闭定时器
+    if (closeTimer) clearTimeout(closeTimer);
 
-    if (needLoadingRequestCount === 0) {
+    if (loadingCount === 0) {
       $bus.emit("loading", {
         show: true,
         text,
       });
     }
 
-    needLoadingRequestCount++;
+    loadingCount++;
   }, 250);
 };
 
 /** @description 关闭loading */
-const close = async () => {
-  clearTimeout(timer);
+const close = () => {
+  //解决loading加载速度太快loading一闪而过
+  clearTimeout(openTimer);
 
-  if (needLoadingRequestCount > 0) {
-    needLoadingRequestCount--;
+  //只有在大于0的时候减少，避免出现负数
+  if (loadingCount > 0) {
+    loadingCount--;
   }
 
-  if (needLoadingRequestCount === 0) {
-    loadingTimeout = setTimeout(() => {
+  //只有当所有loading结束后才能隐藏loading
+  if (loadingCount === 0) {
+    //延迟关闭loading，避免出现请求结束后立即执行下一个请求出现闪现
+    closeTimer = setTimeout(() => {
       $bus.emit("loading", { show: false });
-      loadingTimeout = undefined;
+      closeTimer = undefined;
     }, 500);
   }
 };
