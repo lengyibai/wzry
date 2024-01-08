cfefaccd3d7d695c0696e38db2c2cf3373e12d01
lengyibai <1329670984@qq.com>
2024-01-08 22:29:55 +0800

    perf: readme打开新页面

--- src/views/Base/Login/index.vue
+++ src/views/Base/Login/index.vue
@@ -4,7 +4,6 @@ import { storeToRefs } from "pinia";
 
 import RegLogin from "./childComps/RegLogin/index.vue";
 import Notice from "./childComps/Notice/index.vue";
-import ReadMe from "./childComps/ReadMe/index.vue";
 import Team from "./childComps/Team/index.vue";
 import ToolBar from "./childComps/ToolBar/index.vue";
 import DownLoad from "./childComps/DownLoad/index.vue";
@@ -16,14 +15,12 @@ import { SCENE_TIP } from "@/config";
 
 const { config } = storeToRefs(SettingStore());
 
-const { getImgLink, getVideoLink } = $concise;
+const { getImgLink, getVideoLink, getHtmlLink } = $concise;
 
 const toolbarRef = ref<InstanceType<typeof ToolBar>>();
 
 /** 显示公告 */
 const show_notice = ref(true);
-/** 显示README */
-const show_readme = ref(false);
 /** 显示开黑 */
 const show_team = ref(false);
 /** 数据下载完成 */
@@ -37,7 +34,9 @@ const hideRegLogin = computed(() => !finish.value || show_notice.value || show_t
  * @param v 点击工具栏的静音、README、公告按钮类型
  */
 const onToolType = (v: string) => {
-  show_readme.value = v === "readme";
+  if (v === "readme") {
+    open(getHtmlLink("readme"));
+  }
   show_notice.value = v === "notice";
   show_team.value = v === "team";
 };
@@ -77,9 +76,6 @@ const onCloseNotice = () => {
 
     <!-- 公告 -->
     <Notice v-if="show_notice && finish" v-model="show_notice" @close="onCloseNotice" />
-
-    <!-- README -->
-    <ReadMe v-if="show_readme" v-model="show_readme" />
 
     <!-- 开黑 -->
     <Team v-if="show_team" v-model="show_team" />
