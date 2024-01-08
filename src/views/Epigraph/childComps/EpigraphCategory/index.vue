0a5e9cb68365701e8a2bc3bd35a94e19bab4713d
lengyibai <1329670984@qq.com>
2024-01-09 00:01:35 +0800

    feat: tab栏支持点击tab自动滚动

--- src/views/Epigraph/childComps/EpigraphCategory/index.vue
+++ src/views/Epigraph/childComps/EpigraphCategory/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { onMounted } from "vue";
 
 import { AudioStore, EpigraphStore } from "@/store";
 import { $concise } from "@/utils";
@@ -23,24 +24,47 @@ const epigraph: Record<string, Game.Epigraph.Category>[] = [
   { title: "穿透" },
 ];
 
+const epigraphCategoryRef = ref<HTMLElement>();
+
 /** 当前点击的分类索引 */
 const current_index = ref(0);
+/** 线条x坐标 */
+const left = ref(0);
+/** 线条宽度 */
+const width = ref(0);
 
 /* 点击分类标题 */
 const handleToggle = (index: number, type: Game.Epigraph.Category) => {
-  $audioStore.play("n4r4");
+  if (!epigraphCategoryRef.value) return;
   current_index.value = index;
-  //每次点击重新筛选数据
+  $audioStore.play("n4r4");
   $epigraphStore.setFilter(type);
+
+  const parent_width = epigraphCategoryRef.value.clientWidth;
+  const el = epigraphCategoryRef.value!.children[index + 1] as HTMLElement;
+  const x = el.getBoundingClientRect().x;
+
+  /** 点击的tab距离中间的差值 */
+  const center_difference = x - parent_width / 2 + el.offsetWidth / 2;
+  /** 容器需要滚动的长度 */
+  const scroll_left = el.offsetLeft - x + center_difference;
+
+  left.value = el.offsetLeft;
+  width.value = el.offsetWidth;
+  epigraphCategoryRef.value.scroll({ behavior: "smooth", left: scroll_left });
 };
+
+onMounted(() => {
+  handleToggle(0, "全部");
+});
 </script>
 
 <template>
   <div class="epigraph-tool">
-    <div class="epigraph-category">
+    <div ref="epigraphCategoryRef" class="epigraph-category">
       <!-- 滑动的图标 -->
       <img
-        :style="{ left: current_index * 11.11 + '%' }"
+        :style="{ left: left + 'px', width: width + 'px' }"
         :src="getImgLink('epigraph_active')"
         alt=""
       />
