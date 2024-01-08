93c2fe1bdb88b7ded14ea6b6cb2c9165e95c126a
lengyibai <1329670984@qq.com>
2024-01-08 17:44:50 +0800

    perf: 在技能页右下角增加副技能切换状态

---
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroSkill/childComps/HeroSkillToggle/index.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+
+import { HeroDetailStore } from "@/store";
+import { $tool, $tip } from "@/utils";
+import { MOUSE_TIP } from "@/config";
+import { vMouseTip } from "@/directives";
+
+const $emit = defineEmits<{
+  toggle: [];
+}>();
+
+const { hero_info } = storeToRefs(HeroDetailStore());
+const { setScrollFn } = HeroDetailStore();
+
+const toggleRef = ref<HTMLElement>();
+
+/** 显示技能切换图标 */
+const show = ref(false);
+/** 主副技能索引 */
+const deputy_index = ref(0);
+
+/* 当滚动到技能页，播放出场动画 */
+setScrollFn("skinIcon", (pageName) => {
+  //只有当前页面未加载或者滚动索引为3才会触发
+  if (show.value || pageName !== "技能信息") return;
+  show.value = true;
+
+  //存在多套技能执行下列代码
+  const length = hero_info.value.skills.length;
+  if (length > 1) {
+    setTimeout(() => {
+      if (!toggleRef.value) return;
+      const toggleFocus = new $tool.FocusElement(toggleRef.value);
+
+      $tip({
+        align: "right-top",
+        text: `${hero_info.value.name}存在${
+          length == 3 ? "三" : "两"
+        }套技能，页面右下角有个切换副技能的按钮，点击它吧！`,
+        createFn: () => {
+          toggleFocus.focus();
+        },
+        btnFn: () => {
+          toggleFocus.blur();
+        },
+      });
+    }, 500);
+  }
+});
+
+/* 切换副技能 */
+const handleToggleSkill = () => {
+  $emit("toggle");
+  //判断当前切换是否为最后一个副技能
+  if (deputy_index.value === hero_info.value.skills.length - 1) {
+    deputy_index.value = 0;
+  } else {
+    deputy_index.value += 1;
+  }
+};
+</script>
+
+<template>
+  <div ref="toggleRef" class="hero-skill-toggle" :class="{ 'hide-bottom': !show }">
+    <div class="status">{{ deputy_index + 1 }}/{{ hero_info.skills.length }}</div>
+    <i
+      ref="toggleRef"
+      v-mouse-tip="{
+        text: MOUSE_TIP.wk12,
+      }"
+      class="toggle iconfont wzry-qiehuan"
+      @click="handleToggleSkill"
+    />
+  </div>
+</template>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
--- src/views/Hero/childViews/HeroDetail/childComps/HeroSkill/index.vue
+++ src/views/Hero/childViews/HeroDetail/childComps/HeroSkill/index.vue
@@ -4,53 +4,16 @@ import { storeToRefs } from "pinia";
 
 import HeroSkillIcon from "./childComps/HeroSkillIcon/index.vue";
 import HeroSkillContent from "./childComps/HeroSkillContent/index.vue";
+import HeroSkillToggle from "./childComps/HeroSkillToggle/index.vue";
 
 import { KScrollTip } from "@/components/business";
 import { HeroDetailStore } from "@/store";
-import { $tip, $tool } from "@/utils";
-import { vMouseTip } from "@/directives";
-import { MOUSE_TIP } from "@/config";
 
 const { hero_info } = storeToRefs(HeroDetailStore());
-const { setScrollFn } = HeroDetailStore();
-
 const heroSkillIconRef = ref<InstanceType<typeof HeroSkillIcon>>();
 
-const toggleRef = ref<HTMLElement>();
-
-/** 显示技能切换图标 */
-const show = ref(false);
 /** 技能信息 */
 const hero_skill = ref<Remote.Skill.Info>();
-
-/* 当滚动到技能页，播放出场动画 */
-setScrollFn("skinIcon", (pageName) => {
-  //只有当前页面未加载或者滚动索引为3才会触发
-  if (show.value || pageName !== "技能信息") return;
-  show.value = true;
-
-  //存在多套技能执行下列代码
-  const length = hero_info.value.skills.length;
-  if (length > 1) {
-    setTimeout(() => {
-      if (!toggleRef.value) return;
-      const toggleFocus = new $tool.FocusElement(toggleRef.value);
-
-      $tip({
-        align: "right-top",
-        text: `${hero_info.value.name}存在${
-          length == 3 ? "三" : "两"
-        }套技能，页面底部中间有个切换副技能的按钮，点击它吧！`,
-        createFn: () => {
-          toggleFocus.focus();
-        },
-        btnFn: () => {
-          toggleFocus.blur();
-        },
-      });
-    }, 500);
-  }
-});
 
 /* 选择技能 */
 const onSelectSkill = (skill: Remote.Skill.Info) => {
@@ -70,20 +33,11 @@ const onSelectSkill = (skill: Remote.Skill.Info) => {
       <HeroSkillContent v-if="hero_skill" :skill="hero_skill" />
     </transition>
 
+    <!-- 切换副技能 -->
+    <HeroSkillToggle v-if="hero_info.skills.length > 1" @toggle="heroSkillIconRef?.toggleSkill" />
+
     <!-- 可滚动提示 -->
     <KScrollTip ref="downRef" />
-
-    <!-- 切换副技能 -->
-    <i
-      v-if="hero_info.skills!.length > 1"
-      ref="toggleRef"
-      v-mouse-tip="{
-        text: MOUSE_TIP.wk12,
-      }"
-      class="toggle iconfont wzry-qiehuan"
-      :class="{ 'hide-bottom': !show }"
-      @click="heroSkillIconRef?.toggleSkill"
-    />
   </div>
 </template>
 
