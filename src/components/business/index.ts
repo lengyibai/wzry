001d22bef94c0244f4d4a077eef1d7e2bbef31ea
lengyibai <1329670984@qq.com>
2024-01-22 15:55:42 +0800

    update: 补充组件引入

--- src/components/business/index.ts
+++ src/components/business/index.ts
@@ -5,12 +5,14 @@ import Control from "./Global/Control/index.vue";
 import WaterMark from "./Global/WaterMark/index.vue";
 import KBackTop from "./Parts/K-BackTop/index.vue";
 import KButton from "./Parts/K-Button/index.vue";
+import KCheck from "./Parts/K-Check/index.vue";
 import KDialog from "./Parts/K-Dialog/index.vue";
 import KImageLoad from "./Parts/K-ImageLoad/index.vue";
 import KInput from "./Parts/K-Input/index.vue";
 import KLoadingIcon from "./Parts/K-LoadingIcon/index.vue";
 import KMarquee from "./Parts/K-Marquee/index.vue";
 import KRange from "./Parts/K-Range/index.vue";
+import KSelect from "./Parts/K-Select/index.vue";
 import KText from "./Parts/K-Text/index.vue";
 import KVideo from "./Parts/K-Video/index.vue";
 import KScrollTip from "./Parts/K-ScrollTip/index.vue";
@@ -29,11 +31,13 @@ export {
   KBackTop,
   KButton,
   KImageLoad,
+  KCheck,
   KDialog,
   KInput,
   KLoadingIcon,
   KMarquee,
   KRange,
+  KSelect,
   KText,
   KVideo,
   KScrollTip,
