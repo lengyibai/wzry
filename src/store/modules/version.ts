3298dabdd343527979762fddcdfe2e7fe7b07519
lengyibai <1329670984@qq.com>
2024-01-24 00:31:16 +0800

    perf: 更新日志从版本更新文件中分离

--- src/store/modules/version.ts
+++ src/store/modules/version.ts
@@ -25,7 +25,7 @@ const VersionStore = defineStore("version", () => {
     /** 文件是否需要更新 */
     dist_status: ref(false),
     /** 更新日志汇总 */
-    update_log: ref<Global.Version.UpdateLog>({
+    update_log: ref<Omit<Global.Version.UpdateLog, "voiceKey" | "dataKey">>({
       time: "",
       dataLog: [],
       voiceLog: [],
@@ -50,11 +50,6 @@ const VersionStore = defineStore("version", () => {
     dist_status,
     update_log,
   } = ExposeData;
-
-  /** 本地版本 */
-  local_data_version.value = localStorage.getItem(LOCAL_KEY.VERSION_DATA) || "";
-  /** 文件文件版本 */
-  local_dist_version.value = localStorage.getItem(LOCAL_KEY.VERSION_DIST) || "";
 
   /** @description 更新本地数据/文件版本 */
   const updateVersion = (v: string, type: "DATA" | "DIST") => {
@@ -81,62 +76,65 @@ const VersionStore = defineStore("version", () => {
     },
 
     /** @description 实时获取数据版本、文件版本、更新日志 */
-    watchVersion() {
+    async watchVersion() {
       clearTimeout(version_timer);
       version_timer = setTimeout(() => {
         watchVersion();
       }, 1000 * 60);
 
-      API_DATA.Version().then((res) => {
-        const { dataVersion, distVersion, dataKey, voiceKey, dataLog, distLog, time, voiceLog } =
-          res.data;
-        remote_data_version.value = dataVersion;
-        remote_dist_version.value = distVersion;
-        update_log.value = {
-          time,
-          dataLog,
-          voiceLog,
-          distLog,
-        };
+      const res = await API_DATA.Version();
+      const { dataVersion, distVersion } = res.data;
+      remote_data_version.value = dataVersion;
+      remote_dist_version.value = distVersion;
 
-        /* 数据更新 */
-        if (!local_data_version.value) {
-          updateVersion(dataVersion, "DATA");
-        } else {
-          const local = Number(local_data_version.value.replaceAll(".", ""));
-          const remote = Number(dataVersion.replaceAll(".", ""));
-          const compare = remote - local;
+      //如果本地没有版本，则直接更新数据版本
+      if (!local_data_version.value) {
+        updateVersion(dataVersion, "DATA");
+      } else {
+        const local = Number(local_data_version.value.replaceAll(".", ""));
+        const remote = Number(dataVersion.replaceAll(".", ""));
+        const compare = remote - local;
 
-          //如果为旧版，则自动更新并更新本地版本并返回更新改动
-          if (compare > 0) {
-            clearTimeout(version_timer);
-            data_status.value = true;
-            removeUpdateKey([...dataKey, ...voiceKey]);
+        //如果为旧版，则自动更新并更新本地版本并返回更新改动
+        if (compare > 0) {
+          const res = await API_DATA.UpdateLog();
+          const { dataKey, voiceKey, dataLog, distLog, time, voiceLog } = res.data;
 
-            useGetData()
-              .getData()
-              .then(() => {
-                show_update.value = true;
-              });
-          }
+          data_status.value = true;
+          update_log.value = {
+            time,
+            dataLog,
+            voiceLog,
+            distLog,
+          };
+
+          clearTimeout(version_timer);
+          removeUpdateKey([...dataKey, ...voiceKey]);
+
+          //重新获取被删除的数据
+          useGetData()
+            .getData()
+            .then(() => {
+              show_update.value = true;
+            });
         }
+      }
 
-        /* 文件更新 */
-        if (!local_dist_version.value) {
-          updateVersion(distVersion, "DIST");
-        } else {
-          const local = Number(local_dist_version.value.replaceAll(".", ""));
-          const remote = Number(distVersion.replaceAll(".", ""));
-          const compare = remote - local;
+      //如果本地没有版本，则直接更新文件版本
+      if (!local_dist_version.value) {
+        updateVersion(distVersion, "DIST");
+      } else {
+        const local = Number(local_dist_version.value.replaceAll(".", ""));
+        const remote = Number(distVersion.replaceAll(".", ""));
+        const compare = remote - local;
 
-          //如果为旧版，则自动更新并更新本地版本
-          if (compare > 0) {
-            clearTimeout(version_timer);
-            show_update.value = true;
-            dist_status.value = true;
-          }
+        //如果为旧版，则自动更新并更新本地版本
+        if (compare > 0) {
+          clearTimeout(version_timer);
+          show_update.value = true;
+          dist_status.value = true;
         }
-      });
+      }
     },
 
     /** @description 更新并重启 */
@@ -149,7 +147,16 @@ const VersionStore = defineStore("version", () => {
   };
   const { watchVersion } = ExposeMethods;
 
+  /** 本地版本 */
+  local_data_version.value = localStorage.getItem(LOCAL_KEY.VERSION_DATA) || "";
+  /** 文件文件版本 */
+  local_dist_version.value = localStorage.getItem(LOCAL_KEY.VERSION_DIST) || "";
+
   watchVersion();
+
+  API_DATA.UpdateLog().then((res) => {
+    update_log.value = res.data;
+  });
 
   return {
     ...ExposeData,
