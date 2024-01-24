ed5bf2f2377bcfaf4282418c8b76183619697a56
lengyibai <1329670984@qq.com>
2024-01-25 04:27:21 +0800

    remove: 效果列表移除一键拆卸和保存套装

--- src/views/Epigraph/views/EpigraphCollocation/components/EpigraphSidebar/components/EpigraphEffect/index.vue
+++ src/views/Epigraph/views/EpigraphCollocation/components/EpigraphSidebar/components/EpigraphEffect/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import _debounce from "lodash/debounce";
 
 import EpigraphGain from "./components/EpigraphGain/index.vue";
 import EpigraphInfo from "./components/EpigraphInfo/index.vue";
@@ -9,7 +8,6 @@ import type { CompKey } from "./interface";
 import { EpigraphCollocationStore } from "@/store";
 import { KButton, KSelect } from "@/components/business";
 import { vMouseTip } from "@/directives";
-import { $confirm, $message } from "@/utils";
 import { MOUSE_TIP } from "@/config";
 
 const comps = {
@@ -23,14 +21,6 @@ const comp_name = ref<CompKey>("GAIN");
 
 const $epigraphCollocationStore = EpigraphCollocationStore();
 
-/* 一键拆卸 */
-const handleClear = () => {
-  $confirm({
-    text: "确定拆卸铭文吗？",
-    confirm: $epigraphCollocationStore.clearColors,
-  });
-};
-
 /* 铭文属性及铭文信息切换 */
 const onSelect = (i: number) => {
   comp_name.value = keys[i];
@@ -40,16 +30,6 @@ const onSelect = (i: number) => {
 const handleSuit = () => {
   $epigraphCollocationStore.setSidebarStatus("SUIT");
 };
-
-/* 保存方案 */
-const debounceSaveSuit = _debounce(
-  () => {
-    $epigraphCollocationStore.syncSuit();
-    $message("已同步至方案列表，并自动保存到本地");
-  },
-  1000,
-  { leading: true, trailing: false },
-);
 </script>
 
 <template>
@@ -72,25 +52,6 @@ const debounceSaveSuit = _debounce(
 
     <div class="btn">
       <KButton v-mouse-tip class="k-button" @click="handleSuit">套装方案</KButton>
-      <KButton
-        v-mouse-tip="{
-          text: MOUSE_TIP.zk84,
-        }"
-        type="warning"
-        class="k-button"
-        @click="debounceSaveSuit"
-      >
-        同步并保存
-      </KButton>
-      <KButton
-        v-mouse-tip
-        class="k-button"
-        type="error"
-        :disabled="$epigraphCollocationStore.is_all_empty"
-        @click="handleClear"
-      >
-        一键拆卸
-      </KButton>
     </div>
   </div>
 </template>
