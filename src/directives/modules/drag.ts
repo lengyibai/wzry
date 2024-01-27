ccc032a898c2f8d1b3d5f0f9506b6c7193eaed7e
lengyibai <1329670984@qq.com>
2024-01-27 11:25:22 +0800

    refactor: 将PC和手机事件函数合并

--- src/directives/modules/drag.ts
+++ src/directives/modules/drag.ts
@@ -19,39 +19,56 @@ interface Params {
 }
 
 const vDrag: Directive<HTMLElement, Params> = {
-  // 指令的 mounted 钩子函数，在元素被插入到 DOM 中时触发
+  //指令的 mounted 钩子函数，在元素被插入到 DOM 中时触发
   mounted(el, binding) {
     const { handleDrag, index } = binding.value;
 
-    // 设置元素的样式，禁止用户选择元素内容，并将元素定位为绝对定位
+    //设置元素的样式，禁止用户选择元素内容，并将元素定位为绝对定位
     el.style.userSelect = "none";
     el.style.position = "absolute";
 
-    let x = 0; // 鼠标/触摸点的初始水平位置
-    let y = 0; // 鼠标/触摸点的初始垂直位置
-    let startX = 0; // 元素的初始水平位置
-    let startY = 0; // 元素的初始垂直位置
-    let moveX = 0; // 元素水平方向的移动距离
-    let moveY = 0; // 元素垂直方向的移动距离
+    /* 初始水平位置 */
+    let x = 0;
+    /* 初始垂直位置 */
+    let y = 0;
+    /* 元素的初始水平位置 */
+    let startX = 0;
+    /* 元素的初始垂直位置 */
+    let startY = 0;
+    /* 元素水平方向的移动距离 */
+    let moveX = 0;
+    /* 元素垂直方向的移动距离 */
+    let moveY = 0;
 
-    /* PC端 */
-    el.addEventListener("mousedown", (e: MouseEvent) => {
-      // 记录鼠标按下时的位置和元素初始位置
-      x = e.pageX;
-      y = e.pageY;
+    const handleDown = (event: MouseEvent | TouchEvent) => {
+      //记录置和元素初始位置
+      if (event instanceof MouseEvent) {
+        x = event.pageX;
+        y = event.pageY;
+      } else if (event instanceof TouchEvent) {
+        x = event.touches[0].pageX;
+        y = event.touches[0].pageY;
+      }
+
+      //获取元素初始位置
       startX = el.offsetLeft;
       startY = el.offsetTop;
 
-      function fn(e: MouseEvent) {
-        // 计算鼠标移动的距离
-        moveX = e.pageX - x;
-        moveY = e.pageY - y;
+      function handleMove(event: MouseEvent | TouchEvent) {
+        //计算
+        if (event instanceof MouseEvent) {
+          moveX = event.pageX - x;
+          moveY = event.pageY - y;
+        } else if (event instanceof TouchEvent) {
+          moveX = event.touches[0].pageX - x;
+          moveY = event.touches[0].pageY - y;
+        }
 
-        // 更新元素的位置
+        //更新元素的位置
         el.style.left = `${moveX + startX}px`;
         el.style.top = `${moveY + startY}px`;
 
-        // 调用回调函数，传递元素的位置信息和索引
+        //调用回调函数，传递元素的位置信息和索引
         handleDrag(
           el,
           {
@@ -62,75 +79,24 @@ const vDrag: Directive<HTMLElement, Params> = {
         );
       }
 
-      // 监听鼠标移动事件
-      window.addEventListener("mousemove", fn);
+      //监听      window.addEventListener("mousemove", handleMove);
+      window.addEventListener("touchmove", handleMove);
 
-      function up() {
-        // 鼠标松开时，调用回调函数，传递结束拖拽的信息和索引
+      function handleUp() {
+        //用回调函数，传递结束拖拽的信息和索引
         handleDrag(el, false, index);
 
-        // 移除鼠标移动事件的监听器
-        window.removeEventListener("mousemove", fn);
-
-        // 在下一个事件循环中，移除鼠标松开事件的监听器
-        setTimeout(() => {
-          window.removeEventListener("mouseup", up);
-        });
+        //移除监听器
+        window.removeEventListener("mousemove", handleMove);
+        window.removeEventListener("touchmove", handleMove);
       }
 
-      // 监听鼠标松开事件
-      window.addEventListener("mouseup", up);
-    });
+      //监听      window.addEventListener("mouseup", handleUp, { once: true });
+      window.addEventListener("touchend", handleUp, { once: true });
+    };
 
-    /* 兼容移动端 */
-    el.addEventListener("touchstart", (e: TouchEvent) => {
-      // 记录触摸点按下时的位置和元素初始位置
-      x = e.changedTouches[0].pageX;
-      y = e.changedTouches[0].pageY;
-      startX = el.offsetLeft;
-      startY = el.offsetTop;
-
-      function fn(e: TouchEvent) {
-        // 计算触摸点移动的距离
-        moveX = e.changedTouches[0].pageX - x;
-        moveY = e.changedTouches[0].pageY - y;
-
-        // 更新元素的位置
-        el.style.left = `${moveX + startX}px`;
-        el.style.top = `${moveY + startY}px`;
-
-        // 调用回调函数，传递元素的位置信息和索引
-        handleDrag(
-          el,
-          {
-            x: el.getBoundingClientRect().left + el.offsetWidth / 2,
-            y: el.getBoundingClientRect().top + el.offsetHeight / 2,
-          },
-          index,
-        );
-      }
-
-      // 监听触摸点移动事件
-      window.addEventListener("touchmove", fn);
-
-      function up(e: TouchEvent) {
-        e.stopPropagation();
-
-        // 触摸点松开时，调用回调函数，传递结束拖拽的信息和索引
-        handleDrag(el, false, index);
-
-        // 移除触摸点移动事件的监听器
-        window.removeEventListener("touchmove", fn);
-
-        // 在下一个事件循环中，移除触摸点松开事件的监听器
-        setTimeout(() => {
-          window.removeEventListener("touchend", up);
-        });
-      }
-
-      // 监听触摸点松开事件
-      window.addEventListener("touchend", up);
-    });
+    el.addEventListener("mousedown", handleDown);
+    el.addEventListener("touchstart", handleDown);
   },
 };
 
