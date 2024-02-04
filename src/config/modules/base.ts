c675346c6cdfb07feb4249ddf831980343ef3997
lengyibai <1329670984@qq.com>
2024-02-04 15:02:30 +0800

    update: 域名更换

--- src/config/modules/base.ts
+++ src/config/modules/base.ts
@@ -2,12 +2,10 @@
 const BASE_CONFIG = {
   /** 首页地址 */
   HOME_URL: "/hero",
-  /** 身份token过期时间(秒) */
-  OVERDUE_ROLE_TIME: 86400, //1天
   /** 数据token过期时间(秒) */
   OVERDUE_DATA_TIME: 259200, //3天
   /** 图床链接 */
-  IMGBED: "https://files.lyb.im/wzry-material",
+  IMGBED: "https://lyb.cbb.plus/wzry-material",
 };
 
 export { BASE_CONFIG };
