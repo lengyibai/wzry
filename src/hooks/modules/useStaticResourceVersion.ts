57db634b6551194bbe8d8d77db2e0ffa4613c2ed
lengyibai <1329670984@qq.com>
2024-02-04 15:28:11 +0800

    remove: 移除登录背景封面字段

--- src/hooks/modules/useStaticResourceVersion.ts
+++ src/hooks/modules/useStaticResourceVersion.ts
@@ -5,17 +5,14 @@ import { API_DATA } from "@/api";
 const ExposeData = {
   /** 登录页视频版本 */
   login_video_bg_version: ref(""),
-  /** 登录页视频封面版本 */
-  login_video_cover_version: ref(""),
 };
-const { login_video_bg_version, login_video_cover_version } = ExposeData;
+const { login_video_bg_version } = ExposeData;
 
 /** @description 静态资源版本 */
 const useStaticResourceVersion = () => {
   API_DATA.StaticVersion().then((res) => {
-    const { loginVideoBgVersion, loginVideoCoverVersion } = res.data;
+    const { loginVideoBgVersion } = res.data;
     login_video_bg_version.value = loginVideoBgVersion;
-    login_video_cover_version.value = loginVideoCoverVersion;
   });
 
   return ExposeData;
