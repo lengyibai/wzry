419c980d1b869140ebc7917066779d6282e87527
lengyibai <1329670984@qq.com>
2024-02-04 15:16:29 +0800

    perf: 组件优化

--- src/components/business/Parts/K-Input/index.vue
+++ src/components/business/Parts/K-Input/index.vue
@@ -1,33 +1,3 @@
-<template>
-  <div class="form-input" :class="{ disabled: disabled }">
-    <input
-      :type="type"
-      :value="modelValue"
-      :placeholder="placeholder"
-      :maxlength="max"
-      @input="input"
-      @focus="focus"
-      @blur="blur"
-    />
-
-    <!-- 获取焦点拉长线条 -->
-    <transition name="border">
-      <div v-if="line" v-show="is_focus" class="focus"></div>
-    </transition>
-
-    <!-- 输入不合法拉长线条 -->
-    <transition name="border">
-      <div v-show="!legal" class="border"></div>
-    </transition>
-
-    <!-- 输入不合法提示 -->
-    <transition name="tip">
-      <div v-if="!legal" class="tip">
-        {{ tip }}
-      </div>
-    </transition>
-  </div>
-</template>
 <script setup lang="ts">
 import { ref } from "vue";
 
@@ -61,6 +31,7 @@ const $props = withDefaults(defineProps<Props>(), {
   validate: () => "",
   type: "text",
   noSpecial: false,
+  required: true,
 });
 const $emit = defineEmits<{
   blur: [v: string | number];
@@ -69,8 +40,6 @@ const $emit = defineEmits<{
 
 /** 输入的值 */
 const modelValue = defineModel<number | string>({ default: "" });
-/** 是否为空 */
-const empty = defineModel("empty", { default: false });
 
 const $audioStore = AudioStore();
 
@@ -92,13 +61,13 @@ const focus = () => {
 const blur = (e: Event) => {
   const v = (e.target as HTMLInputElement).value;
   is_focus.value = false;
-  empty.value = false;
   setTimeout(() => {
     if ($props.required && v === "") {
       tip.value = "必填项";
       legal.value = false;
       return;
     }
+
     if ($props.noSpecial && !/^[\u4E00-\u9FA5A-Za-z0-9._]+$/.test(v) && v !== "") {
       tip.value = "不能含有特殊字符";
       legal.value = false;
@@ -124,17 +93,51 @@ const blur = (e: Event) => {
     }
 
     legal.value = true;
-    empty.value = true;
   });
 
   $emit("blur", v);
 };
 
 const input = (e: Event) => {
-  const v = (e.target as HTMLInputElement).value;
-  modelValue.value = v && ($props.number ? Number(v) : v);
+  const el = e.target as HTMLInputElement;
+  if ($props.max) {
+    el.value = el.value.slice(0, $props.max);
+  }
+  modelValue.value = el.value;
 };
 </script>
+
+<template>
+  <div class="form-input" :class="{ disabled: disabled }">
+    <input
+      :type="type"
+      :value="modelValue"
+      :placeholder="placeholder"
+      :maxlength="max"
+      @input="input"
+      @focus="focus"
+      @blur="blur"
+    />
+
+    <!-- 获取焦点拉长线条 -->
+    <transition name="border">
+      <div v-if="line" v-show="is_focus" class="focus"></div>
+    </transition>
+
+    <!-- 输入不合法拉长线条 -->
+    <transition name="border">
+      <div v-show="!legal" class="border"></div>
+    </transition>
+
+    <!-- 输入不合法提示 -->
+    <transition name="tip">
+      <div v-if="!legal" class="tip">
+        {{ tip }}
+      </div>
+    </transition>
+  </div>
+</template>
+
 <style scoped lang="less">
 @import url("./index.less");
 </style>
