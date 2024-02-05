f704336864719a34405920bded0b171756f22245
lengyibai <1329670984@qq.com>
2024-02-05 18:08:25 +0800

    fix: 修复无法显示浏览器名称及版本

--- src/views/Base/Error/NotVersion.vue
+++ src/views/Base/Error/NotVersion.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
-import { storeToRefs } from "pinia";
-
 import { DeviceStore } from "@/store";
 
 const $deviceStore = DeviceStore();
-
-const { browser_version, browser_name } = storeToRefs($deviceStore);
 </script>
 
 <template>
@@ -18,7 +14,9 @@ const { browser_version, browser_name } = storeToRefs($deviceStore);
         <span class="desc__green">Firefox 内核 >= 90</span><br />
         <span class="desc__green">Safari 内核 >= 15</span><br />
         您的
-        <span class="desc__red">{{ browser_name }} 内核为 {{ browser_version }}，</span>
+        <span class="desc__red">
+          {{ $deviceStore.browser_name }} 内核为 {{ $deviceStore.browser_version }}，
+        </span>
         不满足访问条件<br />请尝试更换或升级浏览器后再访问
       </div>
     </div>
