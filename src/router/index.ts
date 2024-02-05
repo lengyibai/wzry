7755b9f114684ec00abcf1982cabc05c0d42f0fe
lengyibai <1329670984@qq.com>
2024-02-05 18:20:29 +0800

    perf: 优化路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,67 +18,57 @@ router.beforeEach((to, from, next) => {
   const $authStore = AuthStore();
   const $deviceStore = DeviceStore();
 
-  const is_exist = isExist(to.path);
-  const is_login = isLogin(to.path);
-  const no_loading = ["/403", "/404", "/400"];
   const user_info = !!localStorage.getItem(LOCAL_KEY.USER_DATA);
 
-  /* 点击英雄详情会静默切换路由用于记录参数，此时不显示loading */
-  if (!to.query.id && !from.query.id && !no_loading.includes(to.path) && to.path !== from.path) {
-    $loading.show(`正在加载${to.meta.title}页`);
-  }
-
-  //浏览器版本过低
+  //浏览器版本过低，则跳转400，400较为特殊，如果跳转400则直接放行
   if (!$deviceStore.browser_status && to.path !== "/400") {
     next("/400");
     return;
-  } else if ($deviceStore.browser_status && to.path === "/400") {
-    next("/");
-    return;
-  } else if (to.path === "/400") {
-    next();
-    return;
   }
 
-  //如果路径不在路由表
-  if (!is_exist) {
+  //如果路径不在路由表，则跳转404
+  if (!isExist(to.path)) {
     next({
       path: "/404",
       replace: true,
     });
     return;
   }
-  //如果当前路由在路由表，但不在侧边栏
-  else if (to.matched.length === 0) {
+
+  //如果当前路由在路由表，但不在侧边栏（未登录状态，侧边栏为空），则跳转403
+  if (to.matched.length === 0) {
     next("/403");
     return;
   }
 
-  //如果需要登录，但是没有用户信息
-  else if (is_login && !user_info) {
+  //如果需要登录，但是没有用户信息，则跳转登录
+  if (isLogin(to.path) && !user_info) {
     next("/login");
     return;
   }
 
-  //如果本地有用户信息，并且跳转到静态路由
-  else if (user_info && to.meta.noVerify && to.path !== "/404") {
+  //如果本地有用户信息，且目标路由为免验证路由，则跳转首页
+  if (user_info && to.meta.noVerify) {
     next(BASE_CONFIG.HOME_URL);
     return;
   }
 
-  //如果未登录，但是本地存在用户信息，且能匹配权限
-  else if (!$authStore.userStatus && user_info) {
+  //如果未登录，但是本地存在用户信息，且能匹配权限，则直接放行
+  if (user_info && !$authStore.userStatus) {
     $authStore.autoLogin();
+  }
+
+  /* 点击英雄详情会静默切换路由用于记录参数，此时不显示loading */
+  if (!to.query.id && !from.query.id && to.path !== from.path) {
+    $loading.show(`正在加载${to.meta.title}页`);
   }
 
   next();
 });
 
 router.afterEach((to, from) => {
-  const no_loading = ["/403", "/404", "/400"];
-
   /* 点击英雄详情会静默切换路由用于记录参数，此时不显示loading */
-  if (!to.query.id && !from.query.id && !no_loading.includes(to.path) && to.path !== from.path) {
+  if (!to.query.id && !from.query.id && to.path !== from.path) {
     $loading.close();
   }
 
