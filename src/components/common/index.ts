9b51577da977695f188cea76759acd061d784dae
lengyibai <1329670984@qq.com>
2024-02-06 01:27:13 +0800

    delete: 删除设置头像组件

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,8 +1,7 @@
 import LibGrid from "./LibGrid/index.vue";
 import LibTable from "./LibTable/index.vue";
 import TableColumn from "./LibTable/components/TableColumn/index.vue";
-import LibUploadImg from "./LibUploadImg/index.vue";
 import LibWaterfall from "./LibWaterfall/index.vue";
 import LibDragSort from "./LibDragSort/index.vue";
 
-export { LibGrid, LibTable, TableColumn, LibUploadImg, LibWaterfall, LibDragSort };
+export { LibGrid, LibTable, TableColumn, LibWaterfall, LibDragSort };
