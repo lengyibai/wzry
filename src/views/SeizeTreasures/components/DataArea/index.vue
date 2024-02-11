80a8b6df8e985f25374c432f3354b67eddeb7680
lengyibai <1329670984@qq.com>
2024-02-11 22:18:21 +0800

    feat: 创建夺宝相关文件

---
+++ src/views/SeizeTreasures/components/DataArea/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="data-area"></div>
+</template>
+
+<style scoped lang="less">
+@import url("./index.less");
+</style>
--- src/views/SeizeTreasures/index.vue
+++ src/views/SeizeTreasures/index.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { onActivated } from "vue";
 
+import PlayArea from "./components/PlayArea/index.vue";
+import DataArea from "./components/DataArea/index.vue";
+
 import { AudioStore } from "@/store";
 
 defineOptions({
@@ -15,7 +18,10 @@ onActivated(() => {
 </script>
 
 <template>
-  <div class="seize-treasures"></div>
+  <div class="seize-treasures">
+    <PlayArea />
+    <DataArea />
+  </div>
 </template>
 
 <style scoped lang="less">
