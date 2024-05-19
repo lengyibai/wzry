cfab740f4415f0a8a48a7b498537c502e1ecd3c3
lengyibai <1329670984@qq.com>
2024-05-19 22:20:10 +0800

    feat: 百度统计

---
+++ public/js/baidu-hmt.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  const hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2c84cd61316a5ce978ca513745ec7eaa";
+  const s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
