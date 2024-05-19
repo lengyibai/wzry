d12849edda006e181124d7bfb3e15abc0b35c424
lengyibai <1329670984@qq.com>
2024-05-19 22:20:37 +0800

    feat: 文件加载失败提示

---
+++ public/js/file-error.js
@@ -0,0 +1,14 @@
+let is_error = false;
+window.addEventListener(
+  "error",
+  function (e) {
+    if (e.target.localName === "script" && e.target.href?.includes("wzry") && !is_error) {
+      is_error = true;
+      const status = confirm(
+        "js文件丢失，网站可能因更新移除了旧的js文件，可能会影响网站正常运行，点击确定后刷新浏览器获取最新文件",
+      );
+      status && location.reload();
+    }
+  },
+  true,
+);
