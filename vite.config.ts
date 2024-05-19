2669e75c01106268fc4a896e81397a5553e011f4
lengyibai <1329670984@qq.com>
2024-05-19 22:29:44 +0800

    perf: 优化PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
-// import { visualizer } from "rollup-plugin-visualizer";
+//import { visualizer } from "rollup-plugin-visualizer";
 import { VitePWA } from "vite-plugin-pwa";
 import basicSSL from "@vitejs/plugin-basic-ssl";
-// import legacy from "@vitejs/plugin-legacy";
+//import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -16,22 +16,44 @@ export default defineConfig(({ mode }) => {
     plugins: [
       vue(),
       basicSSL(), //手机端可能需要注释掉才能下载召唤师卡
-      // visualizer({
-      //   filename: "visualizer.html",
-      // }),
-      // legacy({
-      //   targets: [">0.1%", "last 2 version", "not dead"],
-      // }),
+      //visualizer({
+      //  filename: "visualizer.html",
+      //}),
+      //legacy({
+      //  targets: [">0.1%", "last 2 version", "not dead"],
+      //}),
+
       VitePWA({
         selfDestroying: true,
+        // 注入注册脚本到 HTML 中
+        injectRegister: "script",
+        // 使用 prompt 方式进行注册
         registerType: "autoUpdate",
+        // 开发环境下启用 PWA 功能
+        devOptions: {
+          enabled: true,
+        },
+        // Workbox 配置
+        workbox: {
+          // 匹配需要被缓存的文件
+          globPatterns: ["**/*.js", "**/*.html", "**/*.css", "**/*.svg", "**/*.png"],
+        },
         manifest: {
-          background_color: "#000",
+          related_applications: [
+            {
+              platform: "webapp",
+              url: "https://files.lyb.im/test/manifest.webmanifest",
+            },
+          ],
+          // 显示模式
           display: "standalone",
+          // 应用名称
           name: "王者荣耀图鉴",
+          // 移动端
           short_name: "王者图鉴",
-          start_url: "/king-honor",
+          // 主题颜色
           theme_color: "#000",
+          // start_url: "/king-honor",
           icons: [
             {
               src: "wzry.png",
@@ -39,11 +61,6 @@ export default defineConfig(({ mode }) => {
               type: "image/png",
             },
           ],
-        },
-        workbox: {
-          globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-          ignoreURLParametersMatching: [/.*/],
-          runtimeCaching: [],
         },
       }),
     ],
@@ -64,15 +81,15 @@ export default defineConfig(({ mode }) => {
         "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js",
       },
     },
-    // * 打包去除 console.log && debugger
+    //* 打包去除 console.log && debugger
     esbuild: {
       pure:
         getViteEnv("VITE_CLEAR_LOG") === "1"
-          ? ["console.log", "console.warn", "console.error", "debugger"]
+          ? ["alert", "console.log", "console.warn", "debugger"]
           : [],
     },
     build: {
-      chunkSizeWarningLimit: 2000, //超过2000kb警告
+      chunkSizeWarningLimit: 2000,
       reportCompressedSize: false, //禁用 gzip 压缩大小报告，可略微减少打包时间
       cssTarget: "chrome61",
       rollupOptions: {
@@ -80,7 +97,8 @@ export default defineConfig(({ mode }) => {
           manualChunks: {
             wzry_vue: ["vue"],
             wzry_vuePlugin: ["vue-router", "pinia", "mitt"],
-            wzry_util: ["dayjs", "lodash"],
+            wzry_lodash: ["lodash"],
+            wzry_dayjs: ["dayjs"],
             wzry_axios: ["axios"],
             wzry_i18n: ["vue-i18n"],
           },
