a479e32ade4c01e3c5f3960a53f7f4059e6582c5
lengyibai <1329670984@qq.com>
2024-05-21 11:38:53 +0800

    fix: 修复js文件加载失败没有正常提示

--- public/js/file-error.js
+++ public/js/file-error.js
@@ -2,7 +2,11 @@ let is_error = false;
 window.addEventListener(
   "error",
   function (e) {
-    if (e.target.localName === "script" && e.target.href?.includes("wzry") && !is_error) {
+    if (
+      ["link", "script"].includes(e.target.localName) &&
+      e.target.href?.includes("wzry") &&
+      !is_error
+    ) {
       is_error = true;
       const status = confirm(
         "js文件丢失，网站可能因更新移除了旧的js文件，可能会影响网站正常运行，点击确定后刷新浏览器获取最新文件",
