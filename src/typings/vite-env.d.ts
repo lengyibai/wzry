571415828620630193fcb332ab7c8207d8418e1b
lengyibai <1329670984@qq.com>
2024-05-25 03:27:42 +0800

    rename: 配置重命名

--- src/typings/vite-env.d.ts
+++ src/typings/vite-env.d.ts
@@ -2,7 +2,7 @@ interface ImportMetaEnv {
   /** 是否打包清除打印及debug */
   VITE_CLEAR_LOG: string;
   /** 远程API地址 */
-  VITE_REMOTE_API_URL: string;
+  VITE_DATABASE_URL: string;
   /** 远程静态资源地址 */
   VITE_RESOURCE_URL: string;
   /** 部署地址前缀 */
