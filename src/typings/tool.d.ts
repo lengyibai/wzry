0cc0e20efa0413e76ee9c2c72babe272acd4066b
lengyibai <1329670984@qq.com>
2024-05-27 15:26:49 +0800

    type: 增加泛型组件实例类型

--- src/typings/tool.d.ts
+++ src/typings/tool.d.ts
@@ -2,3 +2,10 @@
 declare type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
 };
+
+/** @description 泛型组件实例类型 */
+declare type GenericComponentInstanceType<D extends (...p: any[]) => any> =
+  //组件通用类型
+  import("vue").ComponentPublicInstance &
+    //defineExpose暴露的数据类型
+    Parameters<NonNullable<NonNullable<ReturnType<D>["__ctx"]>["expose"]>>[0];
