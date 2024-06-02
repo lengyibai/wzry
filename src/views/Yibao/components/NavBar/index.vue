7005986fef51f3f49f167d6d9b6a8e785613fff4
lengyibai <1329670984@qq.com>
2024-06-03 02:32:20 +0800

    feat: 移除KLongNum组件上的类名

--- src/views/Yibao/components/NavBar/index.vue
+++ src/views/Yibao/components/NavBar/index.vue
@@ -23,7 +23,7 @@ const { articles } = storeToRefs($knapsackStore);
         class="jump"
       >
         <img class="icon" :src="_getPropLink('jump_coin')" alt="" />
-        <KLongNum class="k-long-num" font-size="1.5rem" :value="articles.JUMP_COIN" />
+        <KLongNum font-size="1.5rem" :value="articles.JUMP_COIN" />
       </div>
       <div
         v-mouse-tip="{
@@ -33,7 +33,7 @@ const { articles } = storeToRefs($knapsackStore);
         class="guess"
       >
         <img class="icon" :src="_getPropLink('guess_coin')" alt="" />
-        <KLongNum class="k-long-num" font-size="1.5rem" :value="articles.GUESS_COIN" />
+        <KLongNum font-size="1.5rem" :value="articles.GUESS_COIN" />
       </div>
     </div>
   </div>
