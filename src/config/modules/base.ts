f0a514c65702e5fc6b30f9f5b9a5e512fe67191f
lengyibai <1329670984@qq.com>
2024-06-03 21:10:27 +0800

    remove: 移除无用属性

--- src/config/modules/base.ts
+++ src/config/modules/base.ts
@@ -2,8 +2,6 @@
 export const BASE_CONFIG = {
   /** 首页地址 */
   HOME_URL: "/hero",
-  /** 数据token过期时间(秒) */
-  OVERDUE_DATA_TIME: 259200, //3天
   /** 图床链接 */
   IMGBED: "https://lengyibai.github.io/wzry-material",
 };
