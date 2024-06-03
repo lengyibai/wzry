ae0898b563b133920d9aaa1af9a578b675d5d489
lengyibai <1329670984@qq.com>
2024-06-04 01:34:10 +0800

    style: 移除无用代码

--- src/components/business/Parts/K-ScrollTip/index.vue
+++ src/components/business/Parts/K-ScrollTip/index.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
-import { ref } from "vue";
-
 import { _isPhone } from "@/utils/tool";
 
-const kScrollTipRef = ref<HTMLElement>();
-
 const text = _isPhone ? "滑动下一页" : "滚动下一页";
-
-defineExpose({
-  _el: kScrollTipRef,
-});
 </script>
 
 <template>
   <!-- 向上滚动提示 -->
-  <div ref="kScrollTipRef" class="k-scroll-tip">
+  <div class="k-scroll-tip">
     <i class="iconfont wzry-down" />
     <div class="text">{{ text }}</div>
   </div>
