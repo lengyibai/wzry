2946c143cbd3e68be26b427b30e3fb1b4e69ee1d
lengyibai <1329670984@qq.com>
2024-08-29 01:06:43 +0800

    misc: 优化计算方式

--- public/js/flexible.js
+++ public/js/flexible.js
@@ -9,15 +9,12 @@ function onFlexible() {
   //获取当前浏览器窗口的宽度
   const width = flexible_html.clientWidth;
 
-  if (width < flexible_min_device_width) {
-    //如果宽度小于最小设备宽度，使用最小字体大小
-    font_size = flexible_min_font_size;
-  } else if (width > flexible_max_device_width) {
+  if (width > flexible_max_device_width) {
     //若视口宽度大于最大设备宽度，根据超出的px按比例增加字体大小
     font_size = flexible_max_font_size + (width - flexible_max_device_width) / 10 / 12;
   } else {
     //如果宽度在最小和最大设备宽度之间，按比例计算字体大小
-    font_size = flexible_max_font_size - (flexible_max_device_width - width) / 10 / (12 * 2);
+    font_size = flexible_max_font_size - (flexible_max_font_size - flexible_min_font_size) * (1 - width / flexible_max_device_width);
   }
 
   //设置页面根元素的字体大小
