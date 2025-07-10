65f69b98d8305d544f7b1dbbfaceb5a51e25c6d0
lengyibai <1329670984@qq.com>
2025-07-10 17:06:46 +0800

    update: 百度统计

--- public/js/baidu-hmt.js
+++ public/js/baidu-hmt.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
-(function () {
-  const hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?2c84cd61316a5ce978ca513745ec7eaa";
-  const s = document.getElementsByTagName("script")[0];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?97101336d9a830a2a6a46980e43155fb";
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
